415481c7896210c94bf4d69c72cc56ce926171c0
Josh Stewart <josh@noisymime.org>
2015-02-07 13:50:36 +1100

    Split utils file

---
+++ utils.h
@@ -0,0 +1,12 @@
+/*
+These are some utility functions and variables used through the main code
+*/ 
+#include <Arduino.h>
+#define MS_IN_MINUTE 60000
+#define US_IN_MINUTE 60000000
+
+int freeRam ();
+int AIRDEN();
+unsigned int PW();
+unsigned int PW_SD();
+unsigned int PW_AN();
