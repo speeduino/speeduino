a2a4c56c72b835cc7175b8af831a026b0ec5e3f3
Fredrik Johansson <fredrik@mumme.se>
2015-02-20 02:30:37 +0100

    TunerStudio communication had wrong endianess Arduino avr is little endian

--- comms.ino
+++ comms.ino
@@ -136,8 +136,8 @@ void sendValues(int length)
   response[10] = currentStatus.egoCorrection; //Exhaust gas correction (%)
   response[11] = 0x00; //Air Correction (%)
   response[12] = currentStatus.wueCorrection; //Warmup enrichment (%)
-  response[13] = highByte(currentStatus.RPM); //rpm HB
-  response[14] = lowByte(currentStatus.RPM); //rpm LB
+  response[13] = lowByte(currentStatus.RPM); //rpm LB
+  response[14] = highByte(currentStatus.RPM); //rpm HB
   response[15] = currentStatus.TAEamount; //acceleration enrichment (%)
   response[16] = 0x00; //Barometer correction (%)
   response[17] = currentStatus.corrections; //Total GammaE (%)
@@ -148,16 +148,16 @@ void sendValues(int length)
   response[22] = currentStatus.advance;
   response[23] = currentStatus.TPS; // TPS (0% to 100%)
   //Need to split the int loopsPerSecond value into 2 bytes
-  response[24] = highByte(currentStatus.loopsPerSecond);
-  response[25] = lowByte(currentStatus.loopsPerSecond);
+  response[24] = lowByte(currentStatus.loopsPerSecond);
+  response[25] = highByte(currentStatus.loopsPerSecond);
  
   //The following can be used to show the amount of free memory
   int mem = freeRam();
-  response[26] = highByte(mem); //(byte)((currentStatus.loopsPerSecond >> 8) & 0xFF);
-  response[27] = lowByte(mem);
+  response[26] = lowByte(mem); //(byte)((currentStatus.loopsPerSecond >> 8) & 0xFF);
+  response[27] = highByte(mem);
   
-  //response[26] = highByte(cltCalibrationTable.axisX16[0]); //(byte)((currentStatus.loopsPerSecond >> 8) & 0xFF);
-  //response[27] = lowByte(cltCalibrationTable.axisX16[0]);
+  //response[26] = lowByte(cltCalibrationTable.axisX16[0]); //(byte)((currentStatus.loopsPerSecond >> 8) & 0xFF);
+  //response[27] = highByte(cltCalibrationTable.axisX16[0]);
   response[28] = 0x01 & currentStatus.isSequential;
   
 
