a8b53afabfba011e9f78bb48e08b284b52f29387
MoysieWRX <michael.moyes@hotmail.co.uk>
2015-05-23 04:34:29 +0300

    Update speeduino.ino

--- speeduino.ino
+++ speeduino.ino
@@ -201,6 +201,7 @@ void setup()
     default:
       trigger = triggerPri_missingTooth;
       getRPM = getRPM_missingTooth;
+      getCrankAngle = getCrankAngle_missingTooth
       break;
   }
   if(configPage2.TrigEdge == 0)
