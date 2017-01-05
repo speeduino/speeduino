251000893c9cd802b426c31f4e02d8b3ea3fef70
Josh Stewart <josh@noisymime.org>
2017-01-06 08:26:11 +1100

    Jan 17 dev signature

--- comms.ino
+++ comms.ino
@@ -62,12 +62,12 @@ void command()
       break;
 
     case 'S': // send code version
-      Serial.print("Speeduino 2016.12");
+      Serial.print("Speeduino 2017.01-dev");
       currentStatus.secl = 0; //This is required in TS3 due to its stricter timings
       break;
 
     case 'Q': // send code version
-      Serial.print("speeduino 201612");
+      Serial.print("speeduino 201701-dev");
      break;
 
     case 'V': // send VE table and constants in binary
