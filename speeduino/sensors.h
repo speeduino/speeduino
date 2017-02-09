7db51bd41614aee89d08a6c5fe1db0cfca6100d1
Josh Stewart <josh@noisymime.org>
2017-02-09 14:17:20 +1100

    Sanity checks on the average MAP calculation

--- speeduino/sensors.h
+++ speeduino/sensors.h
@@ -1,7 +1,7 @@
 #ifndef SENSORS_H
 #define SENSORS_H
 
-// The following are alpha values for the ADC filters. 
+// The following are alpha values for the ADC filters.
 // Their values are from 0 to 255 with 0 being no filtering and 255 being maximum
 #define ADCFILTER_TPS  128
 #define ADCFILTER_CLT  180
@@ -21,12 +21,12 @@ volatile int AnChannel[15];
 
 unsigned long MAPrunningValue; //Used for tracking either the total of all MAP readings in this cycle (Event average) or the lowest value detected in this cycle (event minimum)
 unsigned int MAPcount; //Number of samples taken in the current MAP cycle
-byte MAPcurRev = 0; //Tracks which revolution we're sampling on
+byte MAPcurRev; //Tracks which revolution we're sampling on
 
 /*
  * Simple low pass IIR filter macro for the analog inputs
  * This is effectively implementing the smooth filter from http://playground.arduino.cc/Main/Smooth
- * But removes the use of floats and uses 8 bits of fixed precision. 
+ * But removes the use of floats and uses 8 bits of fixed precision.
  */
 #define ADC_FILTER(input, alpha, prior) (((long)input * (256 - alpha) + ((long)prior * alpha))) >> 8
 
@@ -45,7 +45,7 @@ ISR(ADC_vect)
 
   //ADCSRA = 0x6E;  // ADC disabled by clearing bit 7(ADEN)
   //BIT_CLEAR(ADCSRA, ADIE);
-  
+
   nChannel = ADMUX & 0x07;
   #if defined(__AVR_ATmega1281__) || defined(__AVR_ATmega2561__)
     if (nChannel==7) { ADMUX = 0x40; }
@@ -57,7 +57,7 @@ ISR(ADC_vect)
       ADCSRB = 0x00; //clear MUX5 bit
     }
     else if (nChannel == 7) //channel 7
-    { 
+    {
       ADMUX = 0x40;
       ADCSRB = 0x08; //Set MUX5 bit
     }
