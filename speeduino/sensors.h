a7d0a956116301b059eadba9663d5ef338ab75b3
Josh Stewart <josh@noisymime.org>
2017-02-14 15:21:38 +1100

    Use currentStatus.spark rather than currentStatus.engine for error indicator. Fixes #67

--- speeduino/sensors.h
+++ speeduino/sensors.h
@@ -2,7 +2,7 @@
 #define SENSORS_H
 
 #include "Arduino.h"
-#include "globals.h"
+
 
 // The following are alpha values for the ADC filters.
 // Their values are from 0 to 255 with 0 being no filtering and 255 being maximum
