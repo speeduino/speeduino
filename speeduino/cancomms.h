60a013655c68db7c60145adb8ce40e94f14b94b7
Josh Stewart <josh@noisymime.org>
2017-05-31 18:57:36 +1000

    MISRA compliant cancomms.ino

--- speeduino/cancomms.h
+++ speeduino/cancomms.h
@@ -6,12 +6,20 @@
 uint8_t currentcanCommand;
 uint8_t currentCanPage = 1;//Not the same as the speeduino config page numbers
 uint8_t nCanretry = 0;      //no of retrys
-uint8_t cancmdfail = 0;     //command fail yes/no 
-uint8_t canlisten = 0;      
+uint8_t cancmdfail = 0;     //command fail yes/no
+uint8_t canlisten = 0;
 uint8_t Lbuffer[8];         //8 byte buffer to store incomng can data
 uint8_t Gdata[9];
 uint8_t Glow, Ghigh;
-  
+
+#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+  HardwareSerial &CANSerial = Serial3;
+#elif defined(CORE_STM32)
+  HardwareSerial &CANSerial = Serial2;
+#elif defined(CORE_TEENSY)
+  HardwareSerial &CANSerial = Serial2;
+#endif
+
 void canCommand();//This is the heart of the Command Line Interpeter.  All that needed to be done was to make it human readable.
 void sendCancommand(uint8_t cmdtype , uint16_t canadddress, uint8_t candata1, uint8_t candata2, uint16_t paramgroup);
 
--- speeduino/cancomms.ino
+++ speeduino/cancomms.ino
@@ -11,20 +11,9 @@ It parses the command and calls the relevant function
 sendcancommand is called when a comman d is to be sent via serial3 to the Can interface
 */
 
-//#include "cancomms.h"
-//#include "globals.h"
-//#include "storage.h"
-
 void canCommand()
 {
-#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)  
-  currentcanCommand = Serial3.read();
-#elif defined(CORE_STM32)
-  currentcanCommand = Serial2.read();
-#elif defined(CORE_TEENSY)
-  currentcanCommand = Serial2.read();
-#else return;  
-#endif  
+  currentcanCommand = CANSerial.read();
 
   switch (currentcanCommand)
   {
@@ -34,32 +23,15 @@ void canCommand()
 
     case 'G': // this is the reply command sent by the Can interface
         //uint8_t Gdata;
-#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)       
-        while (Serial3.available() == 0) { }
-        cancmdfail = Serial3.read();
-#elif defined(CORE_STM32)
-        while (Serial2.available() == 0) { }
-        cancmdfail = Serial2.read();
-#elif defined(CORE_TEENSY)
-        while (Serial2.available() == 0) { }
-        cancmdfail = Serial2.read();
-#else return;        
-#endif
+        while (CANSerial.available() == 0) { }
+        cancmdfail = CANSerial.read();
+
         if (cancmdfail != 0)
         {
           for (byte Gx = 0; Gx < 8; Gx++) //read all 8 bytes of data
             {
-#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)             
-              while (Serial3.available() == 0) { }
-              Gdata[Gx] = Serial3.read();
-#elif defined(CORE_STM32)
-              while (Serial2.available() == 0) { }
-              Gdata[Gx] = Serial2.read();
-#elif defined(CORE_TEENSY)
-              while (Serial2.available() == 0) { }
-              Gdata[Gx] = Serial2.read();
-#else return;              
-#endif
+              while (CANSerial.available() == 0) { }
+              Gdata[Gx] = CANSerial.read();
             }
 
           Glow = Gdata[(configPage10.caninput_param_start_byte[currentStatus.current_caninchannel])];
@@ -72,150 +44,92 @@ void canCommand()
              }
           else
              {
-              Ghigh = 0;     
+              Ghigh = 0;
              }
-                
-          currentStatus.canin[currentStatus.current_caninchannel] = word(Ghigh, Glow);   
+
+          currentStatus.canin[currentStatus.current_caninchannel] = word(Ghigh, Glow);
         }
-        
-        else{}  //continue as command request failed and/or data/device was not available  
-        
+
+        else{}  //continue as command request failed and/or data/device was not available
+
         if (currentStatus.current_caninchannel <= 6)     // if channel is 0-7
            {
             currentStatus.current_caninchannel++;       //inc to next channel
            }
-        else  
+        else
            {
             currentStatus.current_caninchannel = 0;      //reset to start
-           }   
-             
+           }
+
         break;
 
     case 'L':
         uint8_t Llength;
-#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
-        while (Serial3.available() == 0) { }
-        canlisten = Serial3.read();
-#elif defined(CORE_STM32)
-        while (Serial2.available() == 0) { }
-        canlisten = Serial2.read();
-#elif defined(CORE_TEENSY)
-        while (Serial2.available() == 0) { }
-        canlisten = Serial2.read();
-#else return;        
-#endif
+        while (CANSerial.available() == 0) { }
+        canlisten = CANSerial.read();
+
         if (canlisten == 0)
          {
           //command request failed and/or data/device was not available
           break;
          }
-#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)         
-       while (Serial3.available() == 0) { }
-       Llength= Serial3.read();              // next the number of bytes expected value
-#elif defined(CORE_STM32)
-       while (Serial2.available() == 0) { }
-       Llength= Serial2.read();              // next the number of bytes expected value
-#elif defined(CORE_TEENSY)       
-       while (Serial2.available() == 0) { }
-       Llength= Serial2.read();              // next the number of bytes expected value
-#else return;       
-#endif
+
+         while (CANSerial.available() == 0) { }
+         Llength= CANSerial.read();              // next the number of bytes expected value
+
          for (uint8_t Lcount = 0; Lcount <Llength ;Lcount++)
-                {
-#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)                  
-                  while (Serial3.available() == 0){}
-                  // receive all x bytes into "Lbuffer"
-                  Lbuffer[Lcount] = Serial3.read();
-#elif defined(CORE_STM32)
-                  while (Serial2.available() == 0){}
-                  // receive all x bytes into "Lbuffer"
-                  Lbuffer[Lcount] = Serial2.read();
-#elif defined(CORE_TEENSY)
-                  while (Serial2.available() == 0){}
-                  // receive all x bytes into "Lbuffer"
-                  Lbuffer[Lcount] = Serial2.read();
-#else return;                  
-#endif
-                }
-       break;
-       
+         {
+           while (CANSerial.available() == 0){}
+           // receive all x bytes into "Lbuffer"
+           Lbuffer[Lcount] = CANSerial.read();
+         }
+         break;
+
     case 'r': //New format for the optimised OutputChannels
       byte cmd;
-      byte tsCanId_sent;
-#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__) //ATmega2561 does not have Serial3      
-      if (Serial3.available() < 6) { return; }
-      tsCanId_sent = Serial3.read(); //Read the $tsCanId
-      cmd = Serial3.read();
-#elif defined(CORE_STM32)
-      if (Serial2.available() < 6) { return; }
-      tsCanId_sent = Serial2.read(); //Read the $tsCanId
-      cmd = Serial2.read();
-#elif defined(CORE_TEENSY)
-      if (Serial2.available() < 6) { return; }
-      tsCanId_sent = Serial2.read(); //Read the $tsCanId
-      cmd = Serial2.read();
-#else return;      
-#endif
-      uint16_t offset, length;
-      if(cmd == 0x30) //Send output channels command 0x30 is 48dec
+
+      if (CANSerial.available() >= 6)
       {
-        byte tmp;
-#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__) //ATmega2561 does not have Serial3
-        tmp = Serial3.read();
-        offset = word(Serial3.read(), tmp);
-        tmp = Serial3.read();
-        length = word(Serial3.read(), tmp);
-#elif defined(CORE_STM32)
-        tmp = Serial2.read();
-        offset = word(Serial2.read(), tmp);
-        tmp = Serial2.read();
-        length = word(Serial2.read(), tmp);
-#elif defined(CORE_TEENSY)
-        tmp = Serial2.read();
-        offset = word(Serial2.read(), tmp);
-        tmp = Serial2.read();
-        length = word(Serial2.read(), tmp);
-#else return;        
-#endif        
-        sendValues(offset, length, 3);
-      }
-      else
-      {
-        //No other r/ commands should be called
+        CANSerial.read(); //Read the $tsCanId
+        cmd = CANSerial.read();
+
+        uint16_t offset, length;
+        if(cmd == 0x30) //Send output channels command 0x30 is 48dec
+        {
+          byte tmp;
+          tmp = CANSerial.read();
+          offset = word(CANSerial.read(), tmp);
+          tmp = CANSerial.read();
+          length = word(CANSerial.read(), tmp);
+
+          sendValues(offset, length, 3);
+        }
+        else
+        {
+          //No other r/ commands should be called
+        }
       }
       break;
-      
+
     case 'S': // send code version
-       for (unsigned int sig = 0; sig < sizeof(displaySignature) - 1; sig++){
-#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__) //ATmega2561 does not have Serial3        
-           Serial3.write(displaySignature[sig]);
-#elif defined(CORE_STM32) 
-          Serial2.write(displaySignature[sig]);
-#elif defined(CORE_TEENSY)
-          Serial2.write(displaySignature[sig]);
-#else return;
-#endif          
+       for (unsigned int sig = 0; sig < sizeof(displaySignature) - 1; sig++)
+       {
+         CANSerial.write(displaySignature[sig]);
        }
        //Serial3.print("speeduino 201609-dev");
        break;
 
     case 'Q': // send code version
-       for (unsigned int revn = 0; revn < sizeof( TSfirmwareVersion) - 1; revn++){
-#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__) //ATmega2561 does not have Serial3
-           Serial3.write( TSfirmwareVersion[revn]);
-#elif defined(CORE_STM32) 
-           Serial2.write( TSfirmwareVersion[revn]);          
-#elif defined(CORE_TEENSY)
-           Serial2.write( TSfirmwareVersion[revn]);
-#else return;
-#endif           
+       for (unsigned int revn = 0; revn < sizeof( TSfirmwareVersion) - 1; revn++)
+       {
+         CANSerial.write( TSfirmwareVersion[revn]);
        }
        //Serial3.print("speeduino 201609-dev");
        break;
 
     case 'Z': //dev use
        break;
-       
+
     default:
        break;
   }
@@ -227,65 +141,28 @@ void sendCancommand(uint8_t cmdtype, uint16_t canaddress, uint8_t candata1, uint
     switch (cmdtype)
     {
       case 0:
-#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__) //ATmega2561 does not have Serial3       
-        Serial3.print("G");
-        Serial3.write(canaddress);  //tscanid of speeduino device
-        Serial3.write(candata1);    // table id
-        Serial3.write(candata2);    //table memory offset
-#elif defined(CORE_STM32)
-        Serial2.print("G");
-        Serial2.write(canaddress);  //tscanid of speeduino device
-        Serial2.write(candata1);    // table id
-        Serial2.write(candata2);    //table memory offset
-#elif defined(CORE_TEENSY)
-        Serial2.print("G");
-        Serial2.write(canaddress);  //tscanid of speeduino device
-        Serial2.write(candata1);    // table id
-        Serial2.write(candata2);    //table memory offset
-#else return;
-#endif        
-     break;
+        CANSerial.print("G");
+        CANSerial.write(canaddress);  //tscanid of speeduino device
+        CANSerial.write(candata1);    // table id
+        CANSerial.write(candata2);    //table memory offset
+        break;
 
-     case 1:                      //send request to listen for a can message
-#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__) //ATmega2561 does not have Serial3 
-        Serial3.print("L");
-        Serial3.write(canaddress);  //11 bit canaddress of device to listen for
-#elif defined(CORE_STM32)
-        Serial2.print("L");
-        Serial2.write(canaddress);  //11 bit canaddress of device to listen for
-#elif defined(CORE_TEENSY)        
-        Serial2.print("L");
-        Serial2.write(canaddress);  //11 bit canaddress of device to listen for
-
-#else return;
-#endif
-     break;
+      case 1:                      //send request to listen for a can message
+        CANSerial.print("L");
+        CANSerial.write(canaddress);  //11 bit canaddress of device to listen for
+        break;
 
      case 2:
-#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__) //ATmega2561 does not have Serial3     
-        Serial3.print("R");                         //send "R" to request data from the parmagroup whos value is sent next
-        Serial3.write( lowByte(paramgroup) );       //send lsb first
-        Serial3.write( lowByte(paramgroup >> 8) );
-#elif defined(CORE_STM32)
-        Serial2.print("R");                         //send "R" to request data from the parmagroup whos value is sent next
-        Serial2.write( lowByte(paramgroup) );       //send lsb first
-        Serial2.write( lowByte(paramgroup >> 8) );
-#elif defined(CORE_TEENSY)
-        Serial2.print("R");                         //send "R" to request data from the parmagroup whos value is sent next
-        Serial2.write( lowByte(paramgroup) );       //send lsb first
-        Serial2.write( lowByte(paramgroup >> 8) );
-#else return;
-#endif
-     break;
+        CANSerial.print("R");                         //send "R" to request data from the parmagroup whos value is sent next
+        CANSerial.write( lowByte(paramgroup) );       //send lsb first
+        CANSerial.write( lowByte(paramgroup >> 8) );
+        break;
 
      case 3:
         //send to truecan send routine
-#if defined(CORE_STM32)
+        break;
 
-#elif defined(CORE_TEENSY)
-#else return;
-#endif
-     break;
+     default:
+        break;
     }
 }
-
