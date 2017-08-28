8f9619ca96242e2a274c64fb6059c1d6be0457c1
Vitor Moreno B. Sales <vitor_boss@yahoo.com.br>
2017-08-28 01:54:53 -0300

    Update timers.ino

--- speeduino/timers.ino
+++ speeduino/timers.ino
@@ -2,7 +2,7 @@
 Speeduino - Simple engine management for the Arduino Mega 2560 platform
 Copyright (C) Josh Stewart
 A full copy of the license may be found in the projects root directory
-*/
+*/ 
 
 /*
 Timers are used for having actions performed repeatedly at a fixed interval (Eg every 100ms)
