8fe7ff66a96cdf626fa331695ebf30674e5bc297
Josh Stewart <josh@noisymime.org>
2017-12-15 16:23:56 +1100

    December dev signature

--- speeduino/comms.ino
+++ speeduino/comms.ino
@@ -132,7 +132,7 @@ void command()
       break;
 
     case 'Q': // send code version
-      Serial.print("speeduino 201711");
+      Serial.print("speeduino 201712-dev");
       break;
 
     case 'r': //New format for the optimised OutputChannels
@@ -162,7 +162,7 @@ void command()
       break;
 
     case 'S': // send code version
-      Serial.print("Speeduino 2017.11");
+      Serial.print("Speeduino 2017.12-dev");
       currentStatus.secl = 0; //This is required in TS3 due to its stricter timings
       break;
 
