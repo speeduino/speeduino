a10fdd1d65a112554be5af5c1b59c8e0d5b3e99d
ElDominio <normsp@gmail.com>
2018-05-22 13:22:12 -0400

    Fixed more open loop stuff

--- speeduino/globals.h
+++ speeduino/globals.h
@@ -170,6 +170,9 @@
 #define RESET_CONTROL_PREVENT_ALWAYS       2
 #define RESET_CONTROL_SERIAL_COMMAND       3
 
+#define OPEN_LOOP_BOOST     0
+#define CLOSED_LOOP_BOOST   1
+
 #define MAX_RPM 18000 //This is the maximum rpm that the ECU will attempt to run at. It is NOT related to the rev limiter, but is instead dictates how fast certain operations will be allowed to run. Lower number gives better performance
 #define engineSquirtsPerCycle 2 //Would be 1 for a 2 stroke
 
