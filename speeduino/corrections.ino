f276bb7042ca94e21d08ec24c482a8103ccb41b0
Josh Stewart <josh@noisymime.org>
2018-06-29 18:07:39 +1000

    Add Fix angle on/off setting

--- speeduino/corrections.ino
+++ speeduino/corrections.ino
@@ -406,7 +406,7 @@ int8_t correctionsIgn(int8_t base_advance)
 static inline int8_t correctionFixedTiming(int8_t advance)
 {
   int8_t ignFixValue = advance;
-  if (configPage4.FixAng != 0) { ignFixValue = configPage4.FixAng; } //Check whether the user has set a fixed timing angle
+  if (configPage2.fixAngEnable == 1) { ignFixValue = configPage4.FixAng; } //Check whether the user has set a fixed timing angle
   return ignFixValue;
 }
 
