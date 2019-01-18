8c45cb31b07e63f7f8e015c2931616d6081c4ef4
Josh Stewart <josh@noisymime.org>
2019-01-18 21:46:23 +1100

    Initial basic start on the avr2560.h header

---
+++ speeduino/src/boards/avr2560.h
@@ -0,0 +1,14 @@
+#ifndef AVR2560_H
+#define AVR2560_H
+#if defined(CORE_AVR)
+
+//#include "sam.h"
+
+/*
+***********************************************************************************************************
+* General
+*/
+  #define PORT_TYPE uint8_t //Size of the port variables (Eg inj1_pin_port).
+
+#endif //CORE_AVR
+#endif //AVR2560_H
