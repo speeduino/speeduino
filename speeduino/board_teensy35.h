0a4f9666d814c2a0348cb100fc9910bf3a9ae906
Josh Stewart <josh@noisymime.org>
2019-01-20 16:13:24 +1300

    Add missing files for the stm32 and teensy platforms

--- speeduino/board_teensy35.h
+++ speeduino/board_teensy35.h
@@ -6,6 +6,7 @@
 ***********************************************************************************************************
 * General
 */
+  void initBoard();
   #define PORT_TYPE uint8_t //Size of the port variables
   #define BOARD_DIGITAL_GPIO_PINS 34
   #define BOARD_NR_GPIO_PINS 34
