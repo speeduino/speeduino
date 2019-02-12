9ca230df0a29515020019fdc722d0ecd03995765
Josh Stewart <josh@noisymime.org>
2019-02-12 17:06:29 +1100

    Signatures and base tunes for 201902

--- speeduino/comms.ino
+++ speeduino/comms.ino
@@ -195,7 +195,7 @@ void command()
       break;
 
     case 'Q': // send code version
-      Serial.print(F("speeduino 201812-dev"));
+      Serial.print(F("speeduino 201902"));
       break;
 
     case 'r': //New format for the optimised OutputChannels
@@ -225,7 +225,7 @@ void command()
       break;
 
     case 'S': // send code version
-      Serial.print(F("Speeduino 2018.12-dev"));
+      Serial.print(F("Speeduino 2019.02"));
       currentStatus.secl = 0; //This is required in TS3 due to its stricter timings
       break;
 
