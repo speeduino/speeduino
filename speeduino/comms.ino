speeduino/speeduino

commit f41f95a60c544656b35e5bfda4e04b4ef7de53f2

More testing of doxygen

speeduino/comms.ino
@@ -93,10 +93,8 @@ void command()
 
       if(Serial.available() >= 2)
       {
-        cmdGroup = Serial.read();
-        cmdValue = Serial.read();
-        cmdCombined = word(cmdGroup, cmdValue);
-        if (currentStatus.RPM == 0) { commandButtons(); }
+        int cmdCombined = word(Serial.read(), Serial.read());
+        if (currentStatus.RPM == 0) { commandButtons(cmdCombined); }
 
         cmdPending = false;
       }
@@ -513,6 +511,7 @@ void sendValues(uint16_t offset, uint16_t packetLength, byte cmd, byte portNum)
   fullStatus[23] = currentStatus.advance;
   fullStatus[24] = currentStatus.TPS; // TPS (0% to 100%)
   //Need to split the int loopsPerSecond value into 2 bytes
+  if(currentStatus.loopsPerSecond > 60000) { currentStatus.loopsPerSecond = 60000;}
   fullStatus[25] = lowByte(currentStatus.loopsPerSecond);
   fullStatus[26] = highByte(currentStatus.loopsPerSecond);
 
@@ -1656,32 +1655,35 @@ void testComm()
   return;
 }
 
-void commandButtons()
+void commandButtons(int buttonCommand)
 {
-  switch (cmdCombined)
+  switch (buttonCommand)
   {
     case 256: // cmd is stop
       BIT_CLEAR(currentStatus.testOutputs, 1);
-    endCoil1Charge();
-    endCoil2Charge();
-    endCoil3Charge();
-    endCoil4Charge();
-    closeInjector1();
-    closeInjector2();
-    closeInjector3();
-    closeInjector4();
+      endCoil1Charge();
+      endCoil2Charge();
+      endCoil3Charge();
+      endCoil4Charge();
+      closeInjector1();
+      closeInjector2();
+      closeInjector3();
+      closeInjector4();
       break;
 
     case 257: // cmd is enable
       // currentStatus.testactive = 1;
       BIT_SET(currentStatus.testOutputs, 1);
       break;
+
     case 513: // cmd group is for injector1 on actions
         if( BIT_CHECK(currentStatus.testOutputs, 1) ){ openInjector1(); }
       break;
+
     case 514: // cmd group is for injector1 off actions
         if( BIT_CHECK(currentStatus.testOutputs, 1) ){ closeInjector1(); }
       break;
+
     case 515: // cmd group is for injector1 50% dc actions
       //for (byte dcloop = 0; dcloop < 11; dcloop++)
       //{
@@ -1691,67 +1693,90 @@ void commandButtons()
       //  delay(500);
       //}
       break;
+
     case 516: // cmd group is for injector2 on actions
         if( BIT_CHECK(currentStatus.testOutputs, 1) ){ openInjector2(); }
       break;
+
     case 517: // cmd group is for injector2 off actions
         if( BIT_CHECK(currentStatus.testOutputs, 1) ){ closeInjector2(); }
       break;
+
     case 518: // cmd group is for injector2 50%dc actions
 
       break;
+
     case 519: // cmd group is for injector3 on actions
         if( BIT_CHECK(currentStatus.testOutputs, 1) ){ openInjector3(); }
       break;
+
     case 520: // cmd group is for injector3 off actions
         if( BIT_CHECK(currentStatus.testOutputs, 1) ){ closeInjector3(); }
       break;
+
     case 521: // cmd group is for injector3 50%dc actions
 
       break;
+
     case 522: // cmd group is for injector4 on actions
         if( BIT_CHECK(currentStatus.testOutputs, 1) ){ openInjector4(); }
       break;
+
     case 523: // cmd group is for injector4 off actions
         if( BIT_CHECK(currentStatus.testOutputs, 1) ){ closeInjector4(); }
       break;
+
     case 524: // cmd group is for injector4 50% dc actions
 
       break;
+
     case 769: // cmd group is for spark1 on actions
         if( BIT_CHECK(currentStatus.testOutputs, 1) ) { digitalWrite(pinCoil1, coilHIGH); }
       break;
+
     case 770: // cmd group is for spark1 off actions
         if( BIT_CHECK(currentStatus.testOutputs, 1) ) { digitalWrite(pinCoil1, coilLOW); }
       break;
+
     case 771: // cmd group is for spark1 50%dc actions
 
       break;
+
     case 772: // cmd group is for spark2 on actions
         if( BIT_CHECK(currentStatus.testOutputs, 1) ) { digitalWrite(pinCoil2, coilHIGH); }
       break;
+
     case 773: // cmd group is for spark2 off actions
         if( BIT_CHECK(currentStatus.testOutputs, 1) ) { digitalWrite(pinCoil2, coilLOW); }
       break;
+
     case 774: // cmd group is for spark2 50%dc actions
 
       break;
+
     case 775: // cmd group is for spark3 on actions
         if( BIT_CHECK(currentStatus.testOutputs, 1) ) { digitalWrite(pinCoil3, coilHIGH); }
       break;
+
     case 776: // cmd group is for spark3 off actions
         if( BIT_CHECK(currentStatus.testOutputs, 1) ) { digitalWrite(pinCoil3, coilLOW); }
       break;
+
     case 777: // cmd group is for spark3 50%dc actions
 
       break;
+
     case 778: // cmd group is for spark4 on actions
         if( BIT_CHECK(currentStatus.testOutputs, 1) ) { digitalWrite(pinCoil4, coilHIGH); }
       break;
+
     case 779: // cmd group is for spark4 off actions
         if( BIT_CHECK(currentStatus.testOutputs, 1) ) { digitalWrite(pinCoil4, coilLOW); }
       break;
+
     case 780: // cmd group is for spark4 50%dc actions
+
+      break;
 
     default:
       break;

.travis.yml
@@ -72,11 +72,14 @@ script:
   - cd ..
   - chmod +x speeduino/misra/check_misra.sh
   - speeduino/misra/check_misra.sh
-# Do doxygen run and upload to server
+# Do doxygen run and upload to server. The upload is only performed if the commit message contains the word 'doxygen'
   - cd speeduino
   - doxygen
   - cd reference/doxygen/html
-  - find ./ -type f -exec curl --user "speeduino_doxygen@speeduino.com:$WEB_PWD" --basic -T {} "https://speeduino.com:2078/html/{}" \;
+  - |
+    if [[ $TRAVIS_COMMIT_MESSAGE == *"doxygen"* ]]; then
+      find ./ -type f -exec curl --user "speeduino_doxygen@speeduino.com:$WEB_PWD" --basic -T {} "https://speeduino.com:2078/html/{}" \;
+    fi
   #Disable the XML uploading for now as it's not used
   #- cd ../xml
   #- find ./ -type f -exec curl --user "speeduino_doxygen@speeduino.com:$WEB_PWD" --basic -T {} "https://speeduino.com:2078/xml/{}" \;

speeduino/comms.h
@@ -1,3 +1,12 @@
+/** \file comms.h
+ * @brief File for handling all serial requests 
+ * @author Josh Stewart
+ * 
+ * This file contains all the functions associated with serial comms.
+ * This includes sending of live data, sending/receiving current page data, sending CRC values of pages, receiving sensor calibration data etc
+ * 
+ */
+
 #ifndef COMMS_H
 #define COMMS_H
 //These are the page numbers that the Tuner Studio serial protocol uses to transverse the different map and config pages.
@@ -12,20 +21,17 @@
 #define canbusPage   9//Config Page 9
 #define warmupPage   10 //Config Page 10
 
-#define SERIAL_PACKET_SIZE   91 //Must match ochBlockSize in ini file
+#define SERIAL_PACKET_SIZE   91 /**< The size of the live data packet. This MUST match ochBlockSize setting in the ini file */
 
 byte currentPage = 1;//Not the same as the speeduino config page numbers
-bool isMap = true;
-unsigned long requestCount = 0; //The number of times the A command has been issued
-byte currentCommand;
-bool cmdPending = false;
-bool chunkPending = false;
-uint16_t chunkComplete = 0;
-uint16_t chunkSize = 0;
-int valueOffset; //cannot use offset as a variable name, it is a reserved word for several teensy libraries
-byte cmdGroup = 0;
-byte cmdValue = 0;
-int cmdCombined = 0;  //the cmdgroup as high byte and cmdvalue as low byte
+bool isMap = true; /**< Whether or not the currentPage contains only a 3D map that would require translation */
+unsigned long requestCount = 0; /**< The number of times the A command has been issued. This is used to track whether a reset has recently been performed on the controller */
+byte currentCommand; /**< The serial command that is currently being processed. This is only useful when cmdPending=True */
+bool cmdPending = false; /**< Whether or not a serial request has only been partially received. This occurs when a command character has been received in the serial buffer, but not all of its arguments have yet been received. If true, the active command will be stored in the currentCommand variable */
+bool chunkPending = false; /**< Whether or not the current chucnk write is complete or not */
+uint16_t chunkComplete = 0; /**< The number of bytes in a chunk write that have been written so far */
+uint16_t chunkSize = 0; /**< The complete size of the requested chunk write */
+int valueOffset; /**< THe memory offset within a given page for a value to be read from or written to. Note that we cannot use 'offset' as a variable name, it is a reserved word for several teensy libraries */
 byte tsCanId = 0;     // current tscanid requested
 
 const char pageTitles[] PROGMEM //This is being stored in the avr flash instead of SRAM which there is not very much of

speeduino/speeduino.h
@@ -1,5 +1,6 @@
 /** \file speeduino.h
  * @brief Speeduino main file containing initial setup and system loop functions 
+ * @author Josh Stewart
  * 
  * This file contains the main system loop of the Speeduino core and thus much of the logic of the fuel and ignition algorithms is contained within this
  * It is where calls to all the auxilliary control systems, sensor reads, comms etc are made
@@ -18,7 +19,7 @@ byte getAdvance();
 uint16_t calculateInjector2StartAngle(unsigned int);
 uint16_t calculateInjector3StartAngle(unsigned int);
 uint16_t calculateInjector4StartAngle(unsigned int);
-uint16_t calculateInjector4StartAngle(unsigned int);
+uint16_t calculateInjector5StartAngle(unsigned int);
 
 struct config2 configPage2;
 struct config4 configPage4;
@@ -26,58 +27,61 @@ struct config6 configPage6;
 struct config9 configPage9;
 struct config10 configPage10;
 
-uint16_t req_fuel_uS; /** The required fuel variable (As calculated by TunerStudio) in uS */
-uint16_t inj_opentime_uS; /** The injector opening time. This is set within Tuner Studio, but stored here in uS rather than mS */
-/** \name These values are a percentage of the req_fuel value that would be required for each injector channel to deliver that much fuel.
-    
-    Eg:
-    Pri injectors are 250cc
-    Sec injectors are 500cc
-    Total injector capacity = 750cc
+uint16_t req_fuel_uS; /**< The required fuel variable (As calculated by TunerStudio) in uS */
+uint16_t inj_opentime_uS; /**< The injector opening time. This is set within Tuner Studio, but stored here in uS rather than mS */
 
-    staged_req_fuel_mult_pri = 300% (The primary injectors would have to run 3x the overall PW in order to be the equivalent of the full 750cc capacity
-    staged_req_fuel_mult_sec = 150% (The secondary injectors would have to run 1.5x the overall PW in order to be the equivalent of the full 750cc capacity
+bool ignitionOn = false; /**< The current state of the ignition system (on or off) */
+bool fuelOn = false; /**< The current state of the fuel system (on or off) */
+
+byte cltCalibrationTable[CALIBRATION_TABLE_SIZE]; /**< An array containing the coolant sensor calibration values */
+byte iatCalibrationTable[CALIBRATION_TABLE_SIZE]; /**< An array containing the inlet air temperature sensor calibration values */
+byte o2CalibrationTable[CALIBRATION_TABLE_SIZE]; /**< An array containing the O2 sensor calibration values */
+
+unsigned long currentLoopTime; /**< The time the current loop started (uS) */
+unsigned long previousLoopTime; /**< The time the previous loop started (uS) */
+
+byte maxIgnOutputs = 1; /**< Used for rolling rev limiter to indicate how many total ignition channels should currently be firing */
+byte curRollingCut = 0; /**< Rolling rev limiter, current ignition channel being cut */
+byte rollingCutCounter = 0; /**< how many times (revolutions) the ignition has been cut in a row */
+uint32_t rollingCutLastRev = 0; /**< Tracks whether we're on the same or a different rev for the rolling cut */
+
+int channel1IgnDegrees; /**< The number of crank degrees until cylinder 1 is at TDC (This is obviously 0 for virtually ALL engines, but there's some weird ones) */
+int channel2IgnDegrees; /**< The number of crank degrees until cylinder 2 (and 5/6/7/8) is at TDC */
+int channel3IgnDegrees; /**< The number of crank degrees until cylinder 3 (and 5/6/7/8) is at TDC */
+int channel4IgnDegrees; /**< The number of crank degrees until cylinder 4 (and 5/6/7/8) is at TDC */
+int channel5IgnDegrees; /**< The number of crank degrees until cylinder 5 is at TDC */
+int channel6IgnDegrees; /**< The number of crank degrees until cylinder 6 is at TDC */
+int channel7IgnDegrees; /**< The number of crank degrees until cylinder 7 is at TDC */
+int channel8IgnDegrees; /**< The number of crank degrees until cylinder 8 is at TDC */
+int channel1InjDegrees; /**< The number of crank degrees until cylinder 1 is at TDC (This is obviously 0 for virtually ALL engines, but there's some weird ones) */
+int channel2InjDegrees; /**< The number of crank degrees until cylinder 2 (and 5/6/7/8) is at TDC */
+int channel3InjDegrees; /**< The number of crank degrees until cylinder 3 (and 5/6/7/8) is at TDC */
+int channel4InjDegrees; /**< The number of crank degrees until cylinder 4 (and 5/6/7/8) is at TDC */
+int channel5InjDegrees; /**< The number of crank degrees until cylinder 5 is at TDC */
+int channel6InjDegrees; /**< The number of crank degrees until cylinder 6 is at TDC */
+int channel7InjDegrees; /**< The number of crank degrees until cylinder 7 is at TDC */
+int channel8InjDegrees; /**< The number of crank degrees until cylinder 8 is at TDC */
+
+/** @name Staging
+ * These values are a percentage of the total (Combined) req_fuel value that would be required for each injector channel to deliver that much fuel.   
+ * 
+ * Eg:
+ *  - Pri injectors are 250cc
+ *  - Sec injectors are 500cc
+ *  - Total injector capacity = 750cc
+ * 
+ *  - staged_req_fuel_mult_pri = 300% (The primary injectors would have to run 3x the overall PW in order to be the equivalent of the full 750cc capacity
+ *  - staged_req_fuel_mult_sec = 150% (The secondary injectors would have to run 1.5x the overall PW in order to be the equivalent of the full 750cc capacity
 */
-//@{
+///@{
 uint16_t staged_req_fuel_mult_pri;
 uint16_t staged_req_fuel_mult_sec;
-//@}
+///@}
 
-bool ignitionOn = false; /** The current state of the ignition system (on or off) */
-bool fuelOn = false; /** The current state of the ignition system */
-
-byte cltCalibrationTable[CALIBRATION_TABLE_SIZE]; /** An array containing the coolant sensor calibration values */
-byte iatCalibrationTable[CALIBRATION_TABLE_SIZE]; /** An array containing the inlet air temperature sensor calibration values */
-byte o2CalibrationTable[CALIBRATION_TABLE_SIZE]; /** An array containing the O2 sensor calibration values */
-
-unsigned long currentLoopTime; /** The time the current loop started (uS) */
-unsigned long previousLoopTime; /** The time the previous loop started (uS) */
-
-byte maxIgnOutputs = 1; /** Used for rolling rev limiter to indicate how many total ignition channels should currently be firing */
-byte curRollingCut = 0; /** Rolling rev limiter, current ignition channel being cut */
-byte rollingCutCounter = 0; /** how many times (revolutions) the ignition has been cut in a row */
-uint32_t rollingCutLastRev = 0; /** Tracks whether we're on the same or a different rev for the rolling cut */
-
-int channel1IgnDegrees; /** The number of crank degrees until cylinder 1 is at TDC (This is obviously 0 for virtually ALL engines, but there's some weird ones) */
-int channel2IgnDegrees; /** The number of crank degrees until cylinder 2 (and 5/6/7/8) is at TDC */
-int channel3IgnDegrees; /** The number of crank degrees until cylinder 3 (and 5/6/7/8) is at TDC */
-int channel4IgnDegrees; /** The number of crank degrees until cylinder 4 (and 5/6/7/8) is at TDC */
-int channel5IgnDegrees; /** The number of crank degrees until cylinder 5 is at TDC */
-int channel6IgnDegrees; /** The number of crank degrees until cylinder 6 is at TDC */
-int channel7IgnDegrees; /** The number of crank degrees until cylinder 7 is at TDC */
-int channel8IgnDegrees; /** The number of crank degrees until cylinder 8 is at TDC */
-int channel1InjDegrees; /** The number of crank degrees until cylinder 1 is at TDC (This is obviously 0 for virtually ALL engines, but there's some weird ones) */
-int channel2InjDegrees; /** The number of crank degrees until cylinder 2 (and 5/6/7/8) is at TDC */
-int channel3InjDegrees; /** The number of crank degrees until cylinder 3 (and 5/6/7/8) is at TDC */
-int channel4InjDegrees; /** The number of crank degrees until cylinder 4 (and 5/6/7/8) is at TDC */
-int channel5InjDegrees; /** The number of crank degrees until cylinder 5 is at TDC */
-int channel6InjDegrees; /** The number of crank degrees until cylinder 6 is at TDC */
-int channel7InjDegrees; /** The number of crank degrees until cylinder 7 is at TDC */
-int channel8InjDegrees; /** The number of crank degrees until cylinder 8 is at TDC */
-
-/** \name These are the function pointers that get called to begin and end the ignition coil charging. They are required for the various spark output modes
+/** @name IgnitionCallbacks
+ * These are the function pointers that get called to begin and end the ignition coil charging. They are required for the various spark output modes
 */
-//@{
+///@{
 void (*ign1StartFunction)();
 void (*ign1EndFunction)();
 void (*ign2StartFunction)();
@@ -94,6 +98,6 @@ void (*ign7StartFunction)();
 void (*ign7EndFunction)();
 void (*ign8StartFunction)();
 void (*ign8EndFunction)();
-//@}
+///@}
 
 #endif

commit 5a7b17bccd49c294c45e79807e31d7cb6204cae6

Fix for HW test mode that broke in f41f95a60c544656b35e5bfda4e04b4ef7de53f2

reference/speeduino.ini
@@ -2126,19 +2126,19 @@ menuDialog = main
 
 	dialog = outputtestinj1, "Injector CH1", yAxis
         commandButton = "Off", cmdtestinj1off,{testactive}
-        commandButton = "50% DC", cmdtestinj150dc,{!testenabled & testactive}
+        commandButton = "50% DC", cmdtestinj150dc,{!testenabled & testactive & 0}
         commandButton = "On", cmdtestinj1on,{!testenabled & testactive}
     dialog = outputtestinj2, "Injector CH2", yAxis
         commandButton = "Off", cmdtestinj2off,{testactive}
-        commandButton = "50% DC", cmdtestinj250dc,{!testenabled &testactive}
+        commandButton = "50% DC", cmdtestinj250dc,{!testenabled &testactive & 0}
         commandButton = "On", cmdtestinj2on,{!testenabled & testactive}
     dialog = outputtestinj3, "Injector CH3", yAxis
         commandButton = "Off", cmdtestinj3off,{testactive}
-        commandButton = "50% DC", cmdtestinj350dc,{!testenabled & testactive}
+        commandButton = "50% DC", cmdtestinj350dc,{!testenabled & testactive & 0}
         commandButton = "On", cmdtestinj3on,{!testenabled & testactive}
     dialog = outputtestinj4, "Injector CH4", yAxis
         commandButton = "Off", cmdtestinj4off,{testactive}
-        commandButton = "50% DC", cmdtestinj450dc,{!testenabled & testactive}
+        commandButton = "50% DC", cmdtestinj450dc,{!testenabled & testactive & 0}
         commandButton = "On", cmdtestinj4on	,{!testenabled & testactive}
 
     dialog = outputtest_injectors, "Injector Driver Output Test", xAxis
@@ -2149,19 +2149,19 @@ menuDialog = main
 
 	dialog = outputtestspk1, "Spark CH1 ", yAxis
         commandButton = "Off", cmdtestspk1off,{testactive}
-        commandButton = "50% DC", cmdtestspk150dc,{!testenabled & testactive}
+        commandButton = "50% DC", cmdtestspk150dc,{!testenabled & testactive & 0}
         commandButton = "On", cmdtestspk1on,{!testenabled & testactive}
     dialog = outputtestspk2, "Spark CH2", yAxis
         commandButton = "Off", cmdtestspk2off,{testactive}
-        commandButton = "50% DC", cmdtestspk250dc,{!testenabled & testactive}
+        commandButton = "50% DC", cmdtestspk250dc,{!testenabled & testactive & 0}
         commandButton = "On", cmdtestspk2on,{!testenabled & testactive}
     dialog = outputtestspk3, "Spark CH3", yAxis
         commandButton = "Off", cmdtestspk3off,{testactive}
-        commandButton = "50% DC", cmdtestspk350dc,{!testenabled & testactive}
+        commandButton = "50% DC", cmdtestspk350dc,{!testenabled & testactive & 0}
         commandButton = "On", cmdtestspk3on,{!testenabled & testactive}
 	dialog = outputtestspk4, "Spark CH4", yAxis
         commandButton = "Off", cmdtestspk4off,{testactive}
-        commandButton = "50% DC", cmdtestspk450dc,{!testenabled & testactive}
+        commandButton = "50% DC", cmdtestspk450dc,{!testenabled & testactive & 0}
         commandButton = "On", cmdtestspk4on,{!testenabled & testactive}
 
     dialog = outputtest_spark, "Spark Driver Output Test", xAxis

speeduino/comms.ino
@@ -92,7 +92,9 @@ void command()
 
       if(Serial.available() >= 2)
       {
-        int cmdCombined = word(Serial.read(), Serial.read());
+        byte cmdGroup = Serial.read();
+        byte cmdValue = Serial.read();
+        int cmdCombined = word(cmdGroup, cmdValue);
         if (currentStatus.RPM == 0) { commandButtons(cmdCombined); }
 
         cmdPending = false;
@@ -1776,11 +1778,11 @@ void commandButtons(int buttonCommand)
       break;
 
     case 513: // cmd group is for injector1 on actions
-        if( BIT_CHECK(currentStatus.testOutputs, 1) ){ openInjector1(); }
+      if( BIT_CHECK(currentStatus.testOutputs, 1) ){ openInjector1(); }
       break;
 
     case 514: // cmd group is for injector1 off actions
-        if( BIT_CHECK(currentStatus.testOutputs, 1) ){ closeInjector1(); }
+      if( BIT_CHECK(currentStatus.testOutputs, 1) ){ closeInjector1(); }
       break;
 
     case 515: // cmd group is for injector1 50% dc actions

speeduino/corrections.ino
@@ -502,7 +502,6 @@ static inline int8_t correctionCLTadvance(int8_t advance)
   int8_t ignCLTValue = advance;
   //Adjust the advance based on CLT.
   int8_t advanceCLTadjust = (int16_t)(table2D_getValue(&CLTAdvanceTable, currentStatus.coolant + CALIBRATION_TEMPERATURE_OFFSET)) - 15;
-  //if(advanceCLTadjust != 0) { ignCLTValue = (advance + advanceCLTadjust/10); }
   ignCLTValue = (advance + advanceCLTadjust);
   
   return ignCLTValue;