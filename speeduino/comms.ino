ced6a2a4a10c899a76e5525e8f5cd360757bf31d
Josh Stewart <josh@noisymime.org>
2019-09-19 09:41:10 +1000

    October dev signatures

--- speeduino/comms.ino
+++ speeduino/comms.ino
@@ -238,7 +238,7 @@ void command()
       break;
 
     case 'Q': // send code version
-      Serial.print(F("speeduino 201909"));
+      Serial.print(F("speeduino 201910-dev"));
       break;
 
     case 'r': //New format for the optimised OutputChannels
@@ -268,7 +268,7 @@ void command()
       break;
 
     case 'S': // send code version
-      Serial.print(F("Speeduino 2019.09"));
+      Serial.print(F("Speeduino 2019.10-dev"));
       currentStatus.secl = 0; //This is required in TS3 due to its stricter timings
       break;
 
