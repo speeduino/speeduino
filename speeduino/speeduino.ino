19b2d49c0aa925f047672b131941379e97b1d6ad
muuris <55035659+muuris@users.noreply.github.com>
2019-11-06 22:25:01 +0200

    Modified to work with new table2Dlookup and tested.

--- speeduino/speeduino.ino
+++ speeduino/speeduino.ino
@@ -1266,12 +1266,14 @@ uint16_t PW(int REQ_FUEL, byte VE, long MAP, int corrections, int injOpen)
   if (intermediate != 0)
   {
     
-    //Small pulsewidth correction. If option is enabled, do the value lookup.
-    if (configPage2.smallPwEnabled) {
-      byte tmp_pw_byte = (unsigned long) intermediate/10UL;                     //Small pw values are stored in ms*100, so need to divide intermediate by 10 for lookup
-      unsigned long lowPwValue = table2D_getValue(&smallPwTable, tmp_pw_byte);
+    //Small pulsewidth correction. If option is enabled and intermediate is below max x axis value in table, do the value lookup.
+    if ((configPage2.smallPwEnabled) && ((unsigned long) intermediate < ((unsigned long) table2D_getAxisValue(&smallPwTable, 3) * 10UL))) {    
+      byte lookup = (unsigned long) intermediate/10UL;                     //Small pw values are stored in ms*100, so need to divide intermediate by 10 for lookup
+      unsigned long lowPwValue = (unsigned long) table2D_getValue(&smallPwTable, lookup);
       lowPwValue = (unsigned long) lowPwValue * 10UL;                           //Multiply by 10 to get uS
-      intermediate = lowPwValue;
+      if ((lowPwValue > 0) && (lowPwValue < ((unsigned long) table2D_getAxisValue(&smallPwTable, 3) * 10UL))) { //Sanity check
+        intermediate = lowPwValue;
+      }
     }
 
     //If intermeditate is not 0, we need to add the opening time (0 typically indicates that one of the full fuel cuts is active)
