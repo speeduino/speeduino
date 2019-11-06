b24aa0c15db36acb90b6ce1a40ab8039d7197843
muuris <55035659+muuris@users.noreply.github.com>
2019-11-06 10:21:41 +0200

    Added small pulsewidth correction
    Added possibility to adjust actual injector low range (0..2,5 ms) pulsewidth in relation to calculated pulsewidth. Ie for calculated 1,0 ms value a specific injector actually need 1,5 ms pulse to give calculated amount of fuel.

--- speeduino/globals.h
+++ speeduino/globals.h
@@ -292,6 +292,7 @@ struct table2D flexAdvTable;   //6 bin flex fuel correction table for timing adv
 struct table2D flexBoostTable; //6 bin flex fuel correction table for boost adjustments (2D)
 struct table2D knockWindowStartTable;
 struct table2D knockWindowDurationTable;
+struct table2D smallPwTable; //4 bin small pw map (2D)
 
 //These are for the direct port manipulation of the injectors, coils and aux outputs
 volatile PORT_TYPE *inj1_pin_port;
@@ -599,7 +600,10 @@ struct config2 {
   uint16_t EMAPMax;
 
   byte fanWhenOff : 1;      // Only run fan when engine is running
-  byte fanUnused : 7;
+  byte fanUnused_1_5 : 5;
+  byte smallPwEnabled : 1; //Small pw correction, enable bit.  
+  byte fanUnused_7 : 1;
+
   byte asePct[4];  //Afterstart enrichment (%)
   byte aseCount[4]; //Afterstart enrichment cycles. This is the number of ignition cycles that the afterstart enrichment % lasts for
   byte aseBins[4]; //Afterstart enrichment temp axis
@@ -614,7 +618,9 @@ struct config2 {
   byte IdleAdvDelay : 5;
   byte idleAdvRPM;
   byte idleAdvTPS;
-  byte unused2_95[33];
+  byte smallPwValues[4];   //Small pw correction, new values. Stored in ms*100.
+  byte smallPwBins[4];     //Small pw correction, original values. Stored in ms*100.
+  byte unused2_100[28];
 
 #if defined(CORE_AVR)
   };
