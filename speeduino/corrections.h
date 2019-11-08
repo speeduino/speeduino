892e05d2fccf22da861425246ffe25723945ef1e
iLeeeZi <ilari.vehniainen@hotmail.fi>
2019-11-08 07:40:53 +0200

    Add idle ignition timing correction feature (#275)
    * Add idle advance and idle switch settings
    
    Add idle RPM target based ignition timing correction
    
    * Change few names to avoid confusion
    
    changed idle switch to closed throttle position sensor  (CTPS) and moved CTPS settings under idle advance settings dialog for now

--- speeduino/corrections.h
+++ speeduino/corrections.h
@@ -28,6 +28,7 @@ static inline int8_t correctionFlexTiming(int8_t);
 static inline int8_t correctionIATretard(int8_t);
 static inline int8_t correctionBaro(int8_t);
 static inline int8_t correctionCLTadvance(int8_t);
+static inline int8_t correctionIdleAdvance(int8_t);
 static inline int8_t correctionSoftRevLimit(int8_t);
 static inline int8_t correctionNitrous(int8_t);
 static inline int8_t correctionSoftLaunch(int8_t);
