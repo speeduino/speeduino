a611e00f57aa308daa8b0e342a086f8f05caae3f
Josh Stewart <josh@noisymime.org>
2020-03-16 11:58:01 +1100

    Add tests for table lookups

---
+++ test/tests/test_misc/tests_tables.h
@@ -0,0 +1,9 @@
+extern void testTables();
+void setup_FuelTable(void);
+void test_tableLookup_50pct(void);
+void test_tableLookup_exact1Axis(void);
+void test_tableLookup_exact2Axis(void);
+void test_tableLookup_overMaxX(void);
+void test_tableLookup_overMaxY(void);
+void test_tableLookup_underMinX(void);
+void test_tableLookup_underMinY(void);
