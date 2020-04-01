5c166de2f70dc84904b70f39e68fa7e04333d3b2
Josh Stewart <josh@noisymime.org>
2020-04-01 12:15:34 +1100

    Cleaner handling in the wasted spark functions

--- speeduino/scheduledIO.ino
+++ speeduino/scheduledIO.ino
@@ -65,15 +65,15 @@
   inline void endCoil8Charge() { coil8Low(); }
 
   //The below 3 calls are all part of the rotary ignition mode
-  inline void beginTrailingCoilCharge() { digitalWrite(pinCoil2, coilHIGH); }
-  inline void endTrailingCoilCharge1() { digitalWrite(pinCoil2, coilLOW); *ign3_pin_port |= ign3_pin_mask; } //Sets ign3 (Trailing select) high
-  inline void endTrailingCoilCharge2() { digitalWrite(pinCoil2, coilLOW); *ign3_pin_port &= ~(ign3_pin_mask); } //sets ign3 (Trailing select) low
+  inline void beginTrailingCoilCharge() { coil2High(); }
+  inline void endTrailingCoilCharge1() { coil2Low(); coil3High(); } //Sets ign3 (Trailing select) high
+  inline void endTrailingCoilCharge2() { coil2Low(); coil3Low(); } //sets ign3 (Trailing select) low
 
   //As above but for ignition (Wasted COP mode)
-  void beginCoil1and3Charge() { digitalWrite(pinCoil1, coilHIGH); digitalWrite(pinCoil3, coilHIGH); tachoOutputFlag = READY; }
-  void endCoil1and3Charge()   { digitalWrite(pinCoil1, coilLOW);  digitalWrite(pinCoil3, coilLOW);  }
-  void beginCoil2and4Charge() { digitalWrite(pinCoil2, coilHIGH); digitalWrite(pinCoil4, coilHIGH); tachoOutputFlag = READY; }
-  void endCoil2and4Charge()   { digitalWrite(pinCoil2, coilLOW);  digitalWrite(pinCoil4, coilLOW);  }
+  void beginCoil1and3Charge() { beginCoil1Charge(); beginCoil3Charge(); }
+  void endCoil1and3Charge()   { endCoil1Charge();  endCoil3Charge(); }
+  void beginCoil2and4Charge() { beginCoil2Charge(); beginCoil4Charge(); }
+  void endCoil2and4Charge()   { endCoil2Charge(); endCoil4Charge(); }
 #endif
 
 void nullCallback() { return; }
