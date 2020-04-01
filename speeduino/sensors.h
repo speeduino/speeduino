fc40cc24db4339a037b440124727223bc6832944
iLeeeZi <ilari.vehniainen@hotmail.fi>
2020-04-02 02:42:26 +0300

    prevent priming pulse if flood clear is active (#289)

--- speeduino/sensors.h
+++ speeduino/sensors.h
@@ -58,7 +58,7 @@ byte cltErrorCount = 0;
 static inline void instanteneousMAPReading() __attribute__((always_inline));
 static inline void readMAP() __attribute__((always_inline));
 void initialiseADC();
-void readTPS();
+void readTPS(bool=true); //Allows the option to override the use of the filter
 void readO2_2();
 void flexPulse();
 uint16_t readAuxanalog(uint8_t analogPin);
