d5bc11e006503a3c6f07260e685c153ab9c96976
Josh Stewart <josh@noisymime.org>
2020-07-14 14:05:07 +1000

    Fix bug with the delayed inj priming that could cause lockup

--- speeduino/scheduler.ino
+++ speeduino/scheduler.ino
@@ -765,27 +765,27 @@ extern void beginInjectorPriming()
   if( (primingValue > 0) && (currentStatus.TPS < configPage4.floodClear) )
   {
     primingValue = primingValue * 100 * 5; //to acheive long enough priming pulses, the values in tuner studio are divided by 0.5 instead of 0.1, so multiplier of 5 is required.
-    setFuelSchedule1(100, primingValue); 
+    if ( channel1InjEnabled == true ) { setFuelSchedule1(100, primingValue); }
 #if (INJ_CHANNELS >= 2)
-    if ( configPage2.nInjectors >= 2 ) { setFuelSchedule2(100, primingValue); }
+    if ( channel2InjEnabled == true ) { setFuelSchedule2(100, primingValue); }
 #endif
 #if (INJ_CHANNELS >= 3)
-    if ( configPage2.nInjectors >= 3 ) { setFuelSchedule3(100, primingValue); }
+    if ( channel3InjEnabled == true ) { setFuelSchedule3(100, primingValue); }
 #endif
 #if (INJ_CHANNELS >= 4)
-    if ( configPage2.nInjectors >= 4 ) { setFuelSchedule4(100, primingValue); }
+    if ( channel4InjEnabled == true ) { setFuelSchedule4(100, primingValue); }
 #endif
 #if (INJ_CHANNELS >= 5)
-    if ( configPage2.nInjectors >= 5 ) { setFuelSchedule5(100, primingValue); }
+    if ( channel5InjEnabled == true ) { setFuelSchedule5(100, primingValue); }
 #endif
 #if (INJ_CHANNELS >= 6)
-    if ( configPage2.nInjectors >= 6 ) { setFuelSchedule6(100, primingValue); }
+    if ( channel6InjEnabled == true ) { setFuelSchedule6(100, primingValue); }
 #endif
 #if (INJ_CHANNELS >= 7)
-    if ( configPage2.nInjectors >= 7 ) { setFuelSchedule7(100, primingValue); }
+    if ( channel7InjEnabled == true) { setFuelSchedule7(100, primingValue); }
 #endif
 #if (INJ_CHANNELS >= 8)
-    if ( configPage2.nInjectors >= 8 ) { setFuelSchedule8(100, primingValue); }
+    if ( channel8InjEnabled == true ) { setFuelSchedule8(100, primingValue); }
 #endif
   }
 }
