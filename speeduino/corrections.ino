6006831e47e60d790fded442f9f7a0b778cbb86d
sandmanWAjeep <51330596+sandmanWAjeep@users.noreply.github.com>
2020-09-02 12:30:49 -0500

    Update corrections.ino

--- speeduino/corrections.ino
+++ speeduino/corrections.ino
@@ -713,6 +713,10 @@ int8_t correctionIdleAdvance(int8_t advance)
   if( (configPage2.idleAdvEnabled >= 1) && (currentStatus.runSecs >= configPage2.IdleAdvDelay))
   {
     currentStatus.CLIdleTarget = (byte)table2D_getValue(&idleTargetTable, currentStatus.coolant + CALIBRATION_TEMPERATURE_OFFSET); //All temps are offset by 40 degrees
+    
+     /*added this line to change target idle when idle up is active - Sanders */
+    if(currentStatus.idleUpActive == true){currentStatus.CLIdleTarget += 110;}
+    
     int idleRPMdelta = (currentStatus.CLIdleTarget - (currentStatus.RPM / 10) ) + 50;
     // Limit idle rpm delta between -500rpm - 500rpm
     if(idleRPMdelta > 100) { idleRPMdelta = 100; }
