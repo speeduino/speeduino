1189bbb2f0e1e25d29aac1ae6f5afa6d0e04d17d
sandmanWAjeep <51330596+sandmanWAjeep@users.noreply.github.com>
2020-09-08 12:24:25 -0500

    back to original

--- speeduino/idle.ino
+++ speeduino/idle.ino
@@ -216,8 +216,6 @@ void idleControl()
       {
         //Currently cranking. Use the cranking table
         currentStatus.idleDuty = table2D_getValue(&iacCrankDutyTable, currentStatus.coolant + CALIBRATION_TEMPERATURE_OFFSET); //All temps are offset by 40 degrees
-        digitalWrite(pinFan2, LOW);  // Sanders  make sure AC is off if cranking
-        digitalWrite(pinACrelay, LOW);
       }
       else
       {
@@ -235,29 +233,8 @@ void idleControl()
           currentStatus.idleDuty = table2D_getValue(&iacPWMTable, currentStatus.coolant + CALIBRATION_TEMPERATURE_OFFSET); //All temps are offset by 40 degrees
         }
       }
-//Sanders
-      if(currentStatus.idleUpActive == true) { currentStatus.idleDuty += configPage2.idleUpAdder; 
-      if (idleCounter < 20) {
-            idleCounter ++;
-           }
-           if (idleCounter == 12){
-           digitalWrite(pinACrelay, HIGH);
-           }
-           if (idleCounter == 19){
-           digitalWrite(pinFan2, HIGH);           
-           }
-        }
-       
-        else if (currentStatus.idleUpActive == false)
-        {          
-          if (idleCounter != 0){
-            digitalWrite(pinFan2, LOW);
-            digitalWrite(pinACrelay, LOW);
-            idleCounter = 0;
-          }     
-        } 
-      
 
+      if(currentStatus.idleUpActive == true) { currentStatus.idleDuty += configPage2.idleUpAdder; } //Add Idle Up amount if active
       if( currentStatus.idleDuty > 100 ) { currentStatus.idleDuty = 100; } //Safety Check
       if( currentStatus.idleDuty == 0 ) 
       { 
@@ -286,32 +263,6 @@ void idleControl()
       else
       {
         currentStatus.CLIdleTarget = (byte)table2D_getValue(&iacClosedLoopTable, currentStatus.coolant + CALIBRATION_TEMPERATURE_OFFSET); //All temps are offset by 40 degrees
-        
-        //added this line to increase idle speed when AC is on
-        if(currentStatus.idleUpActive == true) 
-        { 
-          currentStatus.CLIdleTarget += 100; 
-           if (idleCounter < 20) {
-            idleCounter ++;
-           }
-           //if (idleCounter == 12){
-           if ((idleCounter & 5) == 1){
-           digitalWrite(pinACrelay, HIGH);
-           }
-           if ((idleCounter & 10) == 1){
-           digitalWrite(pinFan2, HIGH);           
-           }
-        }
-
-         else if (currentStatus.idleUpActive == false)
-        {          
-          //if (idleCounter != 0){
-            digitalWrite(pinFan2, LOW);
-            digitalWrite(pinACrelay, LOW);
-            //idleCounter = 0;
-          //}     
-        } 
-        
         idle_cl_target_rpm = (uint16_t)currentStatus.CLIdleTarget * 10; //Multiply the byte target value back out by 10
         if( (idleCounter & 31) == 1) { idlePID.SetTunings(configPage6.idleKP, configPage6.idleKI, configPage6.idleKD); } //This only needs to be run very infrequently, once every 32 calls to idleControl(). This is approx. once per second
 
