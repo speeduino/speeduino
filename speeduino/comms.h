ced7d32aa0c6edff2c3a5be3b90797beecfc9f04
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2021-03-31 07:02:50 -0500

    Rename comms.ino to comms.cpp
    Provides better encapsulation of non-global
    data & functions.
    
    INO files are all mashed together by some
    custom process. So everything becomes global and
    static functions/variables aren't really private to
     the translation unit. Thus breaking encapsulation :-(

--- speeduino/comms.h
+++ speeduino/comms.h
@@ -35,22 +35,22 @@
 #define SD_RTC_READ_LENGTH  0x0800
 
 
-byte currentPage = 1;//Not the same as the speeduino config page numbers
-bool isMap = true; /**< Whether or not the currentPage contains only a 3D map that would require translation */
-unsigned long requestCount = 0; /**< The number of times the A command has been issued. This is used to track whether a reset has recently been performed on the controller */
-byte currentCommand; /**< The serial command that is currently being processed. This is only useful when cmdPending=True */
-bool cmdPending = false; /**< Whether or not a serial request has only been partially received. This occurs when a command character has been received in the serial buffer, but not all of its arguments have yet been received. If true, the active command will be stored in the currentCommand variable */
-bool chunkPending = false; /**< Whether or not the current chucnk write is complete or not */
-uint16_t chunkComplete = 0; /**< The number of bytes in a chunk write that have been written so far */
-uint16_t chunkSize = 0; /**< The complete size of the requested chunk write */
-int valueOffset; /**< THe memory offset within a given page for a value to be read from or written to. Note that we cannot use 'offset' as a variable name, it is a reserved word for several teensy libraries */
-byte tsCanId = 0;     // current tscanid requested
-byte inProgressOffset;
-byte inProgressLength;
-uint32_t inProgressCompositeTime;
-bool serialInProgress = false;
-bool toothLogSendInProgress = false;
-bool compositeLogSendInProgress = false;
+extern byte currentPage;//Not the same as the speeduino config page numbers
+extern bool isMap; /**< Whether or not the currentPage contains only a 3D map that would require translation */
+extern unsigned long requestCount; /**< The number of times the A command has been issued. This is used to track whether a reset has recently been performed on the controller */
+extern byte currentCommand; /**< The serial command that is currently being processed. This is only useful when cmdPending=True */
+extern bool cmdPending; /**< Whether or not a serial request has only been partially received. This occurs when a command character has been received in the serial buffer, but not all of its arguments have yet been received. If true, the active command will be stored in the currentCommand variable */
+extern bool chunkPending; /**< Whether or not the current chucnk write is complete or not */
+extern uint16_t chunkComplete; /**< The number of bytes in a chunk write that have been written so far */
+extern uint16_t chunkSize; /**< The complete size of the requested chunk write */
+extern int valueOffset; /**< THe memory offset within a given page for a value to be read from or written to. Note that we cannot use 'offset' as a variable name, it is a reserved word for several teensy libraries */
+extern byte tsCanId;     // current tscanid requested
+extern byte inProgressOffset;
+extern byte inProgressLength;
+extern uint32_t inProgressCompositeTime;
+extern bool serialInProgress;
+extern bool toothLogSendInProgress;
+extern bool compositeLogSendInProgress;
 
 void command();//This is the heart of the Command Line Interpeter.  All that needed to be done was to make it human readable.
 void sendValues(uint16_t, uint16_t,byte, byte);
