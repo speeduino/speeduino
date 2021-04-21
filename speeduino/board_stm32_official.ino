02cb7bebd66e59eaef353eaa29520c8eaebc1bf7
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2021-04-20 23:36:27 -0500

    Isolate table3D member access in comms.ino (#545)
    * fix!: fix ODR violations
    
    * refactor: move page specific code into a separate CPP file
    
    * refactor: page getter/setter share mapping logic
    
    Extract common page-to-entity mapping logic from
    getPageValue() & setPageValue() - place in map_page_offset_to_entity()
    and share.
    
    * performance: optimize CRC calc
    
    Calculate page CRC by iterating over entities & tables.
    
    * CRC table calculation - use table iterator
    
    * refactor: use iterators for sendPage()
    
    Re-implement sendPage() using page & table iterators
    Future proof & fast
    
    * refactor: sendPageASCII()
    
    Pull put shared code into functions.
    Use table iterator
    
    * refactor: use shared axis factor
    
    This puts the axis factor usage in one place
    
    * refactor: encapsulate page size & count
    
    Added getPageCount() & getPageSize()
    
    * Added static_assert for all pages.
    
    * Remove C++ language elements
    
    namesapces, scope resolution, enum struct
    
    * Rename comms.ino to comms.cpp
    
    Provides better encapsulation of non-global
    data & functions.
    
    INO files are all mashed together by some
    custom process. So everything becomes global and
    static functions/variables aren't really private to
     the translation unit. Thus breaking encapsulation :-(

--- speeduino/board_stm32_official.ino
+++ speeduino/board_stm32_official.ino
@@ -6,6 +6,56 @@
 #include "scheduler.h"
 #include "HardwareTimer.h"
 
+#if defined(STM32F407xx) || defined(STM32F103xB) || defined(STM32F405xx)
+#define NATIVE_CAN_AVAILABLE
+//This activates CAN1 interface on STM32, but it's named as Can0, because that's how Teensy implementation is done
+STM32_CAN Can0 (_CAN1,DEF);
+#endif
+
+#if defined(SRAM_AS_EEPROM)
+    BackupSramAsEEPROM EEPROM;
+#elif defined(USE_SPI_EEPROM)
+    SPIClass SPI_for_flash(PB5, PB4, PB3); //SPI1_MOSI, SPI1_MISO, SPI1_SCK
+ 
+    //windbond W25Q16 SPI flash EEPROM emulation
+    EEPROM_Emulation_Config EmulatedEEPROMMconfig{255UL, 4096UL, 31, 0x00100000UL};
+    Flash_SPI_Config SPIconfig{USE_SPI_EEPROM, SPI_for_flash};
+    SPI_EEPROM_Class EEPROM(EmulatedEEPROMMconfig, SPIconfig);
+#elif defined(FRAM_AS_EEPROM) //https://github.com/VitorBoss/FRAM
+    #if defined(STM32F407xx)
+      FramClass EEPROM(PB5, PB4, PB3, PB0); /*(mosi, miso, sclk, ssel, clockspeed) 31/01/2020*/
+    #else
+      FramClass EEPROM(PB15, PB14, PB13, PB12); //Blue/Black Pills
+    #endif
+#elif defined(STM32F7xx)
+  #if defined(DUAL_BANK)
+    EEPROM_Emulation_Config EmulatedEEPROMMconfig{4UL, 131072UL, 2047UL, 0x08120000UL};
+  #else
+    EEPROM_Emulation_Config EmulatedEEPROMMconfig{2UL, 262144UL, 4095UL, 0x08180000UL};
+  #endif
+    InternalSTM32F7_EEPROM_Class EEPROM(EmulatedEEPROMMconfig);
+#elif defined(STM32F401xC)
+    EEPROM_Emulation_Config EmulatedEEPROMMconfig{2UL, 131072UL, 4095UL, 0x08040000UL};
+    InternalSTM32F4_EEPROM_Class EEPROM(EmulatedEEPROMMconfig);
+#else //default case, internal flash as EEPROM for STM32F4
+    EEPROM_Emulation_Config EmulatedEEPROMMconfig{4UL, 131072UL, 2047UL, 0x08080000UL};
+    InternalSTM32F4_EEPROM_Class EEPROM(EmulatedEEPROMMconfig);
+#endif
+
+
+HardwareTimer Timer1(TIM1);
+HardwareTimer Timer2(TIM2);
+HardwareTimer Timer3(TIM3);
+HardwareTimer Timer4(TIM4);
+#if !defined(ARDUINO_BLUEPILL_F103C8) && !defined(ARDUINO_BLUEPILL_F103CB) //F103 just have 4 timers
+HardwareTimer Timer5(TIM5);
+#if defined(TIM11)
+HardwareTimer Timer11(TIM11);
+#elif defined(TIM7)
+HardwareTimer Timer11(TIM7);
+#endif
+#endif
+
 STM32RTC& rtc = STM32RTC::getInstance();
 
   void initBoard()
--- speeduino/cancomms.ino
+++ speeduino/cancomms.ino
@@ -21,6 +21,34 @@ sendcancommand is called when a command is to be sent either to serial3
 #include "errors.h"
 #include "utilities.h"
 
+uint8_t currentsecondserialCommand;
+uint8_t currentCanPage = 1;//Not the same as the speeduino config page numbers
+uint8_t nCanretry = 0;      //no of retrys
+uint8_t cancmdfail = 0;     //command fail yes/no
+uint8_t canlisten = 0;
+uint8_t Lbuffer[8];         //8 byte buffer to store incomng can data
+uint8_t Gdata[9];
+uint8_t Glow, Ghigh;
+bool canCmdPending = false;
+
+#if ( defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__) )
+  #define CANSerial_AVAILABLE
+  HardwareSerial &CANSerial = Serial3;
+#elif defined(CORE_STM32)
+  #define CANSerial_AVAILABLE
+  #ifndef Serial2
+    #define Serial2 Serial1
+  #endif
+  #if defined(STM32GENERIC) // STM32GENERIC core
+    SerialUART &CANSerial = Serial2;
+  #else //libmaple core aka STM32DUINO
+    HardwareSerial &CANSerial = Serial2;
+  #endif
+#elif defined(CORE_TEENSY)
+  #define CANSerial_AVAILABLE
+  HardwareSerial &CANSerial = Serial2;
+#endif
+
 void secondserial_Command()
 {
   #if defined(CANSerial_AVAILABLE)
