1e12a2f00e819574f76e7d851b39c66c61787176
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2021-09-13 18:18:10 -0500

    Remove repeated #defines from cancomms.ino
    They are already in cancomms.h

--- speeduino/cancomms.ino
+++ speeduino/cancomms.ino
@@ -32,20 +32,14 @@ uint8_t Glow, Ghigh;
 bool canCmdPending = false;
 
 #if ( defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__) )
-  #define CANSerial_AVAILABLE
   HardwareSerial &CANSerial = Serial3;
 #elif defined(CORE_STM32)
-  #define CANSerial_AVAILABLE
-  #ifndef Serial2
-    #define Serial2 Serial1
-  #endif
   #if defined(STM32GENERIC) // STM32GENERIC core
     SerialUART &CANSerial = Serial2;
   #else //libmaple core aka STM32DUINO
     HardwareSerial &CANSerial = Serial2;
   #endif
 #elif defined(CORE_TEENSY)
-  #define CANSerial_AVAILABLE
   HardwareSerial &CANSerial = Serial2;
 #endif
 
