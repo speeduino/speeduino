2ae3275a2fc28a493645d8458d812b66fbca5ee5
Pasi Kemppainen <48950874+pazi88@users.noreply.github.com>
2021-10-05 12:59:55 +0300

    Minor fixes

--- speeduino/updates.ino
+++ speeduino/updates.ino
@@ -14,7 +14,7 @@
 
 void doUpdates()
 {
-  #define CURRENT_DATA_VERSION    18
+  #define CURRENT_DATA_VERSION    19
   //Only the latest updat for small flash devices must be retained
    #ifndef SMALL_FLASH_MODE
 
@@ -509,12 +509,12 @@ void doUpdates()
     storeEEPROMVersion(18);
   }
 
-  if(EEPROM.read(EEPROM_DATA_VERSION) == 18)
+  if(readEEPROMVersion() == 18)
   {
     configPage2.fanEnable = configPage6.fanUnused; // PWM Fan mode added, but take the previous setting of Fan in use.
 
     writeAllConfig();
-    EEPROM.write(EEPROM_DATA_VERSION, 18);
+    storeEEPROMVersion(19);
   }
 
   //Final check is always for 255 and 0 (Brand new arduino)
