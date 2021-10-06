ee3b2c2d54b9099e3f63d286ff241d08d3e8fab8
Fera31 <66860635+Fera31@users.noreply.github.com>
2021-10-06 20:57:36 -0300

    Update sensors.h

--- speeduino/sensors.h
+++ speeduino/sensors.h
@@ -85,6 +85,7 @@ uint16_t getSpeed();
 byte getGear();
 byte getFuelPressure();
 byte getOilPressure();
+byte getVarLaunch(); 
 uint16_t readAuxanalog(uint8_t analogPin);
 uint16_t readAuxdigital(uint8_t digitalPin);
 void readCLT(bool=true); //Allows the option to override the use of the filter
