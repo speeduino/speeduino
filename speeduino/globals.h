2b1ddb9b629daa9afb19e21124fda3f440c3ee9c
Fera31 <66860635+Fera31@users.noreply.github.com>
2021-10-06 21:17:46 -0300

    Update globals.h

--- speeduino/globals.h
+++ speeduino/globals.h
@@ -728,6 +728,7 @@ struct statuses {
   long vvt2Duty; //Has to be a long for PID calcs (CL VVT control)
   byte outputsStatus;
   byte TS_SD_Status; //TunerStudios SD card status
+  byte VarLaunch; //variable launch
 };
 
 /** Page 2 of the config - mostly variables that are required for fuel.
