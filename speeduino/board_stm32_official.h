0322d31cccdcdbc62666e8a272bee08b484c867b
Tjeerd <33102280+Tjeerdie@users.noreply.github.com>
2021-12-22 04:53:36 +0100

    Added function: Reset eeprom with key (#657)
    * Add eeprom reset with key
    
    * small improvements
    
    * increased robustness against accidental erase of EEPROM.
    
    Co-authored-by: Tjeerd <tjeerdie@users.noreply.github.com>

--- speeduino/board_stm32_official.h
+++ speeduino/board_stm32_official.h
@@ -27,6 +27,10 @@
 #define micros_safe() micros() //timer5 method is not used on anything but AVR, the micros_safe() macro is simply an alias for the normal micros()
 #define TIMER_RESOLUTION 4
 
+#if defined(USER_BTN) 
+  #define EEPROM_RESET_PIN USER_BTN //onboard key0 for black STM32F407 boards and blackpills, keep pressed during boot to reset eeprom
+#endif
+
 #ifdef SD_LOGGING
 #define RTC_ENABLED
 #endif
