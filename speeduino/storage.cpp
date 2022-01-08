97954d667fca6bc4041182ba773b2ef4cbc0554f
Tjeerd <tjeerdie@users.noreply.github.com>
2022-01-08 23:06:56 +0100

    Fix full tune/large chucks of data for STM32. Next to this fix the core needs to be adjusted. Change line 57 in cdc_queue.h to: #define CDC_RECEIVE_QUEUE_BUFFER_SIZE ((uint16_t)(CDC_QUEUE_MAX_PACKET_SIZE * 8))

--- speeduino/storage.cpp
+++ speeduino/storage.cpp
@@ -13,7 +13,9 @@ A full copy of the license may be found in the projects root directory
 #include "pages.h"
 
 //The maximum number of write operations that will be performed in one go. If we try to write to the EEPROM too fast (Each write takes ~3ms) then the rest of the system can hang)
-#if defined(CORE_STM32) || defined(CORE_TEENSY) & !defined(USE_SPI_EEPROM)
+#if defined(CORE_STM32) & !defined(USE_SPI_EEPROM)
+#define EEPROM_MAX_WRITE_BLOCK 256
+#elif defined(CORE_TEENSY) & !defined(USE_SPI_EEPROM)
 #define EEPROM_MAX_WRITE_BLOCK 64
 #else
 #define EEPROM_MAX_WRITE_BLOCK 30
