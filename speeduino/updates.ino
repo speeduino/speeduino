ec9dff1a44efdaa70c2b48cbc3051ffac87d28dc
Benas <bbrazdziunas@gavea.co.uk>
2022-02-02 18:03:58 +0000

    Coolant/RPM Protection

--- speeduino/updates.ino
+++ speeduino/updates.ino
@@ -403,7 +403,8 @@ void doUpdates()
     configPage10.oilPressureProtEnbl = false;
     configPage10.oilPressureEnable = false;
     configPage10.fuelPressureEnable = false;
-
+    configPage9.coolantProtEnbl = false;
+    
     //wmi
     configPage10.wmiEnabled = 0;
     configPage10.wmiMode = 0;
