ae8bfc731075598045ea3f8ede3f4bea9940c13b
Corey-Harding <91717711+Corey-Harding@users.noreply.github.com>
2022-04-17 00:38:11 -0400

    Update sensors.ino
    Change Auxinpina from 127 to 63 and remove spaces from Auxpinb for consistency.

--- speeduino/sensors.ino
+++ speeduino/sensors.ino
@@ -79,7 +79,7 @@ void initialiseADC()
             || (((configPage9.enable_secondarySerial == 0) && ( (configPage9.enable_intcan == 1) && (configPage9.intcan_available == 0) )) && (configPage9.caninput_sel[currentStatus.current_caninchannel]&3) == 2)  
             || (((configPage9.enable_secondarySerial == 0) && (configPage9.enable_intcan == 0)) && ((configPage9.caninput_sel[currentStatus.current_caninchannel]&3) == 2)))  
     {  //if current input channel is enabled as analog local pin check caninput_selxb(bits 2:3) with &12 and caninput_selxa(bits 0:1) with &3
-      byte pinNumber = (configPage9.Auxinpina[currentStatus.current_caninchannel]&127);
+      byte pinNumber = (configPage9.Auxinpina[currentStatus.current_caninchannel]&63);
       if( pinIsUsed(pinNumber) )
       {
         //Do nothing here as the pin is already in use.
@@ -97,7 +97,7 @@ void initialiseADC()
             || (((configPage9.enable_secondarySerial == 0) && ( (configPage9.enable_intcan == 1) && (configPage9.intcan_available == 0) )) && (configPage9.caninput_sel[currentStatus.current_caninchannel]&3) == 3)
             || (((configPage9.enable_secondarySerial == 0) && (configPage9.enable_intcan == 0)) && ((configPage9.caninput_sel[currentStatus.current_caninchannel]&3) == 3)))
     {  //if current input channel is enabled as digital local pin check caninput_selxb(bits 2:3) wih &12 and caninput_selxa(bits 0:1) with &3
-       byte pinNumber = (configPage9.Auxinpinb[currentStatus.current_caninchannel]&63) + 1;
+       byte pinNumber = (configPage9.Auxinpinb[currentStatus.current_caninchannel]&63)+1;
        if( pinIsUsed(pinNumber) )
        {
          //Do nothing here as the pin is already in use.
