bde14c9e9272f9ec8a207e20b7e8e28deee4f3e4
mbbrinkman <michaelbbrinkman@gmail.com>
2022-04-25 03:25:36 -0500

    Update corrections.ino

--- speeduino/corrections.ino
+++ speeduino/corrections.ino
@@ -36,7 +36,7 @@ long PID_O2, PID_output, PID_AFRTarget;
 * Needs to be global as it maintains state outside of each function call.
 * Comes from Arduino (?) PID library.
 */
-PID egoPID(&PID_O2, &PID_output, &PID_AFRTarget, configPage6.egoKP, configPage6.egoKI, configPage6.egoKD, REVERSE);
+PID egoPID(&PID_O2, &PID_output, &PID_AFRTarget, configPage6.egoKP, configPage6.egoKI, configPage6.egoKD, DIRECT);
 
 int MAP_rateOfChange;
 int TPS_rateOfChange;
