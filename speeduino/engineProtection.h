6c269ff8dad64666d5748dc2f7a09362891f0a7b
larmicfre <49905831+larmicfre@users.noreply.github.com>
2022-05-30 02:47:34 +0200

    Moved variables to prevent ODR

--- speeduino/engineProtection.h
+++ speeduino/engineProtection.h
@@ -2,9 +2,6 @@
 #define HARD_REV_FIXED    1
 #define HARD_REV_COOLANT  2
 
-bool afrProtectCountEnabled = false;
-unsigned long afrProtectCount;
-
 byte checkEngineProtect();
 byte checkRevLimit();
 byte checkBoostLimit();
