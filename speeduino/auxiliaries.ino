a40971dde16aaa95377c91c66880088d20ab88ee
rafolg <rafaelgenel@yahoo.com.au>
2022-09-15 09:58:37 +1000

    Configuration of PWM for idle, boost and vvt  for Teensy4.1 (Take2) (#929)
    * Fix VSS not resetting to zero
    
    Reinstated car stop check to avoid  speed not going back to zero.
    
    * Better logic for gear detection
    
    Previous gear detection logic had gears constantly lagging 1 shift behind.
    
    * Update sensors.h
    
    * Update sensors.ino
    
    * Update sensors.ino
    
    * Update sensors.h
    
    * Update board_teensy41.h
    
    Pit timers implementation for idle, boost and vvt
    
    * Update board_teensy41.ino
    
    Pit timers implementation for idle, boost and vvt
    
    * Update board_teensy41.h
    
    Error in fuel5-8 timer disable now corrected
    
    * Update auxiliaries.ino
    
    Enable vvt PWM for Teensy 4.1
    
    * Update idle.ino
    
    * Update board_teensy41.h
    
    For some reason the PIT_LDVAL compares were not showing on  the previous PR
    
    * Update auxiliaries.ino

--- speeduino/auxiliaries.ino
+++ speeduino/auxiliaries.ino
@@ -417,8 +417,10 @@ void initialiseAuxPWM()
 
     #if defined(CORE_AVR)
       vvt_pwm_max_count = 1000000L / (16 * configPage6.vvtFreq * 2); //Converts the frequency in Hz to the number of ticks (at 16uS) it takes to complete 1 cycle. Note that the frequency is divided by 2 coming from TS to allow for up to 512hz
-    #elif defined(CORE_TEENSY)
+    #elif defined(CORE_TEENSY35)
       vvt_pwm_max_count = 1000000L / (32 * configPage6.vvtFreq * 2); //Converts the frequency in Hz to the number of ticks (at 16uS) it takes to complete 1 cycle. Note that the frequency is divided by 2 coming from TS to allow for up to 512hz
+    #elif defined(CORE_TEENSY41)
+      vvt_pwm_max_count = 1000000L / (2 * configPage6.vvtFreq * 2); //Converts the frequency in Hz to the number of ticks (at 2uS) it takes to complete 1 cycle. Note that the frequency is divided by 2 coming fro TS to allow for up to 512hz
     #endif
 
     if(configPage6.vvtMode == VVT_MODE_CLOSED_LOOP)
@@ -449,8 +451,10 @@ void initialiseAuxPWM()
     // config wmi pwm output to use vvt output
     #if defined(CORE_AVR)
       vvt_pwm_max_count = 1000000L / (16 * configPage6.vvtFreq * 2); //Converts the frequency in Hz to the number of ticks (at 16uS) it takes to complete 1 cycle. Note that the frequency is divided by 2 coming from TS to allow for up to 512hz
-    #elif defined(CORE_TEENSY)
+    #elif defined(CORE_TEENSY35)
       vvt_pwm_max_count = 1000000L / (32 * configPage6.vvtFreq * 2); //Converts the frequency in Hz to the number of ticks (at 16uS) it takes to complete 1 cycle. Note that the frequency is divided by 2 coming from TS to allow for up to 512hz
+    #elif defined(CORE_TEENSY41)
+      vvt_pwm_max_count = 1000000L / (2 * configPage6.vvtFreq * 2); //Converts the frequency in Hz to the number of ticks (at 2uS) it takes to complete 1 cycle. Note that the frequency is divided by 2 coming from TS to allow for up to 512hz
     #endif
     BIT_CLEAR(currentStatus.status4, BIT_STATUS4_WMI_EMPTY);
     currentStatus.wmiPW = 0;
@@ -1012,13 +1016,21 @@ void boostDisable()
 {
   if (boost_pwm_state == true)
   {
+    #if defined(CORE_TEENSY41) //PIT TIMERS count down and have opposite effect on PWM
+    BOOST_PIN_HIGH();
+    #else
     BOOST_PIN_LOW();  // Switch pin to low
+    #endif
     SET_COMPARE(BOOST_TIMER_COMPARE, BOOST_TIMER_COUNTER + (boost_pwm_max_count - boost_pwm_cur_value) );
     boost_pwm_state = false;
   }
   else
   {
+    #if defined(CORE_TEENSY41) //PIT TIMERS count down and have opposite effect on PWM
+    BOOST_PIN_LOW();
+    #else
     BOOST_PIN_HIGH();  // Switch pin high
+    #endif
     SET_COMPARE(BOOST_TIMER_COMPARE, BOOST_TIMER_COUNTER + boost_pwm_target_value);
     boost_pwm_cur_value = boost_pwm_target_value;
     boost_pwm_state = true;
@@ -1036,12 +1048,20 @@ void boostDisable()
   {
     if( (vvt1_pwm_value > 0) && (vvt1_max_pwm == false) ) //Don't toggle if at 0%
     {
+      #if defined(CORE_TEENSY41)
+      VVT1_PIN_OFF();
+      #else
       VVT1_PIN_ON();
+      #endif
       vvt1_pwm_state = true;
     }
     if( (vvt2_pwm_value > 0) && (vvt2_max_pwm == false) ) //Don't toggle if at 0%
     {
+      #if defined(CORE_TEENSY41)
+      VVT2_PIN_OFF();
+      #else
       VVT2_PIN_ON();
+      #endif
       vvt2_pwm_state = true;
     }
 
@@ -1068,7 +1088,11 @@ void boostDisable()
     {
       if(vvt1_pwm_value < (long)vvt_pwm_max_count) //Don't toggle if at 100%
       {
+        #if defined(CORE_TEENSY41)
+        VVT1_PIN_ON();
+        #else
         VVT1_PIN_OFF();
+        #endif
         vvt1_pwm_state = false;
         vvt1_max_pwm = false;
       }
@@ -1085,7 +1109,11 @@ void boostDisable()
     {
       if(vvt2_pwm_value < (long)vvt_pwm_max_count) //Don't toggle if at 100%
       {
+        #if defined(CORE_TEENSY41)
+        VVT2_PIN_ON();
+        #else
         VVT2_PIN_OFF();
+        #endif
         vvt2_pwm_state = false;
         vvt2_max_pwm = false;
       }
@@ -1102,7 +1130,11 @@ void boostDisable()
     {
       if(vvt1_pwm_value < (long)vvt_pwm_max_count) //Don't toggle if at 100%
       {
+       #if defined(CORE_TEENSY41)
+        VVT1_PIN_ON();
+        #else
         VVT1_PIN_OFF();
+        #endif
         vvt1_pwm_state = false;
         vvt1_max_pwm = false;
         SET_COMPARE(VVT_TIMER_COMPARE, VVT_TIMER_COUNTER + (vvt_pwm_max_count - vvt1_pwm_cur_value) );
@@ -1110,7 +1142,11 @@ void boostDisable()
       else { vvt1_max_pwm = true; }
       if(vvt2_pwm_value < (long)vvt_pwm_max_count) //Don't toggle if at 100%
       {
+        #if defined(CORE_TEENSY41)
+        VVT2_PIN_ON();
+        #else
         VVT2_PIN_OFF();
+        #endif
         vvt2_pwm_state = false;
         vvt2_max_pwm = false;
         SET_COMPARE(VVT_TIMER_COMPARE, VVT_TIMER_COUNTER + (vvt_pwm_max_count - vvt2_pwm_cur_value) );
