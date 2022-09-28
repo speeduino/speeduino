3997708997b6ad25128709e6967d54076bc7599e
Josh Stewart <josh@noisymime.org>
2022-09-29 09:47:37 +1000

    Improvements to init unit tests

--- test/test_misc/tests_init.h
+++ test/test_misc/tests_init.h
@@ -6,4 +6,8 @@ void test_initialisation_ports(void);
 void test_initialisation_outputs_V03(void);
 void test_initialisation_outputs_V04(void);
 void test_initialisation_outputs_MX5_8995(void);
+void test_initialisation_outputs_PWM_idle(void);
+void test_initialisation_outputs_stepper_idle(void);
+void test_initialisation_outputs_boost(void);
+void test_initialisation_outputs_VVT(void);
 uint8_t getPinMode(uint8_t);
