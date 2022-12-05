c65734abef96f546e06b1ece6d33fbddda0a43d6
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2022-12-04 21:36:42 -0600

    Simplify - make cppcheck do more work Now need for the script to gather source files and run them one at a time, or flatten xml etc.

--- misra/check_misra.sh
+++ misra/check_misra.sh
@@ -4,27 +4,15 @@ script_folder="$(dirname $(readlink -f $0))"
 
 # Initialize variables with defaults
 source_folder="$script_folder/../speeduino" # -s, --source
-file_exts="cpp,ino"                         # -e, --exts
 out_folder="$script_folder/.results"        # -o, --out
 cppcheck_path=""                            # -c, --cppcheck
 quiet=0                                     # -q, --quiet
-output_xml=0                                # -x | --xml
-
-cppcheck_parameters=( --inline-suppr
-                      --language=c++
-                      --addon="$script_folder/misra.json"
-                      --suppressions-list="$script_folder/suppressions.txt"
-                      --platform=avr8
-                      -DCORE_AVR=1
-                      -D__AVR_ATmega2560__
-                      # All violations from included libraries (*src* folders) are ignored
-                      --suppress="\"*:*src*\"")
+output_xml=0                                # -x, --xml
 
 function parse_command_line() {
    while [ $# -gt 0 ] ; do
     case "$1" in
       -s | --source) source_folder="$2" ;;
-      -e | --exts) file_exts="$2" ;;
       -o | --out) out_folder="$2" ;;
       -c | --cppcheck) cppcheck_path="$2" ;;
       -q | --quiet) quiet=1 ;;
@@ -38,97 +26,6 @@ function parse_command_line() {
   done
 }
 
-function get_source_files() {
-  local source_files=() 
-  for i in ${file_exts//,/ }; do
-    local file_list=($source_folder/*.$i)
-    source_files+=(${file_list[@]})
-  done
-  
-  echo ${source_files[@]}
-}
-
-function run_cppcheck() {
-  # There is no way to tell the misra add on to skip certain headers
-  # libdivide adds 10+ minutes to each file so rename the folder 
-  # before the scan
-  mv "$source_folder"/src/libdivide "$source_folder"/src/_libdivide
-  shopt -s nullglob nocaseglob
-  
-  err_file="$out_folder/cppcheck_output.txt"
-
-  for i in "${@}"; do
-    # Must redirect stdout: if not, the cppcheck output is mixed in
-    # with the function output (I.e. returned to caller)
-    # Print current file to stderr instead.
-    >&2 echo "Checking " $i
-
-    out_file="$out_folder/$(basename $i).tmp"
-    rm -f $out_file    
-    echo $out_file # Function return
-
-    "$cppcheck_bin" ${cppcheck_parameters[@]} --output-file=$out_file $i > $err_file
-
-    # Echo cppcheck output to stderr
-    >&2 cat $err_file
-  done
-
-  shopt -u nullglob nocaseglob
-  # Restore libdivide folder name after scan
-  mv "$source_folder"/src/_libdivide "$source_folder"/src/libdivide
-}
-
-function merge_txt_files() {
-  local merged_file="$out_folder/merged.txt.tmp"
-
-  rm -f $merged_file
-  for i in "${@}"; do
-    cat $i >> $merged_file
-  done
-
-  echo "$merged_file"
-}
-
-function process_txt_results() {
-
-  local merged_file=$(merge_txt_files "${@}")
-
-  local out_file="$out_folder/results.txt"
-  rm -f $out_file
-
-  # It's grouped into chunks of 3 lines: fold those into one line
-  sed '$!N;$!N;s/\n/~/g' < "$merged_file" |\
-    # Remove duplicate lines
-    sort | uniq | \
-    # Unfold the line groups for readability
-    tr '~' '\n' > "$out_file"
-
-  echo $out_file
-}
-
-function merge_xml_files() {
-  local merged_file="$out_folder/merged.xml.tmp"
-  rm -f $merged_file
-
-  echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?><errors>" > $merged_file
-  for i in "${@}"; do
-      xmllint --xpath "/results/errors/*" $i >> $merged_file
-  done
-  echo "</errors>" >> $merged_file
-
-  echo "$merged_file"
-}
-
-
-function process_xml_results() {
-  local out_file="$out_folder/results.xml"
-  rm -f $out_file
-
-  xsltproc "$script_folder/flatten_merged.xsl" $(merge_xml_files "${@}") > $out_file
-  
-  echo "$out_file"
-}
-
 parse_command_line "$@"
 
 cppcheck_bin="${cppcheck_path}/cppcheck"
@@ -136,23 +33,33 @@ cppcheck_misra="${cppcheck_path}/addons/misra.py"
 
 mkdir -p "$out_folder"
 
+cppcheck_parameters=( --inline-suppr
+                      --language=c++
+                      --addon="$script_folder/misra.json"
+                      --suppressions-list="$script_folder/suppressions.txt"
+                      --platform=avr8
+                      -DCORE_AVR=1
+                      -D__AVR_ATmega2560__
+                      # All violations from included libraries (*src* folders) are ignored
+                      --suppress="*:*src*"
+                      # Don't parse the /src folder
+                      -i "$source_folder/src"
+                      "$source_folder/**.ino"
+                      "$source_folder/**.cpp")
+
+cppcheck_out_file="$out_folder/results.txt"
 if [ $output_xml -eq 1 ]; then
+  cppcheck_out_file="$out_folder/results.xml"
   cppcheck_parameters+=(--xml)
 fi
 
-source_files=($(get_source_files))
-result_files=($(run_cppcheck ${source_files[@]}))
-if [ $output_xml -eq 1 ]; then
-  out_file=$(process_xml_results ${result_files[@]})
-else
-  out_file=$(process_txt_results ${result_files[@]})
-fi
+"$cppcheck_bin" ${cppcheck_parameters[@]} 2> $cppcheck_out_file
 
 # Count lines for Mandatory or Required rules
-error_count=`grep -i "Mandatory - \|Required - " < "$out_file" | wc -l`
+error_count=`grep -i "Mandatory - \|Required - " < "$cppcheck_out_file" | wc -l`
 
 if [ $quiet -eq 0 ]; then
-  cat "$out_file"
+  cat "$cppcheck_out_file"
 fi
 echo $error_count MISRA violations
 echo $error_count > "$out_folder/error_count.txt"
