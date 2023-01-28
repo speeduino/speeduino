e0b1f238ea233099b17a291dabb66e587edea273
DeionSi <DeionSi@users.noreply.github.com>
2023-01-28 11:42:39 +0100

    Clean up PW calculations: Reduce duplication, remove unnecessary declarations and group code together where they are used.

--- speeduino/speeduino.ino
+++ speeduino/speeduino.ino
@@ -1317,46 +1317,44 @@ uint16_t PW(int REQ_FUEL, byte VE, long MAP, uint16_t corrections, int injOpen)
   //Standard float version of the calculation
   //return (REQ_FUEL * (float)(VE/100.0) * (float)(MAP/100.0) * (float)(TPS/100.0) * (float)(corrections/100.0) + injOpen);
   //Note: The MAP and TPS portions are currently disabled, we use VE and corrections only
-  uint16_t iVE, iCorrections;
-  uint16_t iMAP = 100;
-  uint16_t iAFR = 147;
 
   //100% float free version, does sacrifice a little bit of accuracy, but not much.
 
+  uint16_t iVE = ((unsigned int)VE << 7) / 100;
+  //iVE = divu100(((unsigned int)VE << 7));
+  unsigned long intermediate = ((uint32_t)REQ_FUEL * (uint32_t)iVE) >> 7; //Need to use an intermediate value to avoid overflowing the long
+
+  //Check whether either of the multiply MAP modes is turned on
+  if ( configPage2.multiplyMAP == MULTIPLY_MAP_MODE_100 ) {
+    uint16_t iMAP = ((unsigned int)MAP << 7) / 100;
+    intermediate = (intermediate * (unsigned long)iMAP) >> 7;
+  }
+  else if ( configPage2.multiplyMAP == MULTIPLY_MAP_MODE_BARO ) {
+    uint16_t iMAP = ((unsigned int)MAP << 7) / currentStatus.baro;
+    intermediate = (intermediate * (unsigned long)iMAP) >> 7;
+  }
+  
+  if ( (configPage2.includeAFR == true) && (configPage6.egoType == EGO_TYPE_WIDE) && (currentStatus.runSecs > configPage6.ego_sdelay) ) {
+    //EGO type must be set to wideband and the AFR warmup time must've elapsed for this to be used
+    uint16_t iAFR = ((unsigned int)currentStatus.O2 << 7) / currentStatus.afrTarget;  //Include AFR (vs target) if enabled
+    intermediate = (intermediate * (unsigned long)iAFR) >> 7;
+  }
+
+  if ( (configPage2.incorporateAFR == true) && (configPage2.includeAFR == false) ) {
+    uint16_t iAFR = ((unsigned int)configPage2.stoich << 7) / currentStatus.afrTarget;  //Incorporate stoich vs target AFR, if enabled.
+    intermediate = (intermediate * (unsigned long)iAFR) >> 7;
+  }
+  
   //If corrections are huge, use less bitshift to avoid overflow. Sacrifices a bit more accuracy (basically only during very cold temp cranking)
   byte bitShift = 7;
   if (corrections > 511 ) { bitShift = 6; }
   if (corrections > 1023) { bitShift = 5; }
-  
-  iVE = ((unsigned int)VE << 7) / 100;
-  //iVE = divu100(((unsigned int)VE << 7));
 
-  //Check whether either of the multiply MAP modes is turned on
-  if ( configPage2.multiplyMAP == MULTIPLY_MAP_MODE_100) { iMAP = ((unsigned int)MAP << 7) / 100; }
-  else if( configPage2.multiplyMAP == MULTIPLY_MAP_MODE_BARO) { iMAP = ((unsigned int)MAP << 7) / currentStatus.baro; }
-  
-  if ( (configPage2.includeAFR == true) && (configPage6.egoType == EGO_TYPE_WIDE) && (currentStatus.runSecs > configPage6.ego_sdelay) ) {
-    iAFR = ((unsigned int)currentStatus.O2 << 7) / currentStatus.afrTarget;  //Include AFR (vs target) if enabled
-  }
-  if ( (configPage2.incorporateAFR == true) && (configPage2.includeAFR == false) ) {
-    iAFR = ((unsigned int)configPage2.stoich << 7) / currentStatus.afrTarget;  //Incorporate stoich vs target AFR, if enabled.
-  }
-  iCorrections = (corrections << bitShift) / 100;
+  uint16_t iCorrections = (corrections << bitShift) / 100;
   //iCorrections = divu100((corrections << bitShift));
 
-
-  unsigned long intermediate = ((uint32_t)REQ_FUEL * (uint32_t)iVE) >> 7; //Need to use an intermediate value to avoid overflowing the long
-  if ( configPage2.multiplyMAP > 0 ) { intermediate = (intermediate * (unsigned long)iMAP) >> 7; }
-  
-  if ( (configPage2.includeAFR == true) && (configPage6.egoType == EGO_TYPE_WIDE) && (currentStatus.runSecs > configPage6.ego_sdelay) ) {
-    //EGO type must be set to wideband and the AFR warmup time must've elapsed for this to be used
-    intermediate = (intermediate * (unsigned long)iAFR) >> 7;  
-  }
-  if ( (configPage2.incorporateAFR == true) && (configPage2.includeAFR == false) ) {
-    intermediate = (intermediate * (unsigned long)iAFR) >> 7;
-  }
-  
   intermediate = (intermediate * (unsigned long)iCorrections) >> bitShift;
+
   if (intermediate != 0)
   {
     //If intermediate is not 0, we need to add the opening time (0 typically indicates that one of the full fuel cuts is active)
