063ccd25f5ab5ca26db5d7f044abebf5fecb9149
Bruno Bousquet <21108660+brunob45@users.noreply.github.com>
2023-02-06 13:23:12 -0500

    create disassembly and size files

---
+++ build.sh
@@ -0,0 +1,7 @@
+pio run
+
+rm speeduino.S
+~/.platformio/packages/toolchain-atmelavr/bin/avr-objdump -d .pio/build/megaatmega2560/firmware.elf > speeduino.S
+
+rm size.txt
+~/.platformio/packages/toolchain-atmelavr/bin/avr-nm -S --size-sort .pio/build/megaatmega2560/firmware.elf > size.txt
