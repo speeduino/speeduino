7848c1c0da02fd412896ba8e3f4be34f13282c1a
Bruno Bousquet <21108660+brunob45@users.noreply.github.com>
2023-02-06 14:39:21 -0500

    remove disassembly and size files

--- build.sh
+++
@@ -1,8 +0,0 @@
-pio run -t clean
-pio run
-
-rm speeduino.S
-~/.platformio/packages/toolchain-atmelavr/bin/avr-objdump -d .pio/build/megaatmega2560/firmware.elf > speeduino.S
-
-rm size.txt
-~/.platformio/packages/toolchain-atmelavr/bin/avr-nm -S --size-sort .pio/build/megaatmega2560/firmware.elf > size.txt
