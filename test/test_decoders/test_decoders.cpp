8d3174c29150095c61cf56fe6a51b11216e08536
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2023-03-04 23:59:09 -0600

    Add Nissan360 decoder unit tests.

--- test/test_decoders/test_decoders.cpp
+++ test/test_decoders/test_decoders.cpp
@@ -5,6 +5,7 @@
 
 #include "missing_tooth/missing_tooth.h"
 #include "dual_wheel/dual_wheel.h"
+#include "Nissan360/Nissan360.h"
 
 void setup()
 {
@@ -18,6 +19,7 @@ void setup()
 
     testMissingTooth();
     testDualWheel();
+    testNissan360();
 
     UNITY_END(); // stop unit testing
 }
