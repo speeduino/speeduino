1ddb19445fb1d2a2c452ec803d8eeb3257bb7d57
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2023-03-05 23:38:57 -0600

    Doxygen comments

--- speeduino/scheduler.h
+++ speeduino/scheduler.h
@@ -48,22 +48,37 @@ See page 136 of the processors datasheet: http://www.atmel.com/Images/doc2549.pd
 #define USE_IGN_REFRESH
 #define IGNITION_REFRESH_THRESHOLD  30 //Time in uS that the refresh functions will check to ensure there is enough time before changing the end compare
 
+/** @brief Initialize all schedulers to the OFF state */
 void initialiseIgnitionSchedulers(void);
 
+/** @brief Start the timers that drive schedulers  */
 void startIgnitionSchedulers(void);
+
+/** @brief Stop the timers that drive schedulers  */
 void stopIgnitionSchedulers(void);
+
+/** @brief Disable an ignition scheduler */
 void disableIgnSchedule(uint8_t channel);
 
+/** @brief Disable all ignition schedulers */
 void disableAllIgnSchedules(void);
 
+/** @brief Initialize all schedulers to the OFF state */
 void initialiseFuelSchedulers(void);
 
+/** @brief Start the timers that drive schedulers  */
 void startFuelSchedulers(void);
+
+/** @brief Stop the timers that drive schedulers  */
 void stopFuelSchedulers(void);
 
+/** @brief Start fuel system priming the fuel */
 void beginInjectorPriming(void);
 
+/** @brief Disable a fuel scheduler  */
 void disableFuelSchedule(uint8_t channel);
+
+/** @brief Disable all ignition schedulers */
 void disableAllFuelSchedules(void);
 
 /** \enum ScheduleStatus
@@ -82,7 +97,6 @@ enum ScheduleStatus {
   RUNNING_WITHNEXT = 0b00000100U,
 }; 
 
-
 /**
  * @brief A schedule for a single output channel. 
  * 
@@ -100,7 +114,8 @@ enum ScheduleStatus {
  * \endcode
  * 
  * @par Timers are modelled as registers
- * Once set, Schedule instances are usually driven externally by a timer ISR
+ * Once set, Schedule instances are usually driven externally by a timer
+ * ISR calling moveToNextState() periodically to update the schedule states.
  */
 struct Schedule {
   // Deduce the real types of the counter and compare registers.
@@ -140,7 +155,10 @@ struct Schedule {
   compare_t &_compare;       ///< **Reference**to the compare register. E.g. OCR3A
 };
 
-/** @brief Is the schedule action currently running? */
+/**
+ * @brief Is the schedule running?
+ * I.e. the action has started, but not finished. E.g. injector is open
+ */
 static inline bool isRunning(const Schedule &schedule) {
   // Using flags and bitwise AND (&) to check multiple states is much quicker
   // than a logical or (||) (one less branch & 30% less instructions)
@@ -149,11 +167,12 @@ static inline bool isRunning(const Schedule &schedule) {
 }
 
 /**
- * @brief Set the schedule action start & end callbacks
+ * @brief Set the schedule callbacks. I.e the functions called when the action
+ * needs to start & stop
  * 
  * @param schedule Schedule to modify
- * @param pStartCallback Start callback
- * @param pEndCallback End callback
+ * @param pStartCallback The new start callback - called when the schedule switches to RUNNING status
+ * @param pEndCallback The new end callback - called when the schedule switches from RUNNING to OFF status
  */
 void setCallbacks(Schedule &schedule, voidVoidCallback pStartCallback, voidVoidCallback pEndCallback);
 
@@ -170,16 +189,31 @@ void setSchedule(Schedule &schedule, uint32_t delay, uint16_t duration, bool all
 /** @brief Disable the schedule */
 void disableSchedule(Schedule &schedule);
 
-/** Ignition schedule.
+/** @brief An ignition schedule.
+ *
+ * Goal is to fire the spark as close to the requested angle as possible.
+ * 
+ * \code 
+ *   <--------------- Delay ---------------><---- Charge Coil ---->
+ *                                                                ^
+ *                                                              Spark
+ * \endcode
+ * 
+ * Terminology: dwell is the period when the ignition system applies an electric
+ * current to the ignition coil's primary winding in order to charge up the coil
+ * so it can generate a spark. 
+ * 
+ * Note that dwell times use uint16_t & therefore maximum dwell is 65.535ms. 
+ * This limit is imposed elsewhere in Speeduino also.
  */
 struct IgnitionSchedule : public Schedule {
 
   using Schedule::Schedule;
 
-  volatile uint32_t startTime; /**< The system time (in uS) that the schedule started, used by the overdwell protection in timers.ino */
-  int16_t startAngle;        ///< Angle the coil should begin charging.
-  int16_t endAngle;          ///< Angle the spark should fire at.
-  int16_t channelIgnDegrees; ///< The number of crank degrees until cylinder is at TDC  
+  volatile uint32_t startTime;///< The system time (in uS) that the schedule started, used by the overdwell protection in timers.ino
+  int16_t startAngle;         ///< Angle the coil should begin charging.
+  int16_t endAngle;           ///< Angle the spark should fire at.
+  int16_t channelIgnDegrees;  ///< The number of crank degrees until cylinder is at TDC  
 };
 
 /**
@@ -196,13 +230,15 @@ static inline void setIgnitionSchedule(IgnitionSchedule &schedule, uint32_t dela
 }
 
 /**
- * @brief Called once per millisecond by an **external** timer. The over dwell protection system
- * runs independently of the standard ignition schedules and monitors the time that each ignition 
- * output has been active. If the active time exceeds this amount, the output will be ended to
- * prevent damage to coils.
+ * @brief Check that no ignition channel has been charging the coil for too long
+ * 
+ * The over dwell protection system runs independently of the standard ignition 
+ * schedules and monitors the time that each ignition output has been active. If the 
+ * active time exceeds this amount, the output will be ended to prevent damage to coils.
+ * 
+ * @note Must be called once per millisecond by an **external** timer.
  */
 void applyOverDwellProtection(void);
-
 
 /**
  * @brief Shared ignition schedule timer ISR *implementation*. Should be called by the actual ignition timer ISRs
