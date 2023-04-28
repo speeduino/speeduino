92de3df1f0b2e4544c8928575a0e19c42f900526
Teemo Vaas <teemo@digiwood.ee>
2023-04-28 13:28:08 +0300

    Use ATOMIC_BLOCK instead of 'interrupts()'
    Replace noInterrupts();interrupts(); pair in the timers.ino with ATOMIC_BLOCK solution. This way we do not accidentally enable nested interrupts on some platforms where interrupt nesting might not be used.

--- speeduino/timers.ino
+++ speeduino/timers.ino
@@ -19,6 +19,7 @@ Timers are typically low resolution (Compared to Schedulers), with maximum frequ
 #include "scheduler.h"
 #include "auxiliaries.h"
 #include "comms.h"
+#include <util/atomic.h>
 
 #if defined(CORE_AVR)
   #include <avr/wdt.h>
@@ -64,7 +65,7 @@ void oneMSInterval(void) //Most ARM chips can simply call a function
   //dwell limiter mainly is intended to catch race conditions on port writes when read-modify-write to some other pin may revert the pin state when schedule interrupt occurs between read and write.
   uint8_t currentMillis=(uint8_t)millis();
   if((configPage4.useDwellLim) && (isCrankLocked == false)){
-    noInterrupts();
+    ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
     if((uint8_t)(currentMillis-ignitionSchedule1.startTime) > configPage4.dwellLimit) { ignitionSchedule1.EndFunction(); }//casts needed here for overflow proof optimal comparison(dissassembly listing confirmed)
     if((uint8_t)(currentMillis-ignitionSchedule2.startTime) > configPage4.dwellLimit) { ignitionSchedule2.EndFunction(); } 
     if((uint8_t)(currentMillis-ignitionSchedule3.startTime) > configPage4.dwellLimit) { ignitionSchedule3.EndFunction(); } 
@@ -79,7 +80,7 @@ void oneMSInterval(void) //Most ARM chips can simply call a function
     #endif    
     #endif
     #endif
-    interrupts();
+    }//ATOMIC_BLOCK here is for preventing variable editing race condition in case of nested interrupts.
   }
 
   //Tacho is flagged as being ready for a pulse by the ignition outputs, or the sweep interval upon startup
