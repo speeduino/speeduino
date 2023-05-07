1dfe060f2ac2723fd0c4d266bb61037ffc355b8f
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2023-05-07 00:38:29 -0500

    Test start/end callback assignment

--- test/test_init/test_ignition_schedule_init.cpp
+++ test/test_init/test_ignition_schedule_init.cpp
@@ -3,6 +3,19 @@
 #include "globals.h"
 #include "init.h"
 #include "schedule_calcs.h"
+#include "scheduledIO.h"
+
+static void assert_ignition_channel(uint16_t angle, uint8_t channel, int channelInjDegrees, void (*startFunction)(void), void (*endFunction)(void))
+{
+  char msg[32];
+
+  sprintf_P(msg, PSTR("channe%" PRIu8 "1InjDegrees"), channel+1);
+  TEST_ASSERT_EQUAL_MESSAGE(angle, channelInjDegrees, msg);
+  sprintf_P(msg, PSTR("ign%" PRIu8 "StartFunction"), channel+1);
+  TEST_ASSERT_TRUE_MESSAGE(channel>=maxIgnOutputs || (startFunction!=nullCallback), msg);
+  sprintf_P(msg, PSTR("ign%" PRIu8 "EndFunction"), channel+1);
+  TEST_ASSERT_TRUE_MESSAGE(channel>=maxIgnOutputs || (endFunction!=nullCallback), msg);
+}
 
 static void assert_ignition_schedules(uint16_t crankAngle, uint16_t expectedOutputs, uint16_t angle[])
 {
@@ -13,29 +26,21 @@ static void assert_ignition_schedules(uint16_t crankAngle, uint16_t expectedOutp
   strcpy_P(msg, PSTR("maxIgnOutputs"));
   TEST_ASSERT_EQUAL_UINT16_MESSAGE(expectedOutputs, maxIgnOutputs, msg);
 
-  strcpy_P(msg, PSTR("channel1IgnDegrees"));
-  TEST_ASSERT_EQUAL_MESSAGE(angle[0], channel1IgnDegrees, msg);
-  strcpy_P(msg, PSTR("channel2IgnDegrees"));
-  TEST_ASSERT_EQUAL_MESSAGE(angle[1], channel2IgnDegrees, msg);
-  strcpy_P(msg, PSTR("channel3IgnDegrees"));
-  TEST_ASSERT_EQUAL_MESSAGE(angle[2], channel3IgnDegrees, msg);
-  strcpy_P(msg, PSTR("channel4IgnDegrees"));
-  TEST_ASSERT_EQUAL_MESSAGE(angle[3], channel4IgnDegrees, msg);
+  assert_ignition_channel(angle[0], 0, channel1IgnDegrees, ign1StartFunction, ign1EndFunction);
+  assert_ignition_channel(angle[1], 1, channel2IgnDegrees, ign2StartFunction, ign2EndFunction);
+  assert_ignition_channel(angle[2], 2, channel3IgnDegrees, ign3StartFunction, ign3EndFunction);
+  assert_ignition_channel(angle[3], 3, channel4IgnDegrees, ign4StartFunction, ign4EndFunction);
 #if IGN_CHANNELS>=5
-  strcpy_P(msg, PSTR("channel5IgnDegrees"));
-  TEST_ASSERT_EQUAL_MESSAGE(angle[4], channel5IgnDegrees, msg);
+  assert_ignition_channel(angle[4], 4, channel5IgnDegrees, ign5StartFunction, ign5EndFunction);
 #endif
 #if IGN_CHANNELS>=6
-  strcpy_P(msg, PSTR("channel6IgnDegrees"));
-  TEST_ASSERT_EQUAL_MESSAGE(angle[5], channel6IgnDegrees, msg);
+  assert_ignition_channel(angle[5], 5, channel6IgnDegrees, ign6StartFunction, ign6EndFunction);
 #endif
 #if IGN_CHANNELS>=7
-  strcpy_P(msg, PSTR("channel7IgnDegrees"));
-  TEST_ASSERT_EQUAL_MESSAGE(angle[6], channel7IgnDegrees, msg);
+  assert_ignition_channel(angle[6], 6, channel7IgnDegrees, ign7StartFunction, ign7EndFunction);
 #endif
 #if IGN_CHANNELS>=8
-  strcpy_P(msg, PSTR("channel8IgnDegrees"));
-  TEST_ASSERT_EQUAL_MESSAGE(angle[7], channel8IgnDegrees, msg);
+  assert_ignition_channel(angle[7], 7, channel8IgnDegrees, ign8StartFunction, ign8EndFunction);
 #endif 
 }
 
