0d7f35e6cbb229fb32392228fd5eb6551ad425e3
Josh Stewart <josh@noisymime.org>
2023-05-19 09:59:32 +1000

    Use 1kHz section for map reads on all MCUs

--- speeduino/speeduino.ino
+++ speeduino/speeduino.ino
@@ -242,16 +242,10 @@ void loop(void)
 
     //***Perform sensor reads***
     //-----------------------------------------------------------------------------------------------------
-#if defined(CORE_AVR) //AVR loop speed wont saturate MAP sampling time
-    readMAP();
-#endif
-    if (BIT_CHECK(LOOP_TIMER, BIT_TIMER_1KHZ)) //Every 1ms
+    if (BIT_CHECK(LOOP_TIMER, BIT_TIMER_1KHZ)) //Every 1ms. NOTE: This is NOT guaranteed to run at 1kHz on AVR systems. It will run at 1kHz if possible or as fast as loops/s allows if not. 
     {
       BIT_CLEAR(TIMER_mask, BIT_TIMER_1KHZ);
-
-#if !defined(CORE_AVR)
-      readMAP(); //On AVR this is performed about 600 to 1200x/S
-#endif
+      readMAP();
     }
     
     if (BIT_CHECK(LOOP_TIMER, BIT_TIMER_15HZ)) //Every 32 loops
