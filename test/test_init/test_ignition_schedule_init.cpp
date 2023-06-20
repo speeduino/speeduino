f41f156bf8511c4b5d04c34948586a1e2380fe53
Teemo Vaas <teemo@digiwood.ee>
2023-06-20 15:25:30 +0300

    Fix tests.
    Some tests were left out from this branch, so bring them back in. Also test folder structure was outdated, level this with the master.

---
+++ test/test_init/test_ignition_schedule_init.cpp
@@ -0,0 +1,308 @@
+#include <Arduino.h>
+#include <unity.h>
+#include "globals.h"
+#include "init.h"
+#include "scheduledIO.h"
+#include "scheduler.h"
+#include "../test_utils.h"
+
+static void assert_ignition_channel(uint16_t angle, uint8_t channel, int channelInjDegrees, voidVoidCallback startFunction, voidVoidCallback endFunction)
+{
+  char msg[32];
+
+  sprintf_P(msg, PSTR("channe%" PRIu8 "1InjDegrees"), channel+1);
+  TEST_ASSERT_EQUAL_MESSAGE(angle, channelInjDegrees, msg);
+  sprintf_P(msg, PSTR("ign%" PRIu8 "StartFunction"), channel+1);
+  TEST_ASSERT_TRUE_MESSAGE(channel>=maxIgnOutputs || (startFunction!=nullCallback), msg);
+  sprintf_P(msg, PSTR("ign%" PRIu8 "EndFunction"), channel+1);
+  TEST_ASSERT_TRUE_MESSAGE(channel>=maxIgnOutputs || (endFunction!=nullCallback), msg);
+}
+
+static void assert_ignition_schedules(uint16_t crankAngle, uint16_t expectedOutputs, const uint16_t angle[])
+{
+  char msg[48];
+
+  strcpy_P(msg, PSTR("CRANK_ANGLE_MAX_IGN"));
+  TEST_ASSERT_EQUAL_INT16_MESSAGE(crankAngle, CRANK_ANGLE_MAX_IGN, msg);
+  strcpy_P(msg, PSTR("maxIgnOutputs"));
+  TEST_ASSERT_EQUAL_UINT16_MESSAGE(expectedOutputs, maxIgnOutputs, msg);
+
+  assert_ignition_channel(angle[0], 0, ignitionSchedule1.channelDegrees, ignitionSchedule1.StartFunction, ignitionSchedule1.EndFunction);
+  assert_ignition_channel(angle[1], 1, ignitionSchedule2.channelDegrees, ignitionSchedule2.StartFunction, ignitionSchedule2.EndFunction);
+  assert_ignition_channel(angle[2], 2, ignitionSchedule3.channelDegrees, ignitionSchedule3.StartFunction, ignitionSchedule3.EndFunction);
+  assert_ignition_channel(angle[3], 3, ignitionSchedule4.channelDegrees, ignitionSchedule4.StartFunction, ignitionSchedule4.EndFunction);
+#if IGN_CHANNELS>=5
+  assert_ignition_channel(angle[4], 4, ignitionSchedule5.channelDegrees, ignitionSchedule5.StartFunction, ignitionSchedule5.EndFunction);
+#endif
+#if IGN_CHANNELS>=6
+  assert_ignition_channel(angle[5], 5, ignitionSchedule6.channelDegrees, ignitionSchedule6.StartFunction, ignitionSchedule6.EndFunction);
+#endif
+#if IGN_CHANNELS>=7
+  assert_ignition_channel(angle[6], 6, ignitionSchedule7.channelDegrees, ignitionSchedule7.StartFunction, ignitionSchedule7.EndFunction);
+#endif
+#if IGN_CHANNELS>=8
+  assert_ignition_channel(angle[7], 7, ignitionSchedule8.channelDegrees, ignitionSchedule8.StartFunction, ignitionSchedule8.EndFunction);
+#endif 
+}
+
+static void cylinder1_stroke4_seq_even(void)
+{
+  configPage4.sparkMode = IGN_MODE_SEQUENTIAL;
+  configPage2.engineType = EVEN_FIRE;
+  initialiseAll(); //Run the main initialise function
+  const uint16_t angle[] = {0,0,0,0,0,0,0,0};
+  assert_ignition_schedules(720U, 1U, angle);
+}
+
+static void cylinder1_stroke4_wasted_even(void)
+{
+  configPage4.sparkMode = IGN_MODE_WASTED;
+  configPage2.engineType = EVEN_FIRE;
+  initialiseAll(); //Run the main initialise function
+  const uint16_t angle[] = {0,0,0,0,0,0,0,0};
+  assert_ignition_schedules(360U, 1U, angle);
+}  
+
+static void cylinder1_stroke4_seq_odd(void)
+{
+  configPage4.sparkMode = IGN_MODE_SEQUENTIAL;
+  configPage2.engineType = ODD_FIRE;
+  initialiseAll(); //Run the main initialise function
+  const uint16_t angle[] = {0,0,0,0,0,0,0,0};
+  assert_ignition_schedules(720U, 1U, angle);
+}
+
+static void run_1_cylinder_4stroke_tests(void)
+{
+  configPage2.nCylinders = 1;
+  configPage2.strokes = FOUR_STROKE;
+
+  RUN_TEST_P(cylinder1_stroke4_seq_even);
+  RUN_TEST_P(cylinder1_stroke4_wasted_even);
+  RUN_TEST_P(cylinder1_stroke4_seq_odd);
+}
+
+static void cylinder2_stroke4_seq_even(void)
+{
+  configPage4.sparkMode = IGN_MODE_SEQUENTIAL;
+  configPage2.engineType = EVEN_FIRE;
+  initialiseAll(); //Run the main initialise function
+  const uint16_t angle[] = {0,180,0,0,0,0,0,0};
+  assert_ignition_schedules(720U, 2U, angle);
+}
+
+static void cylinder2_stroke4_wasted_even(void)
+{
+  configPage4.sparkMode = IGN_MODE_WASTED;
+  configPage2.engineType = EVEN_FIRE;
+  initialiseAll(); //Run the main initialise function
+  const uint16_t angle[] = {0,180,0,0,0,0,0,0};
+  assert_ignition_schedules(360U, 2U, angle);
+}  
+
+static void cylinder2_stroke4_seq_odd(void)
+{
+  configPage4.sparkMode = IGN_MODE_SEQUENTIAL;
+  configPage2.engineType = ODD_FIRE;
+  configPage2.oddfire2 = 13;
+  configPage2.oddfire3 = 111;
+  configPage2.oddfire4 = 217;
+
+  initialiseAll(); //Run the main initialise function
+  const uint16_t angle[] = {0,13,0,0,0,0,0,0};
+  assert_ignition_schedules(720U, 2U, angle);
+}
+
+static void run_2_cylinder_4stroke_tests(void)
+{
+  configPage2.nCylinders = 2;
+  configPage2.strokes = FOUR_STROKE;
+
+  RUN_TEST_P(cylinder2_stroke4_seq_even);
+  RUN_TEST_P(cylinder2_stroke4_wasted_even);
+  RUN_TEST_P(cylinder2_stroke4_seq_odd);
+}
+
+static void cylinder3_stroke4_seq_even(void)
+{
+  configPage4.sparkMode = IGN_MODE_SEQUENTIAL;
+  configPage2.engineType = EVEN_FIRE;
+  initialiseAll(); //Run the main initialise function
+  const uint16_t angle[] = {0,240,480,0,0,0,0,0};
+  assert_ignition_schedules(720U, 3U, angle);
+}
+
+static void cylinder3_stroke4_wasted_even(void)
+{
+  configPage4.sparkMode = IGN_MODE_WASTED;
+  configPage2.engineType = EVEN_FIRE;
+  initialiseAll(); //Run the main initialise function
+  const uint16_t angle[] = {0,120,240,0,0,0,0,0};
+  assert_ignition_schedules(360U, 3U, angle);
+}  
+
+static void cylinder3_stroke4_wasted_odd(void)
+{
+  configPage4.sparkMode = IGN_MODE_WASTED;
+  configPage2.engineType = ODD_FIRE;
+  configPage2.oddfire2 = 13;
+  configPage2.oddfire3 = 111;
+  configPage2.oddfire4 = 217;
+  initialiseAll(); //Run the main initialise function
+  const uint16_t angle[] = {0,13,111,0,0,0,0,0};
+  assert_ignition_schedules(360U, 3U, angle);
+}  
+
+static void run_3_cylinder_4stroke_tests(void)
+{
+  configPage2.nCylinders = 3;
+  configPage2.strokes = FOUR_STROKE;
+
+  RUN_TEST_P(cylinder3_stroke4_seq_even);
+  RUN_TEST_P(cylinder3_stroke4_wasted_even);
+  RUN_TEST_P(cylinder3_stroke4_wasted_odd);
+}
+
+static void cylinder4_stroke4_seq_even(void)
+{
+  configPage4.sparkMode = IGN_MODE_SEQUENTIAL;
+  configPage2.engineType = EVEN_FIRE;
+  initialiseAll(); //Run the main initialise function
+  const uint16_t angle[] = {0,180,360,540,0,0,0,0};
+  assert_ignition_schedules(720U, 4U, angle);
+}
+
+static void cylinder4_stroke4_wasted_even(void)
+{
+  configPage4.sparkMode = IGN_MODE_WASTED;
+  configPage2.engineType = EVEN_FIRE;
+  initialiseAll(); //Run the main initialise function
+  const uint16_t angle[] = {0,180,0,0,0,0,0,0};
+  assert_ignition_schedules(360U, 2U, angle);
+}  
+
+static void cylinder4_stroke4_seq_odd(void)
+{
+  configPage4.sparkMode = IGN_MODE_SEQUENTIAL;
+  configPage2.engineType = ODD_FIRE;
+  configPage2.oddfire2 = 13;
+  configPage2.oddfire3 = 111;
+  configPage2.oddfire4 = 217;
+  initialiseAll(); //Run the main initialise function
+  const uint16_t angle[] = {0,13,111,217,0,0,0,0};
+  assert_ignition_schedules(360U, 4U, angle);
+}
+
+
+static void run_4_cylinder_4stroke_tests(void)
+{
+  configPage2.nCylinders = 4;
+  configPage2.strokes = FOUR_STROKE;
+
+  RUN_TEST_P(cylinder4_stroke4_seq_even);
+  RUN_TEST_P(cylinder4_stroke4_wasted_even);
+  RUN_TEST_P(cylinder4_stroke4_seq_odd);
+}
+
+static void cylinder5_stroke4_seq_even(void)
+{
+  configPage4.sparkMode = IGN_MODE_SEQUENTIAL;
+  configPage2.engineType = EVEN_FIRE;
+  initialiseAll(); //Run the main initialise function
+  const uint16_t angle[] = {0,144,288,432,576,0,0,0};
+  assert_ignition_schedules(720U, 5U, angle);
+}
+
+static void cylinder5_stroke4_wasted_even(void)
+{
+  configPage4.sparkMode = IGN_MODE_WASTED;
+  configPage2.engineType = EVEN_FIRE;
+  initialiseAll(); //Run the main initialise function
+  const uint16_t angle[] = {0,72,144,216,288,0,0,0};
+  assert_ignition_schedules(360U, 5U, angle);
+}  
+
+static void run_5_cylinder_4stroke_tests(void)
+{
+  configPage2.nCylinders = 5;
+  configPage2.strokes = FOUR_STROKE;
+
+  RUN_TEST_P(cylinder5_stroke4_seq_even);
+  RUN_TEST_P(cylinder5_stroke4_wasted_even);
+}
+
+static void cylinder6_stroke4_seq_even(void)
+{
+  configPage4.sparkMode = IGN_MODE_SEQUENTIAL;
+  configPage2.engineType = EVEN_FIRE;
+  initialiseAll(); //Run the main initialise function
+#if IGN_CHANNELS >= 6
+  const uint16_t angle[] = {0,120,240,360,480,540,0,0};
+  assert_ignition_schedules(720U, 6U, angle);
+#else
+  const uint16_t angle[] = {0,120,240,0,0,0,0,0};
+  assert_ignition_schedules(360U, 3U, angle);
+#endif
+}
+
+static void cylinder6_stroke4_wasted_even(void)
+{
+  configPage4.sparkMode = IGN_MODE_WASTED;
+  configPage2.engineType = EVEN_FIRE;
+  initialiseAll(); //Run the main initialise function
+  const uint16_t angle[] = {0,120,240,0,0,0,0,0};
+  assert_ignition_schedules(360U, 3U, angle);
+} 
+
+static void run_6_cylinder_4stroke_tests(void)
+{
+  configPage2.nCylinders = 6;
+  configPage2.strokes = FOUR_STROKE;
+
+  RUN_TEST_P(cylinder6_stroke4_seq_even);
+  RUN_TEST_P(cylinder6_stroke4_wasted_even); 
+}
+
+
+static void cylinder8_stroke4_seq_even(void)
+{
+  configPage4.sparkMode = IGN_MODE_SEQUENTIAL;
+  configPage2.engineType = EVEN_FIRE;
+  initialiseAll(); //Run the main initialise function
+#if IGN_CHANNELS >= 8
+  const uint16_t angle[] = {0,90,180,270,360,450,540,630};
+  assert_ignition_schedules(720U, 8U, angle);
+#else
+  const uint16_t angle[] = {0,90,180,270,0,0,0,0};
+  assert_ignition_schedules(360U, 4U, angle);
+#endif
+}
+
+static void cylinder8_stroke4_wasted_even(void)
+{
+  configPage4.sparkMode = IGN_MODE_WASTED;
+  configPage2.engineType = EVEN_FIRE;
+  initialiseAll(); //Run the main initialise function
+  const uint16_t angle[] = {0,90,180,270,0,0,0,0};
+  assert_ignition_schedules(360U, 4U, angle);
+}  
+
+static void run_8_cylinder_4stroke_tests(void)
+{
+  configPage2.nCylinders = 8;
+  configPage2.strokes = FOUR_STROKE;
+
+  RUN_TEST_P(cylinder8_stroke4_seq_even);
+  RUN_TEST_P(cylinder8_stroke4_wasted_even);
+}
+
+void testIgnitionScheduleInit()
+{
+  run_1_cylinder_4stroke_tests();
+  run_2_cylinder_4stroke_tests();
+  run_3_cylinder_4stroke_tests();
+  run_4_cylinder_4stroke_tests();
+  run_5_cylinder_4stroke_tests();
+  run_6_cylinder_4stroke_tests();
+  run_8_cylinder_4stroke_tests();
+}
---
+++ test/test_init/tests_init.cpp
@@ -0,0 +1,232 @@
+#include <unity.h>
+#include "globals.h"
+#include "init.h"
+#include "../test_utils.h"
+
+#define UNKNOWN_PIN 0xFF
+
+#if !defined(NOT_A_PIN)
+#define NOT_A_PIN 0
+#endif
+
+uint8_t getPinMode(uint8_t pin)
+{
+  uint8_t bit = digitalPinToBitMask(pin);
+  uint8_t port = digitalPinToPort(pin);
+
+  // I don't see an option for mega to return this, but whatever...
+  if (NOT_A_PIN == port) return UNKNOWN_PIN;
+
+  // Is there a bit we can check?
+  if (0 == bit) return UNKNOWN_PIN;
+
+  // Is there only a single bit set?
+  if (bit & (bit - 1)) return UNKNOWN_PIN;
+
+  volatile uint8_t *reg, *out;
+  reg = portModeRegister(port);
+  out = portOutputRegister(port);
+
+  if (*reg & bit)
+    return OUTPUT;
+  else if (*out & bit)
+    return INPUT_PULLUP;
+  else
+    return INPUT;
+}
+
+void test_initialisation_complete(void)
+{
+  initialiseAll(); //Run the main initialise function
+  TEST_ASSERT_EQUAL(true, initialisationComplete);
+}
+
+void test_initialisation_ports(void)
+{
+  //Test that all the port values have been set
+  initialiseAll(); //Run the main initialise function
+  TEST_ASSERT_NOT_EQUAL(0, inj1_pin_port);
+  TEST_ASSERT_NOT_EQUAL(0, inj2_pin_port);
+  TEST_ASSERT_NOT_EQUAL(0, inj3_pin_port);
+  TEST_ASSERT_NOT_EQUAL(0, inj4_pin_port);
+  TEST_ASSERT_NOT_EQUAL(0, ign1_pin_port);
+  TEST_ASSERT_NOT_EQUAL(0, ign2_pin_port);
+  TEST_ASSERT_NOT_EQUAL(0, ign3_pin_port);
+  TEST_ASSERT_NOT_EQUAL(0, ign4_pin_port);
+}
+
+//Test that all mandatory output pins have their mode correctly set to output
+void test_initialisation_outputs_V03(void)
+{
+  configPage2.pinMapping = 2; //Set the board number to test
+  initialiseAll(); //Run the main initialise function
+
+  char msg[32];
+  strcpy_P(msg, PSTR("Coil1"));
+  TEST_ASSERT_EQUAL_MESSAGE(OUTPUT, getPinMode(pinCoil1), msg);
+  strcpy_P(msg, PSTR("Coil2"));
+  TEST_ASSERT_EQUAL_MESSAGE(OUTPUT, getPinMode(pinCoil2), msg);
+  strcpy_P(msg, PSTR("Coil3"));
+  TEST_ASSERT_EQUAL_MESSAGE(OUTPUT, getPinMode(pinCoil3), msg);
+  strcpy_P(msg, PSTR("Coil4"));
+  TEST_ASSERT_EQUAL_MESSAGE(OUTPUT, getPinMode(pinCoil4), msg);
+  strcpy_P(msg, PSTR("Injector 1"));
+  TEST_ASSERT_EQUAL_MESSAGE(OUTPUT, getPinMode(pinInjector1), msg);
+  strcpy_P(msg, PSTR("Injector 2"));
+  TEST_ASSERT_EQUAL_MESSAGE(OUTPUT, getPinMode(pinInjector2), msg);
+  strcpy_P(msg, PSTR("Injector 3"));
+  TEST_ASSERT_EQUAL_MESSAGE(OUTPUT, getPinMode(pinInjector3), msg);
+  strcpy_P(msg, PSTR("Injector 4"));
+  TEST_ASSERT_EQUAL_MESSAGE(OUTPUT, getPinMode(pinInjector4), msg);
+  strcpy_P(msg, PSTR("Tacho Out"));
+  TEST_ASSERT_EQUAL_MESSAGE(OUTPUT, getPinMode(pinTachOut), msg);
+  strcpy_P(msg, PSTR("Fuel Pump"));
+  TEST_ASSERT_EQUAL_MESSAGE(OUTPUT, getPinMode(pinFuelPump), msg);
+  strcpy_P(msg, PSTR("Fan"));
+  TEST_ASSERT_EQUAL_MESSAGE(OUTPUT, getPinMode(pinFan), msg);
+}
+
+//Test that all mandatory output pins have their mode correctly set to output
+void test_initialisation_outputs_V04(void)
+{
+  configPage2.pinMapping = 3; //Set the board number to test
+
+  initialiseAll(); //Run the main initialise function
+
+  char msg[32];
+  strcpy_P(msg, PSTR("Coil1"));
+  TEST_ASSERT_EQUAL_MESSAGE(OUTPUT, getPinMode(pinCoil1), msg);
+  strcpy_P(msg, PSTR("Coil2"));
+  TEST_ASSERT_EQUAL_MESSAGE(OUTPUT, getPinMode(pinCoil2), msg);
+  strcpy_P(msg, PSTR("Coil3"));
+  TEST_ASSERT_EQUAL_MESSAGE(OUTPUT, getPinMode(pinCoil3), msg);
+  strcpy_P(msg, PSTR("Coil4"));
+  TEST_ASSERT_EQUAL_MESSAGE(OUTPUT, getPinMode(pinCoil4), msg);
+  strcpy_P(msg, PSTR("Injector 1"));
+  TEST_ASSERT_EQUAL_MESSAGE(OUTPUT, getPinMode(pinInjector1), msg);
+  strcpy_P(msg, PSTR("Injector 2"));
+  TEST_ASSERT_EQUAL_MESSAGE(OUTPUT, getPinMode(pinInjector2), msg);
+  strcpy_P(msg, PSTR("Injector 3"));
+  TEST_ASSERT_EQUAL_MESSAGE(OUTPUT, getPinMode(pinInjector3), msg);
+  strcpy_P(msg, PSTR("Injector 4"));
+  TEST_ASSERT_EQUAL_MESSAGE(OUTPUT, getPinMode(pinInjector4), msg);
+  strcpy_P(msg, PSTR("Tacho Out"));
+  TEST_ASSERT_EQUAL_MESSAGE(OUTPUT, getPinMode(pinTachOut), msg);
+  strcpy_P(msg, PSTR("Fuel Pump"));
+  TEST_ASSERT_EQUAL_MESSAGE(OUTPUT, getPinMode(pinFuelPump), msg);
+  strcpy_P(msg, PSTR("Fan"));
+  TEST_ASSERT_EQUAL_MESSAGE(OUTPUT, getPinMode(pinFan), msg);
+  /*
+  if(isIdlePWM) 
+  {
+    TEST_ASSERT_EQUAL_MESSAGE(OUTPUT, getPinMode(pinIdle1), "Idle 1");
+    TEST_ASSERT_EQUAL_MESSAGE(OUTPUT, getPinMode(pinIdle2), "Idle 2");
+  }
+  else if (isIdleStepper)
+  {
+    TEST_ASSERT_EQUAL_MESSAGE(OUTPUT, getPinMode(pinStepperDir), "Stepper Dir");
+    TEST_ASSERT_EQUAL_MESSAGE(OUTPUT, getPinMode(pinStepperStep), "Stepper Step");
+    TEST_ASSERT_EQUAL_MESSAGE(OUTPUT, getPinMode(pinStepperEnable), "Stepper Enable");
+  }
+  
+  TEST_ASSERT_EQUAL_MESSAGE(OUTPUT, getPinMode(pinFan), "Fan");
+  TEST_ASSERT_EQUAL_MESSAGE(OUTPUT, getPinMode(pinBoost), "Boost");
+  TEST_ASSERT_EQUAL_MESSAGE(OUTPUT, getPinMode(pinVVT_1), "VVT1");
+  TEST_ASSERT_EQUAL_MESSAGE(OUTPUT, getPinMode(pinVVT_2), "VVT2");
+  */
+
+}
+
+//Test that all mandatory output pins have their mode correctly set to output
+void test_initialisation_outputs_MX5_8995(void)
+{
+  configPage2.pinMapping = 9; //Set the board number to test
+  initialiseAll(); //Run the main initialise function
+
+  char msg[32];
+  strcpy_P(msg, PSTR("Coil1"));
+  TEST_ASSERT_EQUAL_MESSAGE(OUTPUT, getPinMode(pinCoil1), msg);
+  strcpy_P(msg, PSTR("Coil2"));
+  TEST_ASSERT_EQUAL_MESSAGE(OUTPUT, getPinMode(pinCoil2), msg);
+  strcpy_P(msg, PSTR("Coil3"));
+  TEST_ASSERT_EQUAL_MESSAGE(OUTPUT, getPinMode(pinCoil3), msg);
+  strcpy_P(msg, PSTR("Coil4"));
+  TEST_ASSERT_EQUAL_MESSAGE(OUTPUT, getPinMode(pinCoil4), msg);
+  strcpy_P(msg, PSTR("Injector 1"));
+  TEST_ASSERT_EQUAL_MESSAGE(OUTPUT, getPinMode(pinInjector1), msg);
+  strcpy_P(msg, PSTR("Injector 2"));
+  TEST_ASSERT_EQUAL_MESSAGE(OUTPUT, getPinMode(pinInjector2), msg);
+  strcpy_P(msg, PSTR("Injector 3"));
+  TEST_ASSERT_EQUAL_MESSAGE(OUTPUT, getPinMode(pinInjector3), msg);
+  strcpy_P(msg, PSTR("Injector 4"));
+  TEST_ASSERT_EQUAL_MESSAGE(OUTPUT, getPinMode(pinInjector4), msg);
+  strcpy_P(msg, PSTR("Tacho Out"));
+  TEST_ASSERT_EQUAL_MESSAGE(OUTPUT, getPinMode(pinTachOut), msg);
+  strcpy_P(msg, PSTR("Fuel Pump"));
+  TEST_ASSERT_EQUAL_MESSAGE(OUTPUT, getPinMode(pinFuelPump), msg);
+  strcpy_P(msg, PSTR("Fan"));
+  TEST_ASSERT_EQUAL_MESSAGE(OUTPUT, getPinMode(pinFan), msg);
+}
+
+void test_initialisation_outputs_PWM_idle(void)
+{
+  configPage2.pinMapping = 3; //Set the board number to test (v0.4)
+  //Force 2 channel PWM idle
+  configPage6.iacChannels = 1;
+  configPage6.iacAlgorithm = 2;
+
+  initialiseAll(); //Run the main initialise function
+
+  bool isIdlePWM = (configPage6.iacAlgorithm > 0) && ((configPage6.iacAlgorithm <= 3) || (configPage6.iacAlgorithm == 6));
+
+  char msg[32];
+  strcpy_P(msg, PSTR("Is PWM Idle"));
+  TEST_ASSERT_TRUE_MESSAGE(isIdlePWM, msg);
+  strcpy_P(msg, PSTR("Idle 1"));
+  TEST_ASSERT_EQUAL_MESSAGE(OUTPUT, getPinMode(pinIdle1), msg);
+  strcpy_P(msg, PSTR("Idle 2"));
+  TEST_ASSERT_EQUAL_MESSAGE(OUTPUT, getPinMode(pinIdle2), msg);
+}
+
+void test_initialisation_outputs_stepper_idle(void)
+{
+  bool isIdleStepper = (configPage6.iacAlgorithm > 3) && (configPage6.iacAlgorithm != 6);
+
+  char msg[32];
+  strcpy_P(msg, PSTR("Is Stepper Idle"));
+  TEST_ASSERT_TRUE_MESSAGE(isIdleStepper, msg);
+  strcpy_P(msg, PSTR("Stepper Dir"));
+  TEST_ASSERT_EQUAL_MESSAGE(OUTPUT, getPinMode(pinStepperDir), msg);
+  strcpy_P(msg, PSTR("Stepper Step"));
+  TEST_ASSERT_EQUAL_MESSAGE(OUTPUT, getPinMode(pinStepperStep), msg);
+  strcpy_P(msg, PSTR("Stepper Enable"));
+  TEST_ASSERT_EQUAL_MESSAGE(OUTPUT, getPinMode(pinStepperEnable), msg);
+}
+
+void test_initialisation_outputs_boost(void)
+{
+  char msg[32];
+  strcpy_P(msg, PSTR("Boost"));
+  TEST_ASSERT_EQUAL_MESSAGE(OUTPUT, getPinMode(pinBoost), msg);
+}
+
+void test_initialisation_outputs_VVT(void)
+{
+  char msg[32];
+  strcpy_P(msg, PSTR("VVT1"));
+  TEST_ASSERT_EQUAL_MESSAGE(OUTPUT, getPinMode(pinVVT_1), msg);
+  strcpy_P(msg, PSTR("VVT2"));
+  TEST_ASSERT_EQUAL_MESSAGE(OUTPUT, getPinMode(pinVVT_2), msg);
+}
+
+void testInitialisation()
+{
+  RUN_TEST_P(test_initialisation_complete);
+  RUN_TEST_P(test_initialisation_ports);
+  RUN_TEST_P(test_initialisation_outputs_V03);
+  RUN_TEST_P(test_initialisation_outputs_V04);
+  RUN_TEST_P(test_initialisation_outputs_MX5_8995);
+  RUN_TEST_P(test_initialisation_outputs_PWM_idle);
+  RUN_TEST_P(test_initialisation_outputs_boost);
+  RUN_TEST_P(test_initialisation_outputs_VVT);
+}
