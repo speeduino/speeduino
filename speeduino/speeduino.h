93899f9fc8b8e3f3db1858388a9e8b91044fd653
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2023-12-22 09:58:27 -0600

    Bug Fix: getAdvance1() & getAdvance2() should return a signed value.

--- speeduino/speeduino.h
+++ speeduino/speeduino.h
@@ -18,8 +18,8 @@
 void setup(void);
 void loop(void);
 uint16_t PW(int REQ_FUEL, byte VE, long MAP, uint16_t corrections, int injOpen);
-byte getVE1(void);
-byte getAdvance1(void);
+uint8_t getVE1(void);
+int8_t getAdvance1(void);
 uint16_t calculatePWLimit();
 void calculateStaging(uint32_t);
 void calculateIgnitionAngles(uint16_t dwellAngle);
