ffab533e7edc16ba64f780c7d05f7067f67d94e7
Josh Stewart <josh@noisymime.org>
2024-01-05 14:52:35 +0900

    Ensure injector end angle is not greater than the maximum inj angle

--- speeduino/speeduino.ino
+++ speeduino/speeduino.ino
@@ -595,6 +595,7 @@ void loop()
       //***********************************************************************************************
       //BEGIN INJECTION TIMING
       currentStatus.injAngle = table2D_getValue(&injectorAngleTable, currentStatus.RPMdiv100);
+      currentStatus.injAngle = max(currentStatus.injAngle, CRANK_ANGLE_MAX_INJ);
       unsigned int PWdivTimerPerDegree = div(currentStatus.PW1, timePerDegree).quot; //How many crank degrees the calculated PW will take at the current speed
 
       injector1StartAngle = calculateInjectorStartAngle(PWdivTimerPerDegree, channel1InjDegrees);
