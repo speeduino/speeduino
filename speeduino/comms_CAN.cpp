63a1a1c4f5ece413b46a18036428a20199b796ee
Josh Stewart <josh@noisymime.org>
2024-02-04 21:36:17 +1100

    Remove duplicate code from last PR merge

--- speeduino/comms_CAN.cpp
+++ speeduino/comms_CAN.cpp
@@ -24,14 +24,6 @@ CAN_message_t outMsg;
   FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> Can1; 
 #endif
 
-//These are declared locally for Teensy due to this issue: https://github.com/tonton81/FlexCAN_T4/issues/67
-#if defined(CORE_TEENSY35)         // use for Teensy 3.5 only 
-  FlexCAN_T4<CAN0, RX_SIZE_256, TX_SIZE_16> Can0;
-#elif defined(CORE_TEENSY41)         // use for Teensy 3.6 only
-  FlexCAN_T4<CAN0, RX_SIZE_256, TX_SIZE_16> Can0;
-  FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> Can1; 
-#endif
-
 // Forward declare
 void DashMessage(uint16_t DashMessageID);
 
