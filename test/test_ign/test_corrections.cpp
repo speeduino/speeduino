8bb2c4f81eb1f50f5e07d0bd15c8e98925d5b0f9
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2024-04-18 00:12:13 -0500

    Optimize correctionIdleAdvance

--- test/test_ign/test_corrections.cpp
+++ test/test_ign/test_corrections.cpp
@@ -332,9 +332,9 @@ static void test_correctionIdleAdvance_ctps_lookup_nodelay(void) {
 
 static void test_correctionIdleAdvance_inactive_notrunning(void) {
     setup_correctionIdleAdvance();
+    
     TEST_ASSERT_EQUAL(23, correctionIdleAdvance(8));
     currentStatus.engineIsRunning = false;
-    TEST_ASSERT_EQUAL(23, correctionIdleAdvance(8));
     TEST_ASSERT_EQUAL(8, correctionIdleAdvance(8));
 }
 
