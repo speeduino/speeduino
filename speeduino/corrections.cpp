f7457b64c886eb55559626881f37aee21859659b
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2024-04-19 15:20:59 -0500

    correctionCLTadvance performance: update at the same frequency as the sensor

--- speeduino/corrections.cpp
+++ speeduino/corrections.cpp
@@ -846,9 +846,12 @@ int8_t correctionFixedTiming(int8_t advance)
  */
 TESTABLE_INLINE_STATIC int8_t correctionCLTadvance(int8_t advance)
 {
-  //Adjust the advance based on CLT.
-  int8_t advanceCLTadjust = (int8_t)(table2D_getValue(&CLTAdvanceTable, temperatureAddOffset(currentStatus.coolant))) - 15;
-  return (advance + advanceCLTadjust);
+  static uint8_t cachedValue = 0U; // Setting this to non-zero will use additional RAM for static initialisation
+  // Performance: only update as fast as the sensor is read
+  if( BIT_CHECK(LOOP_TIMER, CLT_TIMER_BIT)) { 
+    cachedValue = table2D_getValue(&CLTAdvanceTable, temperatureAddOffset(currentStatus.coolant));
+  }
+  return advance + (int8_t)cachedValue - 15;
 }
 
 /** Correct ignition timing to configured fixed value to use during craning.
