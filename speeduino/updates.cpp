2bab6338f5d976775bb1a7b618fe8fc0766cd7b2
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2024-04-20 11:10:35 -0500

    MISRA - apply recently  #defined symbols (IDLEADVANCE_MODE_, IDLEADVANCE_ALGO_, VSS_MODE_), )  across code base

--- speeduino/updates.cpp
+++ speeduino/updates.cpp
@@ -291,7 +291,7 @@ void doUpdates(void)
     configPage4.baroFuelValues[7] = 100;
 
     //Idle advance curve was added. Add default values
-    configPage2.idleAdvEnabled = 0; //Turn this off by default
+    configPage2.idleAdvEnabled = IDLEADVANCE_MODE_OFF; //Turn this off by default
     configPage2.idleAdvTPS = 5; //Active below 5% tps
     configPage2.idleAdvRPM = 20; //Active below 2000 RPM
     configPage4.idleAdvBins[0] = 30;
@@ -372,7 +372,7 @@ void doUpdates(void)
     configPage2.SoftLimitMode = SOFT_LIMIT_FIXED;
 
     //VSS was added for testing, disable it by default
-    configPage2.vssMode = 0;
+    configPage2.vssMode = VSS_MODE_OFF;
 
     writeAllConfig();
     storeEEPROMVersion(14);
