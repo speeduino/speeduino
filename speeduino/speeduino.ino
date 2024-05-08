e92446edbfc8de9731818c7b355e7ef04e1f68b8
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2024-05-08 16:02:31 -0500

    Move deferEEPROMWritesUntil to comms.cpp This is the only plce it's really used And encapsulate it behind a couple of functions.

--- speeduino/speeduino.ino
+++ speeduino/speeduino.ino
@@ -150,7 +150,7 @@ void __attribute__((always_inline)) loop(void)
     if(currentLoopTime > micros())
     {
       //Occurs when micros() has overflowed
-      deferEEPROMWritesUntil = 0; //Required to ensure that EEPROM writes are not deferred indefinitely
+      setStorageWriteTimeout(0); //Required to ensure that EEPROM writes are not deferred indefinitely
     }
 
     currentLoopTime = micros();
@@ -262,7 +262,7 @@ void __attribute__((always_inline)) loop(void)
       #endif
 
       //Check for any outstanding EEPROM writes.
-      if( (isEepromWritePending() == true) && (serialStatusFlag == SERIAL_INACTIVE) && (micros() > deferEEPROMWritesUntil)) { writeAllConfig(); } 
+      if( (isEepromWritePending() == true) && (serialStatusFlag == SERIAL_INACTIVE) && storageWriteTimeoutExpired()) { writeAllConfig(); } 
     }
     if (BIT_CHECK(LOOP_TIMER, BIT_TIMER_15HZ)) //Every 32 loops
     {
