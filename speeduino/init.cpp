c1ee03d25c595797c317132b6c9e3da3d9d2c386
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2024-05-08 10:55:04 -0500

    Move resetConfigPages() to pages.* - it's unrelated to storage. Rename setTuneToEmpty() and zero out *all* of the pages

--- speeduino/init.cpp
+++ speeduino/init.cpp
@@ -21,6 +21,7 @@
 #include "idle.h"
 #include "table2d.h"
 #include "acc_mc33810.h"
+#include "pages.h"
 #include BOARD_H //Note that this is not a real file, it is defined in globals.h. 
 #ifdef SD_LOGGING
   #include "SD_logger.h"
@@ -143,7 +144,8 @@ void initialiseAll(void)
     if((configPage2.pinMapping == 255) || (configPage2.pinMapping == 0)) //255 = EEPROM value in a blank AVR; 0 = EEPROM value in new FRAM
     {
       //First time running on this board
-      resetConfigPages();
+      setTuneToEmpty();
+      configPage4.triggerTeeth = 4; //Avoiddiv by 0 when start decoders
       setPinMapping(3); //Force board to v0.4
     }
     else { setPinMapping(configPage2.pinMapping); }
