2f2b2d7f0171caf4f4548804108b74080310b0f2
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2024-05-09 19:18:04 -0500

    Storage: consistent function naming, esp. prefix (load/store/read/write/save). And Doxygen comments

--- speeduino/comms_legacy.cpp
+++ speeduino/comms_legacy.cpp
@@ -365,7 +365,7 @@ void legacySerialCommand(void)
       tableID = primarySerial.read(); //Not currently used for anything
 
       receiveCalibration(tableID); //Receive new values and store in memory
-      writeCalibrationTables(); //Store received values in EEPROM
+      saveAllCalibrationTables(); //Store received values in EEPROM
 
       break;
 
@@ -537,7 +537,7 @@ void legacySerialHandler(byte cmd, Stream &targetPort, SerialStatus &targetStatu
       if (targetPort.available() >= 2)
       {
         targetPort.read(); //Ignore the first table value, it's always 0
-        writeConfig(targetPort.read());
+        savePage(targetPort.read());
         targetStatusFlag = SERIAL_INACTIVE;
       }
       break;
@@ -548,7 +548,7 @@ void legacySerialHandler(byte cmd, Stream &targetPort, SerialStatus &targetStatu
       if (targetPort.available() >= 2)
       {
         targetPort.read(); //Ignore the first table value, it's always 0
-        writeConfig(targetPort.read());
+        savePage(targetPort.read());
         targetStatusFlag = SERIAL_INACTIVE;
       }
       break;
@@ -1221,11 +1221,11 @@ void receiveCalibration(byte tableID)
       
       pTargetTable->values[x] = temperatureAddOffset(tempValue);
       pTargetTable->axis[x] = (x * 32U);
-      writeCalibrationTables();
+      saveAllCalibrationTables();
     }
   }
 
-  writeCalibrationTables();
+  saveAllCalibrationTables();
 }
 
 /** Send 256 tooth log entries to primarySerial.
