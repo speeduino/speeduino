fa80a1f76e8852e5cf71814e8d4b192dab9ef64f
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2024-11-11 15:42:05 -0600

    Extract percentApprox from PW calculations, optimize and unit test.

--- test/test_math/test_fp_support.h
+++ test/test_math/test_fp_support.h
@@ -1,4 +1,4 @@
 #pragma once
 #include <stdint.h>
 
-void assert_rounded_div(int32_t a, int32_t b, int32_t actual);
+void assert_rounded_div(int32_t a, int32_t b, int32_t actual, uint8_t delta = 0);
