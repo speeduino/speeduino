fc1a0dd9bdbe78f154b535fe18747dea8386ece1
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2024-11-26 14:45:35 -0600

    Move revolutionTime into statuses - will shorten parameter lists

--- speeduino/pw_calcs.h
+++ speeduino/pw_calcs.h
@@ -23,7 +23,7 @@ struct pulseWidths {
 };
 
 uint16_t calculateRequiredFuel(const config2 &page2, const statuses &current);
-uint16_t calculatePWLimit(const config2 &page2, const statuses &current, uint32_t revTime);
+uint16_t calculatePWLimit(const config2 &page2, const statuses &current);
 uint16_t PW(int REQ_FUEL, byte VE, long MAP, uint16_t corrections, int injOpen, const config10 &page10, const statuses &current);
 
 // Apply the pwLimit if staging is disabled and engine is not cranking
