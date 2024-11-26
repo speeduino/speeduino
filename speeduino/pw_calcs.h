49ef9dbc03e687af1508066a9ce3d4cbc933f9a1
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2024-11-26 15:00:13 -0600

    Bring PW calc orchestration togther into computePulseWidths()

--- speeduino/pw_calcs.h
+++ speeduino/pw_calcs.h
@@ -22,28 +22,6 @@ struct pulseWidths {
   uint16_t secondary;
 };
 
-uint16_t calculateRequiredFuel(const config2 &page2, const statuses &current);
-uint16_t calculatePWLimit(const config2 &page2, const statuses &current);
-uint16_t PW(int REQ_FUEL, byte VE, long MAP, uint16_t corrections, int injOpen, const config10 &page10, const statuses &current);
+pulseWidths computePulseWidths(byte VE, long MAP, uint16_t corrections, const config2 &page2, const config10 &page10, statuses &current);
 
-// Apply the pwLimit if staging is disabled and engine is not cranking
-static inline uint16_t applyPwLimits(uint16_t pw, uint16_t pwLimit, uint16_t injOpenTime, const config10 &page10, const statuses &current) {
-  if (pw<=injOpenTime) {
-    return 0U;
-  }
-  if( !current.engineIsCranking && (page10.stagingEnabled == false) ) { 
-    return min(pw, pwLimit);
-  }
-  return pw;
-}
-
-static inline bool canApplyStaging(const config2 &page2, const config10 &page10) {
-    //To run staged injection, the number of cylinders must be less than the injector channels (ie Assuming you're running paired injection, you need at least as many injector channels as you have cylinders, half for the primaries and half for the secondaries)
- return  (page10.stagingEnabled == true) 
-      && (page2.nCylinders <= INJ_CHANNELS || page2.injType == INJ_TYPE_TBODY); //Final check is to ensure that DFCO isn't active, which would cause an overflow below (See #267)  
-}
-
-pulseWidths calculateSecondaryPw(uint16_t primaryPw, uint16_t pwLimit, uint16_t injOpenTime, const config2 &page2, const config10 &page10, const statuses &current);
 void applyPwToInjectorChannels(const pulseWidths &pulse_widths, const config2 &page2, statuses &current);
-
-uint16_t calculateOpenTime(const config2 &page2, const statuses &current);
