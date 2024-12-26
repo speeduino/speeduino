3f8c347b84111c6097c3ab49dfe453e2c6230f63
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2024-12-26 15:43:55 -0600

    Start the schedule timers externally And never turn them off - they were pretty much on continuosuly anyway.
    Saves 36 bytes

--- speeduino/init.cpp
+++ speeduino/init.cpp
@@ -191,7 +191,8 @@ void initialiseAll(void)
     //Set the tacho output default state
     digitalWrite(pinTachOut, HIGH);
     //Perform all initialisations
-    initialiseSchedulers();
+    initialiseFuelSchedulers();
+    initialiseIgnitionSchedulers();
     //initialiseDisplay();
     initialiseIdle(true);
     initialiseFan();
@@ -3592,6 +3593,10 @@ void initialiseTriggers(void)
     //Teensy 4 requires a HYSTERESIS flag to be set on any external interrupt pins to prevent false interrupts
     setTeensy41PinsHysteresis();
   #endif
+  
+  // The schedulers are all configured & pins are mapped - so start the schedulers
+  startIgnitionSchedulers();
+  startFuelSchedulers();
 }
 
 static inline bool isAnyFuelScheduleRunning(void) {
