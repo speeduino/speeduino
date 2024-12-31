a11d43a4b41887e1716b5c5ca69ae885804c978a
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2024-12-31 14:45:42 -0600

    Secondary table tests - no global state

--- test/test_secondary/test_secondary_fuel.cpp
+++ test/test_secondary/test_secondary_fuel.cpp
@@ -5,30 +5,8 @@
 #include "../test_utils.h"
 #include "storage.h"
 
-
-TEST_DATA_P table3d_value_t fuel2TableValues[] = {
- //0    1    2   3     4    5    6    7    8    9   10   11   12   13    14   15
-34,  34,  34,  34,  34,  34,  34,  34,  34,  35,  35,  35,  35,  35,  35,  35, 
-34,  35,  36,  37,  39,  41,  42,  43,  43,  44,  44,  44,  44,  44,  44,  44, 
-35,  36,  38,  41,  44,  46,  47,  48,  48,  49,  49,  49,  49,  49,  49,  49, 
-36,  39,  42,  46,  50,  51,  52,  53,  53,  53,  53,  53,  53,  53,  53,  53, 
-38,  43,  48,  52,  55,  56,  57,  58,  58,  58,  58,  58,  58,  58,  58,  58, 
-42,  49,  54,  58,  61,  62,  62,  63,  63,  63,  63,  63,  63,  63,  63,  63, 
-48,  56,  60,  64,  66,  66,  68,  68,  68,  68,  68,  68,  68,  68,  68,  68, 
-54,  62,  66,  69,  71,  71,  72,  72,  72,  72,  72,  72,  72,  72,  72,  72, 
-61,  69,  72,  74,  76,  76,  77,  77,  77,  77,  77,  77,  77,  77,  77,  77, 
-68,  75,  78,  79,  81,  81,  81,  82,  82,  82,  82,  82,  82,  82,  82,  82, 
-74,  80,  83,  84,  85,  86,  86,  86,  87,  87,  87,  87,  87,  87,  87,  87, 
-81,  86,  88,  89,  90,  91,  91,  91,  91,  91,  91,  91,  91,  91,  91,  91, 
-93,  96,  98,  99,  99,  100, 100, 101, 101, 101, 101, 101, 101, 101, 101, 101, 
-98,  101, 103, 103, 104, 105, 105, 105, 105, 105, 105, 105, 105, 105, 105, 105, 
-104, 106, 107, 108, 109, 109, 110, 110, 110, 110, 110, 110, 110, 110, 110, 110, 
-150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150,
-  };
 TEST_DATA_P table3d_axis_t tempXAxis[] = {500, 700, 900, 1200, 1600, 2000, 2500, 3100, 3500, 4100, 4700, 5300, 5900, 6500, 6750, 7000};
 TEST_DATA_P table3d_axis_t tempYAxis[] = {16, 26, 30, 36, 40, 46, 50, 56, 60, 66, 70, 76, 86, 90, 96, 100};
-
-static table3d16RpmLoad veLookupTable;
 
 static void __attribute__((noinline)) assert_2nd_fuel_is_off(const statuses &current, uint8_t expectedVE) {
     TEST_ASSERT_BIT_LOW(BIT_STATUS3_FUEL2_ACTIVE, current.status3);
@@ -47,20 +25,28 @@ static void __attribute__((noinline)) assert_2nd_fuel_is_on(const statuses &curr
 static void test_no_secondary_fuel(void) {
     config10 page10 = {};
     statuses current = {};
+    table3d16RpmLoad lookupTable;
 
     page10.fuel2Mode = FUEL2_MODE_OFF;
     page10.fuel2Algorithm = LOAD_SOURCE_MAP;
     current.VE1 = 50;
     current.VE = current.VE1;
 
-    calculateSecondaryFuel(page10, veLookupTable, current);
+    calculateSecondaryFuel(page10, lookupTable, current);
 
     assert_2nd_fuel_is_off(current, 50);
 }
 
+static constexpr int16_t SIMPLE_LOAD_VALUE = 150;
+
 static void __attribute__((noinline)) test_fuel_mode_cap_UINT8_MAX(uint8_t mode) {
     config10 page10 = {};
     statuses current = {};
+    table3d16RpmLoad lookupTable;
+
+    fill_table_values(lookupTable, SIMPLE_LOAD_VALUE);
+    populate_table_axis_P(lookupTable.axisX.begin(), tempXAxis);
+    populate_table_axis_P(lookupTable.axisY.begin(), tempYAxis);
 
     page10.fuel2Mode = mode;    
     page10.fuel2Algorithm = LOAD_SOURCE_MAP;
@@ -69,26 +55,33 @@ static void __attribute__((noinline)) test_fuel_mode_cap_UINT8_MAX(uint8_t mode)
     current.MAP = 100; //Load source value
     current.RPM = 7000;
 
-    calculateSecondaryFuel(page10, veLookupTable, current);
+    calculateSecondaryFuel(page10, lookupTable, current);
 
     assert_2nd_fuel_is_on(current, 200, 150, UINT8_MAX);
 }
 
 static constexpr int8_t SIMPLE_VE1 = 75;
-static constexpr int16_t SIMPLE_LOAD_VALUE = 150;
 
-static void __attribute__((noinline)) test_fuel_mode_simple(uint8_t mode, uint8_t expectedVE) {
-    config10 page10 = {};
-    statuses current = {};
+static void __attribute__((noinline)) setup_test_fuel_mode_simple(config10 &page10, statuses &current, table3d16RpmLoad &lookupTable, uint8_t mode) {
+    fill_table_values(lookupTable, SIMPLE_LOAD_VALUE);
+    populate_table_axis_P(lookupTable.axisX.begin(), tempXAxis);
+    populate_table_axis_P(lookupTable.axisY.begin(), tempYAxis);
 
     page10.fuel2Mode = mode;    
     page10.fuel2Algorithm = LOAD_SOURCE_MAP;
     current.VE1 = SIMPLE_VE1;
     current.VE = current.VE1;
-    current.MAP = 100; //Load source value
-    current.RPM = 7000;
+    current.MAP = tempYAxis[0]; //Load source value
+    current.RPM = tempXAxis[0];
+}
+static void __attribute__((noinline)) test_fuel_mode_simple(uint8_t mode, uint8_t expectedVE) {
+    config10 page10 = {};
+    statuses current = {};
+    table3d16RpmLoad lookupTable;
 
-    calculateSecondaryFuel(page10, veLookupTable, current);
+    setup_test_fuel_mode_simple(page10, current, lookupTable, mode);
+
+    calculateSecondaryFuel(page10, lookupTable, current);
 
     assert_2nd_fuel_is_on(current, SIMPLE_VE1, SIMPLE_LOAD_VALUE, expectedVE);
 }
@@ -112,13 +105,12 @@ static void test_fuel_mode_add_cap_UINT8_MAX(void) {
 static constexpr int16_t SWITCHED_LOAD = 50;
 static constexpr int16_t SWITCHED_VE2 = 68;
 
-static void __attribute__((noinline)) setup_test_fuel_mode_cond_switch(config10 &page10, statuses &current, uint8_t cond, uint16_t trigger) {
-    page10.fuel2Mode = FUEL2_MODE_CONDITIONAL_SWITCH; 
+static void __attribute__((noinline)) setup_test_fuel_mode_cond_switch(config10 &page10, statuses &current, table3d16RpmLoad &lookupTable, uint8_t cond, uint16_t trigger) {
+    setup_test_fuel_mode_simple(page10, current, lookupTable, FUEL2_MODE_CONDITIONAL_SWITCH);
+    fill_table_values(lookupTable, SWITCHED_VE2);
     page10.fuel2SwitchVariable = cond;
     page10.fuel2SwitchValue = trigger;
-    page10.fuel2Algorithm = LOAD_SOURCE_MAP;    
-    current.VE1 = SIMPLE_VE1;
-    current.VE = current.VE1;
+
     current.MAP = SWITCHED_LOAD; //Load source value
     current.RPM = 3500;
     current.TPS = 50;
@@ -128,9 +120,10 @@ static void __attribute__((noinline)) setup_test_fuel_mode_cond_switch(config10 
 static void __attribute__((noinline)) test_fuel_mode_cond_switch_negative(uint8_t cond, uint16_t trigger) {
     config10 page10 = {};
     statuses current = {};
-    setup_test_fuel_mode_cond_switch(page10, current, cond, trigger);
+    table3d16RpmLoad lookupTable;
+    setup_test_fuel_mode_cond_switch(page10, current, lookupTable, cond, trigger);
 
-    calculateSecondaryFuel(page10, veLookupTable, current);
+    calculateSecondaryFuel(page10, lookupTable, current);
 
     assert_2nd_fuel_is_off(current, SIMPLE_VE1);    
 }
@@ -138,9 +131,10 @@ static void __attribute__((noinline)) test_fuel_mode_cond_switch_negative(uint8_
 static void __attribute__((noinline)) test_fuel_mode_cond_switch_positive(uint8_t cond, uint16_t trigger) {
     config10 page10 = {};
     statuses current = {};
-    setup_test_fuel_mode_cond_switch(page10, current, cond, trigger);
+    table3d16RpmLoad lookupTable;
+    setup_test_fuel_mode_cond_switch(page10, current, lookupTable, cond, trigger);
 
-    calculateSecondaryFuel(page10, veLookupTable, current);
+    calculateSecondaryFuel(page10, lookupTable, current);
 
     assert_2nd_fuel_is_on(current, SIMPLE_VE1, SWITCHED_VE2, SWITCHED_VE2);
 }
@@ -172,38 +166,33 @@ static void __attribute__((noinline)) test_fuel_mode_cond_switch_ethanol_pct(voi
 static void __attribute__((noinline)) test_fuel_mode_input_switch(void) {
     config10 page10 = {};
     statuses current = {};
+    table3d16RpmLoad lookupTable;
+    setup_test_fuel_mode_simple(page10, current, lookupTable, FUEL2_MODE_INPUT_SWITCH);
+    fill_table_values(lookupTable, SWITCHED_VE2);
 
-    page10.fuel2Mode = FUEL2_MODE_INPUT_SWITCH; 
-    page10.fuel2Algorithm = LOAD_SOURCE_MAP;
-    current.VE1 = SIMPLE_VE1;
-    current.VE = current.VE1;
-    current.MAP = SWITCHED_LOAD; //Load source value
-    current.RPM = 3500;
     page10.fuel2InputPolarity = HIGH;
     pinFuel2Input = 3;   
     pinMode(pinFuel2Input, OUTPUT);
 
     // On
     digitalWrite(pinFuel2Input, page10.fuel2InputPolarity);
-    calculateSecondaryFuel(page10, veLookupTable, current);
+    calculateSecondaryFuel(page10, lookupTable, current);
     assert_2nd_fuel_is_on(current, SIMPLE_VE1, SWITCHED_VE2, SWITCHED_VE2);
 
     // Off
     digitalWrite(pinFuel2Input, !page10.fuel2InputPolarity);
     current.VE = current.VE1;
-    calculateSecondaryFuel(page10, veLookupTable, current);
+    calculateSecondaryFuel(page10, lookupTable, current);
     assert_2nd_fuel_is_off(current, SIMPLE_VE1);
 
     // On again
     digitalWrite(pinFuel2Input, page10.fuel2InputPolarity);
-    calculateSecondaryFuel(page10, veLookupTable, current);
+    calculateSecondaryFuel(page10, lookupTable, current);
     assert_2nd_fuel_is_on(current, SIMPLE_VE1, SWITCHED_VE2, SWITCHED_VE2);
 }
 
 void test_calculateSecondaryFuel(void)
 {
-    populate_table_P(veLookupTable, tempXAxis, tempYAxis, fuel2TableValues);
-
     SET_UNITY_FILENAME() {
         RUN_TEST(test_no_secondary_fuel);
         RUN_TEST(test_fuel_mode_multiply);
--- test/test_secondary/test_secondary_spark.cpp
+++ test/test_secondary/test_secondary_spark.cpp
@@ -6,30 +6,8 @@
 #include "storage.h"
 #include "corrections.h"
 
-
-TEST_DATA_P table3d_value_t values[] = {
- //0    1    2   3     4    5    6    7    8    9   10   11   12   13    14   15
-34,  34,  34,  34,  34,  34,  34,  34,  34,  35,  35,  35,  35,  35,  35,  35, 
-34,  35,  36,  37,  39,  41,  42,  43,  43,  44,  44,  44,  44,  44,  44,  44, 
-35,  36,  38,  41,  44,  46,  47,  48,  48,  49,  49,  49,  49,  49,  49,  49, 
-36,  39,  42,  46,  50,  51,  52,  53,  53,  53,  53,  53,  53,  53,  53,  53, 
-38,  43,  48,  52,  55,  56,  57,  58,  58,  58,  58,  58,  58,  58,  58,  58, 
-42,  49,  54,  58,  61,  62,  62,  63,  63,  63,  63,  63,  63,  63,  63,  63, 
-48,  56,  60,  64,  66,  66,  68,  68,  68,  68,  68,  68,  68,  68,  68,  68, 
-54,  62,  66,  69,  71,  71,  72,  72,  72,  72,  72,  72,  72,  72,  72,  72, 
-61,  69,  72,  74,  76,  76,  77,  77,  77,  77,  77,  77,  77,  77,  77,  77, 
-68,  75,  78,  79,  81,  81,  81,  82,  82,  82,  82,  82,  82,  82,  82,  82, 
-74,  80,  83,  84,  85,  86,  86,  86,  87,  87,  87,  87,  87,  87,  87,  87, 
-81,  86,  88,  89,  90,  91,  91,  91,  91,  91,  91,  91,  91,  91,  91,  91, 
-93,  96,  98,  99,  99,  107, 107, 101, 101, 101, 101, 101, 101, 101, 101, 101, 
-98,  101, 103, 103, 104, 105, 105, 105, 105, 105, 105, 105, 105, 105, 105, 105, 
-104, 106, 107, 108, 109, 109, 110, 110, 110, 110, 110, 110, 110, 110, 110, 110, 
-150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150,
-  };
 TEST_DATA_P table3d_axis_t tempXAxis[] = {500, 700, 900, 1200, 1600, 2000, 2500, 3100, 3500, 4100, 4700, 5300, 5900, 6500, 6750, 7000};
 TEST_DATA_P table3d_axis_t tempYAxis[] = {16, 26, 30, 36, 40, 46, 50, 56, 60, 66, 70, 76, 86, 90, 96, 100};
-
-static table3d16RpmLoad lookupTable;
 
 static void __attribute__((noinline)) assert_2nd_spark_is_off(const statuses &current, int8_t expectedAdvance) {
     TEST_ASSERT_BIT_LOW(BIT_STATUS5_SPARK2_ACTIVE, current.status5);
@@ -49,6 +27,7 @@ static void __attribute__((noinline)) test_mode_off_no_secondary_spark(void) {
     config2 page2 = {};
     config10 page10 = {};
     statuses current = {};
+    table3d16RpmLoad lookupTable;
 
     page10.spark2Mode = SPARK2_MODE_OFF;
     page10.spark2Algorithm = LOAD_SOURCE_MAP;
@@ -61,26 +40,28 @@ static void __attribute__((noinline)) test_mode_off_no_secondary_spark(void) {
 }
 
 static constexpr int8_t CAP_ADVANCE1 = INT8_MAX - 1;
-static constexpr int16_t CAP_LOAD_LOOKUP_MAP = 100;
-static constexpr uint16_t CAP_LOAD_LOOKUP_RPM = 7000;
 static constexpr int16_t CAP_LOAD_LOOKUP_RESULT = 150;
 static constexpr int16_t CAP_LOAD_VALUE = CAP_LOAD_LOOKUP_RESULT-INT16_C(OFFSET_IGNITION);
 
-static void __attribute__((noinline)) setup_test_mode_cap_INT8_MAX(config2 &, config10 &page10, statuses &current, uint8_t mode) {
+static void __attribute__((noinline)) setup_test_mode_cap_INT8_MAX(config2 &, config10 &page10, statuses &current, table3d16RpmLoad &lookupTable, uint8_t mode) {
     page10.spark2Mode = mode;    
     page10.spark2Algorithm = LOAD_SOURCE_MAP;
     current.advance1 = CAP_ADVANCE1;
     current.advance = current.advance1;
-    current.MAP = CAP_LOAD_LOOKUP_MAP;
-    current.RPM = CAP_LOAD_LOOKUP_RPM;
+    current.MAP = tempYAxis[0];
+    current.RPM = tempXAxis[0];
+    fill_table_values(lookupTable, CAP_LOAD_LOOKUP_RESULT);
+    populate_table_axis_P(lookupTable.axisX.begin(), tempXAxis);
+    populate_table_axis_P(lookupTable.axisY.begin(), tempYAxis);
 }
 
 static void __attribute__((noinline)) test_mode_cap_INT8_MAX(uint8_t mode, int8_t expectedAdvance2) {
     config2 page2 = {};
     config10 page10 = {};
     statuses current = {};
+    table3d16RpmLoad lookupTable;
 
-    setup_test_mode_cap_INT8_MAX(page2, page10, current, mode);
+    setup_test_mode_cap_INT8_MAX(page2, page10, current, lookupTable, mode);
 
     calculateSecondarySpark(page2, page10, lookupTable, current);
 
@@ -88,26 +69,28 @@ static void __attribute__((noinline)) test_mode_cap_INT8_MAX(uint8_t mode, int8_
 }
 
 static constexpr int8_t SIMPLE_ADVANCE1 = 75;
-static constexpr int16_t SIMPLE_LOAD_LOOKUP_MAP = 50;
-static constexpr uint16_t SIMPLE_LOAD_LOOKUP_RPM = 3500;
 static constexpr int16_t SIMPLE_LOAD_LOOKUP_RESULT = 68;
 static constexpr int16_t SIMPLE_LOAD_VALUE = SIMPLE_LOAD_LOOKUP_RESULT-INT16_C(OFFSET_IGNITION);
 
-static void __attribute__((noinline)) setup_test_mode_simple(config2 &, config10 &page10, statuses &current, uint8_t mode) {
+static void __attribute__((noinline)) setup_test_mode_simple(config2 &, config10 &page10, statuses &current, table3d16RpmLoad &lookupTable, uint8_t mode) {
     page10.spark2Mode = mode;    
     page10.spark2Algorithm = LOAD_SOURCE_MAP;
     current.advance1 = SIMPLE_ADVANCE1;
     current.advance = current.advance1;
-    current.MAP = SIMPLE_LOAD_LOOKUP_MAP;
-    current.RPM = SIMPLE_LOAD_LOOKUP_RPM;
+    current.MAP = tempYAxis[0];
+    current.RPM = tempXAxis[0];
+    fill_table_values(lookupTable, SIMPLE_LOAD_LOOKUP_RESULT);
+    populate_table_axis_P(lookupTable.axisX.begin(), tempXAxis);
+    populate_table_axis_P(lookupTable.axisY.begin(), tempYAxis);
 }
 
 static void __attribute__((noinline)) test_mode_simple(uint8_t mode, int8_t expectedAdvance, int8_t expectedAdvance2) {
     config2 page2 = {};
     config10 page10 = {};
     statuses current = {};
+    table3d16RpmLoad lookupTable;
 
-    setup_test_mode_simple(page2, page10, current, mode);
+    setup_test_mode_simple(page2, page10, current, lookupTable, mode);
     calculateSecondarySpark(page2, page10, lookupTable, current);
     assert_2nd_spark_is_on(current, SIMPLE_ADVANCE1, expectedAdvance2, expectedAdvance);
 }
@@ -124,7 +107,9 @@ static void __attribute__((noinline)) test_fixed_timing_no_secondary_spark(void)
     config2 page2 = {};
     config10 page10 = {};
     statuses current = {};
-    setup_test_mode_simple(page2, page10, current, SPARK2_MODE_MULTIPLY);
+    table3d16RpmLoad lookupTable;
+
+    setup_test_mode_simple(page2, page10, current, lookupTable, SPARK2_MODE_MULTIPLY);
     page2.fixAngEnable = 1U;// Should turn 2nd table off
     calculateSecondarySpark(page2, page10, lookupTable, current);
     assert_2nd_spark_is_off(current, SIMPLE_ADVANCE1);
@@ -134,7 +119,9 @@ static void __attribute__((noinline)) test_cranking_no_secondary_spark(void) {
     config2 page2 = {};
     config10 page10 = {};
     statuses current = {};
-    setup_test_mode_simple(page2, page10, current, SPARK2_MODE_MULTIPLY);
+    table3d16RpmLoad lookupTable;
+
+    setup_test_mode_simple(page2, page10, current, lookupTable, SPARK2_MODE_MULTIPLY);
     BIT_SET(current.engine, BIT_ENGINE_CRANK);// Should turn 2nd table off
     calculateSecondarySpark(page2, page10, lookupTable, current);
     assert_2nd_spark_is_off(current, SIMPLE_ADVANCE1);
@@ -148,13 +135,10 @@ static void __attribute__((noinline)) test_sparkmode_add_cap_INT8_MAX(void) {
     test_mode_cap_INT8_MAX(SPARK2_MODE_ADD, (int16_t)150-INT16_C(OFFSET_IGNITION));
 }
 
-static void __attribute__((noinline)) setup_test_mode_cond_switch(config2 &, config10 &page10, statuses &current, uint8_t cond, uint16_t trigger) {
-    page10.spark2Mode = SPARK2_MODE_CONDITIONAL_SWITCH; 
+static void __attribute__((noinline)) setup_test_mode_cond_switch(config2 &page2, config10 &page10, statuses &current, table3d16RpmLoad &lookupTable, uint8_t cond, uint16_t trigger) {
+    setup_test_mode_simple(page2, page10, current, lookupTable, SPARK2_MODE_CONDITIONAL_SWITCH);
     page10.spark2SwitchVariable = cond;
     page10.spark2SwitchValue = trigger;
-    page10.spark2Algorithm = LOAD_SOURCE_MAP;    
-    current.advance1 = SIMPLE_ADVANCE1;
-    current.advance = current.advance1;
     current.MAP = 50; //Load source value
     current.RPM = 3500;
     current.TPS = 50;
@@ -165,8 +149,9 @@ static void __attribute__((noinline)) test_sparkmode_cond_switch_negative(uint8_
     config2 page2 = {};
     config10 page10 = {};
     statuses current = {};
+    table3d16RpmLoad lookupTable;
 
-    setup_test_mode_cond_switch(page2, page10, current, cond, trigger);
+    setup_test_mode_cond_switch(page2, page10, current, lookupTable, cond, trigger);
 
     calculateSecondarySpark(page2, page10, lookupTable, current);
 
@@ -177,8 +162,9 @@ static void __attribute__((noinline)) test_sparkmode_cond_switch_positive(uint8_
     config2 page2 = {};
     config10 page10 = {};
     statuses current = {};
+    table3d16RpmLoad lookupTable;
 
-    setup_test_mode_cond_switch(page2, page10, current, cond, trigger);
+    setup_test_mode_cond_switch(page2, page10, current, lookupTable, cond, trigger);
 
     calculateSecondarySpark(page2, page10, lookupTable, current);
 
@@ -213,13 +199,10 @@ static void __attribute__((noinline)) test_sparkmode_input_switch(void) {
     config2 page2 = {};
     config10 page10 = {};
     statuses current = {};
+    table3d16RpmLoad lookupTable;
 
-    page10.spark2Mode = SPARK2_MODE_INPUT_SWITCH; 
-    page10.spark2Algorithm = LOAD_SOURCE_MAP;
-    current.advance1 = SIMPLE_ADVANCE1;
-    current.advance = current.advance1;
-    current.MAP = 50; //Load source value
-    current.RPM = 3500;
+    setup_test_mode_simple(page2, page10, current, lookupTable, SPARK2_MODE_INPUT_SWITCH);
+
     page10.spark2InputPolarity = HIGH;
     pinSpark2Input = 3;   
     pinMode(pinSpark2Input, OUTPUT);
@@ -243,8 +226,6 @@ static void __attribute__((noinline)) test_sparkmode_input_switch(void) {
 
 void test_calculateSecondarySpark(void)
 {
-    populate_table_P(lookupTable, tempXAxis, tempYAxis, values);
-
     SET_UNITY_FILENAME() {
         RUN_TEST(test_mode_off_no_secondary_spark);
         RUN_TEST(test_fixed_timing_no_secondary_spark);
