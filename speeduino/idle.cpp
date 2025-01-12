f0bd3d5fb7a118a0ce2fdb6a4e6d3998a35112c1
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2025-01-12 09:54:14 -0600

    Convert statuses::status2: to bit fields

--- speeduino/idle.cpp
+++ speeduino/idle.cpp
@@ -371,10 +371,12 @@ static inline void doStep(void)
     idleStepper.stepperStatus = STEPPING;
     idleOn = true;
 
-    BIT_SET(currentStatus.status2, BIT_STATUS2_IDLE);
+    currentStatus.idleOn = true;
   }
   else
-    BIT_CLEAR(currentStatus.status2, BIT_STATUS2_IDLE);
+  {
+    currentStatus.idleOn = false;
+  }
 }
 
 /*
@@ -438,14 +440,14 @@ void idleControl(void)
       {
         IDLE_PIN_HIGH();
         idleOn = true;
-        BIT_SET(currentStatus.status2, BIT_STATUS2_IDLE); //Turn the idle control flag on
+        currentStatus.idleOn = true;
 		    currentStatus.idleLoad = 100;
       }
       else if (idleOn)
       {
         IDLE_PIN_LOW();
         idleOn = false; 
-        BIT_CLEAR(currentStatus.status2, BIT_STATUS2_IDLE); //Turn the idle control flag on
+        currentStatus.idleOn = false;
 		    currentStatus.idleLoad = 0;
       }
       break;
@@ -761,7 +763,7 @@ void idleControl(void)
   {
     if(currentStatus.idleLoad >= 100)
     {
-      BIT_SET(currentStatus.status2, BIT_STATUS2_IDLE); //Turn the idle control flag on
+      currentStatus.idleOn = true;
       IDLE_TIMER_DISABLE();
       if (configPage6.iacPWMdir == 0)
       {
@@ -782,7 +784,7 @@ void idleControl(void)
     }
     else
     {
-      BIT_SET(currentStatus.status2, BIT_STATUS2_IDLE); //Turn the idle control flag on
+      currentStatus.idleOn = true;
       IDLE_TIMER_ENABLE();
     }
   }
@@ -828,7 +830,7 @@ void disableIdle(void)
         idle_pid_target_value = idleStepper.targetIdleStep<<2;
     }
   }
-  BIT_CLEAR(currentStatus.status2, BIT_STATUS2_IDLE); //Turn the idle control flag off
+  currentStatus.idleOn = false;
   currentStatus.idleLoad = 0;
 }
 
