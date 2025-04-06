7b75b8cb6004566e3b86c8d1e0cb6347ab6eb116
Josh Stewart <josh@noisymime.org>
2025-04-07 07:28:34 +1000

    Signatures for 202402.4

--- speeduino/comms_legacy.cpp
+++ speeduino/comms_legacy.cpp
@@ -655,7 +655,7 @@ void legacySerialHandler(byte cmd, Stream &targetPort, SerialStatus &targetStatu
       break;
 
     case 'S': // send code version
-      targetPort.print(F("Speeduino 202402.3"));
+      targetPort.print(F("Speeduino 202402.4"));
       break;
   }
 }
