88a70112da3943dd2a2ed747e467ebc2f558777e
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2025-06-11 09:40:31 -0500

    Add fast_map() - break out 8-bit interpolation into it's own method

--- speeduino/table2d.cpp
+++ speeduino/table2d.cpp
@@ -5,6 +5,7 @@ A full copy of the license may be found in the projects root directory
 */
 
 #include "table2d.h"
+#include "maths.h"
 #if !defined(UNIT_TEST)
 #include "globals.h"
 #endif
@@ -17,20 +18,6 @@ uint8_t _table2d_detail::getCacheTime(void) {
 #endif
 }
 
-
 uint8_t _table2d_detail::interpolate(const uint8_t axisValue, const Bin<uint8_t> &axisBin, const Bin<uint8_t> &valueBin) {
-  // Using uint16_t to avoid overflow when calculating the result
-  uint16_t m = axisValue - axisBin.lowerValue();
-  uint8_t xRange = axisBin.upperValue() - axisBin.lowerValue();
-
-  uint8_t yMax = valueBin.upperValue();
-  uint8_t yMin = valueBin.lowerValue();
-  
-  if (yMin>yMax) {
-    uint8_t yRange = yMin-yMax;
-    return yMin - ((m * yRange) / xRange);
-  } else {
-    uint8_t yRange = yMax-yMin;
-    return ((m * yRange) / xRange) + yMin;
-  }
+  return ::fast_map(axisValue, axisBin.lowerValue(), axisBin.upperValue(), valueBin.lowerValue(), valueBin.upperValue());
 }
