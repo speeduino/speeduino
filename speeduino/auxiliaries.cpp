9d6e315e6df47e527a1df6756deca5ca9d0c4428
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2025-06-16 23:32:04 -0500

    Header dependencies: move helper pre-processor macros into their own header file

--- speeduino/auxiliaries.cpp
+++ speeduino/auxiliaries.cpp
@@ -9,8 +9,8 @@ A full copy of the license may be found in the projects root directory
 #include "src/PID_v1/PID_v1.h"
 #include "decoders.h"
 #include "timers.h"
-#include "utilities.h"
 #include "units.h"
+#include "preprocessor.h"
 
 static long vvt1_pwm_value;
 static long vvt2_pwm_value;
