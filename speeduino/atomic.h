f534200786c44572ad423fe8aac634feca60d8ef
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2025-11-09 06:19:39 -0600

    Fix CI build - round 1

--- speeduino/atomic.h
+++ speeduino/atomic.h
@@ -2,16 +2,6 @@
 
 #if !defined(ATOMIC)
 
-#if __has_include(<SimplyAtomic.h>)
-  #include <SimplyAtomic.h>
-#elif __has_include(<util/atomic.h>)
-  //Fallback for Arduino IDE when SimplyAtomic is not installed
-  #include <util/atomic.h>
-  #define ATOMIC() ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
-  #warning It is strongly recommended to install the SimplyAtomic library rather than relying on the built-in ATOMIC
-#else
-  #define ATOMIC()
-  #warning ATOMIC() macro is empty - no atomic operations are in effect
-#endif
+#include <SimplyAtomic.h>
 
 #endif
