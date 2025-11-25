f50ceff38e893d41f6c6e3d7d44f9baee0b52b84
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2025-11-25 15:12:59 -0600

    Separate out decoder_t and decoder_builder_t

---
+++ speeduino/decoder_builder.cpp
@@ -0,0 +1,77 @@
+#include "decoder_builder.h"
+
+#pragma GCC optimize ("Os")
+
+static void nullTriggerHandler (void){return;} //initialisation function for triggerhandlers, does exactly nothing
+static uint16_t nullGetRPM(void){return 0;} //initialisation function for getRpm, returns safe value of 0
+static int nullGetCrankAngle(void){return 0;} //initialisation function for getCrankAngle, returns safe value of 0
+
+decoder_builder_t::decoder_builder_t(void)
+{
+    (void)setPrimaryTrigger(&nullTriggerHandler, TRIGGER_EDGE_NONE);
+    (void)setSecondaryTrigger(&nullTriggerHandler, TRIGGER_EDGE_NONE);
+    (void)setTertiaryTrigger(&nullTriggerHandler, TRIGGER_EDGE_NONE);
+    (void)setGetRPM(&nullGetRPM);
+    (void)setGetCrankAngle(&nullGetCrankAngle);
+    (void)setSetEndTeeth(&nullTriggerHandler);
+}
+
+decoder_builder_t& decoder_builder_t::setPrimaryTrigger(interrupt_t trigger)
+{
+    if (trigger.callback == nullptr || trigger.edge == TRIGGER_EDGE_NONE)
+    {
+        trigger = { &nullTriggerHandler, TRIGGER_EDGE_NONE };
+    } 
+    decoder.primary = trigger;
+    return *this;
+}
+decoder_builder_t& decoder_builder_t::setPrimaryTrigger(interrupt_t::callback_t handler, uint8_t edge)
+{
+    return setPrimaryTrigger( interrupt_t{ handler, edge } );
+}
+
+decoder_builder_t& decoder_builder_t::setSecondaryTrigger(interrupt_t trigger)
+{
+    if (trigger.callback == nullptr || trigger.edge == TRIGGER_EDGE_NONE)
+    {
+        trigger = { &nullTriggerHandler, TRIGGER_EDGE_NONE };
+    }
+    decoder.secondary = trigger;
+    return *this;
+}
+decoder_builder_t& decoder_builder_t::setSecondaryTrigger(interrupt_t::callback_t handler, uint8_t edge)
+{
+    return setSecondaryTrigger( interrupt_t{ handler, edge } );
+}
+
+decoder_builder_t& decoder_builder_t::setTertiaryTrigger(interrupt_t trigger)
+{
+    if (trigger.callback == nullptr || trigger.edge == TRIGGER_EDGE_NONE)
+    {
+        trigger = { &nullTriggerHandler, TRIGGER_EDGE_NONE };
+    }
+    decoder.tertiary = trigger;
+    return *this;
+}
+decoder_builder_t& decoder_builder_t::setTertiaryTrigger(interrupt_t::callback_t handler, uint8_t edge)
+{
+    return setTertiaryTrigger( interrupt_t{ handler, edge } );
+}
+
+decoder_builder_t& decoder_builder_t::setGetRPM(decoder_t::getRPM_t getRPM)
+{
+    decoder.getRPM = getRPM==nullptr ? &nullGetRPM : getRPM;
+    return *this;
+}
+
+decoder_builder_t& decoder_builder_t::setGetCrankAngle(decoder_t::getCrankAngle_t getCrankAngle)
+{
+    decoder.getCrankAngle = getCrankAngle==nullptr ? &nullGetCrankAngle : getCrankAngle;
+    return *this;
+}
+
+decoder_builder_t& decoder_builder_t::setSetEndTeeth(decoder_t::setEndTeeth_t setEndTeeth)
+{
+    decoder.setEndTeeth = setEndTeeth==nullptr ? &nullTriggerHandler : setEndTeeth;
+    return *this;
+}
---
+++ speeduino/decoder_t.h
@@ -0,0 +1,71 @@
+#pragma once
+
+#include <stdint.h>
+#include <Arduino.h>
+
+/** @brief This constant represents no trigger edge */
+static constexpr uint8_t TRIGGER_EDGE_NONE = 99;
+// Just in case
+static_assert(TRIGGER_EDGE_NONE != LOW, "LOW edge value conflict");
+static_assert(TRIGGER_EDGE_NONE != HIGH, "HIGH edge value conflict");
+static_assert(TRIGGER_EDGE_NONE != RISING, "RISING edge value conflict");
+static_assert(TRIGGER_EDGE_NONE != FALLING, "FALLING edge value conflict");
+static_assert(TRIGGER_EDGE_NONE != CHANGE, "CHANGE edge value conflict");
+
+/** @brief This structure represents a trigger interrupt */
+struct interrupt_t
+{
+  using callback_t = void(*)(void);
+
+  /** @brief The callback function to be called on interrupt */
+  callback_t callback = nullptr;
+  /** @brief The edge type for the interrupt. E.g. RISING, FALLING, CHANGE */
+  uint8_t edge = TRIGGER_EDGE_NONE;
+
+  /** @brief Attach the interrupt to a pin */
+  void attach(uint8_t pin) const
+  {
+    detach(pin);
+    if (isValid())
+    {
+      attachInterrupt(digitalPinToInterrupt(pin), callback, edge);
+    }
+  }  
+
+  /** @brief Detach the interrupt from a pin */
+  void detach(uint8_t pin) const
+  {
+    detachInterrupt( digitalPinToInterrupt(pin) );
+  }  
+
+  bool isValid(void) const
+  {
+    return edge!=TRIGGER_EDGE_NONE && callback!=nullptr;
+  }
+};
+
+/** @brief This structure represents a decoder configuration 
+ * 
+ * Create using decoder_builder_t
+*/
+struct decoder_t
+{
+  /** @brief The primary interrupt configuration - usually the crank trigger */
+  interrupt_t primary;
+  /** @brief The secondary interrupt configuration - usually the cam trigger */
+  interrupt_t secondary;
+  /** @brief The tertiary interrupt configuration - for decoders that use a 3rd input. E.g. VVT */
+  interrupt_t tertiary;
+
+  using getRPM_t = uint16_t(*)(void);
+  /** @brief The function to get the RPM */
+  getRPM_t getRPM;
+
+  using getCrankAngle_t = int(*)(void);
+  /** @brief The function to get the crank angle */
+  getCrankAngle_t getCrankAngle;
+
+  using setEndTeeth_t = void(*)(void); 
+  /** @brief The function to set the end teeth for ignition calculations */
+  setEndTeeth_t setEndTeeth;
+};
