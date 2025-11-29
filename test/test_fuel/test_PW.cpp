20f3baf739be6f894a0987a8d58b80e7546afd23
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2025-11-29 07:20:17 -0600

    Enhance PW() unit tests

--- test/test_fuel/test_PW.cpp
+++ test/test_fuel/test_PW.cpp
@@ -19,16 +19,30 @@ void test_PW_setCommon()
   MAP = 94;
   corrections = 113;
   injOpen = 1000;
+  // Turns off pwLimit
+  configPage2.dutyLim = 100;
+  revolutionTime = UINT16_MAX;
+  currentStatus.nSquirts = 1;
+  // No staging
+  configPage10.stagingEnabled = false;
+  // Nitrous off
+  currentStatus.nitrous_status = NITROUS_OFF;
+
+  configPage2.injOpen = 10;
+  currentStatus.batCorrection = 100;
+}
+
+static void setup_basic_pw(void) {
+  configPage2.multiplyMAP = 0;
+  configPage2.includeAFR = 0;
+  configPage2.incorporateAFR = 0;
+  configPage2.aeApplyMode = 0;
 }
 
 void test_PW_No_Multiply()
 {
   test_PW_setCommon();
-
-  configPage2.multiplyMAP = 0;
-  configPage2.includeAFR = 0;
-  configPage2.incorporateAFR = 0;
-  configPage2.aeApplyMode = 0;
+  setup_basic_pw();
   revolutionTime = 10000UL; //6000 rpm
 
   uint16_t result = PW(REQ_FUEL, VE, MAP, corrections, injOpen);
@@ -38,6 +52,7 @@ void test_PW_No_Multiply()
 void test_PW_MAP_Multiply()
 {
   test_PW_setCommon();
+  setup_basic_pw();
 
   configPage2.multiplyMAP = 1;
   currentStatus.baro = 103;
@@ -52,6 +67,7 @@ void test_PW_MAP_Multiply()
 void test_PW_MAP_Multiply_Compatibility()
 {
   test_PW_setCommon();
+  setup_basic_pw();
 
   configPage2.multiplyMAP = 2; //Divide MAP reading by 100 rather than by Baro reading
   currentStatus.baro = 103;
@@ -66,6 +82,7 @@ void test_PW_MAP_Multiply_Compatibility()
 void test_PW_AFR_Multiply()
 {
   test_PW_setCommon();
+  setup_basic_pw();
 
   configPage2.multiplyMAP = 0;
   currentStatus.baro = 100;
@@ -93,6 +110,8 @@ void test_PW_Large_Correction()
 {
   //This is the same as the test_PW_No_Multiply, but with correction changed to 600
   test_PW_setCommon();
+  setup_basic_pw();
+
   corrections = 600;
 
   configPage2.multiplyMAP = 0;
@@ -108,6 +127,8 @@ void test_PW_Very_Large_Correction()
 {
   //This is the same as the test_PW_No_Multiply, but with correction changed to 1500
   test_PW_setCommon();
+  setup_basic_pw();
+
   corrections = 1500;
 
   configPage2.multiplyMAP = 0;
@@ -119,11 +140,30 @@ void test_PW_Very_Large_Correction()
   TEST_ASSERT_UINT16_WITHIN(PW_ALLOWED_ERROR+30, 21670, result); //Additional allowed error here 
 }
 
+void test_PW_Zero_Correction()
+{
+  //This is the same as the test_PW_No_Multiply, but with correction changed to 600
+  test_PW_setCommon();
+  setup_basic_pw();
+
+  corrections = 0;
+
+  configPage2.multiplyMAP = 0;
+  configPage2.includeAFR = 0;
+  configPage2.incorporateAFR = 0;
+  configPage2.aeApplyMode = 0;
+
+  uint16_t result = PW(REQ_FUEL, VE, MAP, corrections, injOpen);
+  TEST_ASSERT_EQUAL(0, result);
+}
+
+
 //Test that unused pulse width values are set to 0
 //This test is for a 4 cylinder using paired injection where only INJ 1 and 2 should have PW > 0
 void test_PW_4Cyl_PW0(void)
 {
   test_PW_setCommon();
+  setup_basic_pw();
 
   configPage2.nCylinders = 4;
   configPage2.injLayout = INJ_PAIRED;
@@ -134,6 +174,25 @@ void test_PW_4Cyl_PW0(void)
   TEST_ASSERT_EQUAL(0, currentStatus.PW4);
 }
 
+static constexpr uint16_t NO_MULTIPLY_EXPECTED = 2557U;
+
+
+static void test_PW_ae_adder(void) {
+  // Same as test_PW_No_Multiply, but we add in acceleration enrichment
+  test_PW_setCommon();
+  setup_basic_pw();
+
+  currentStatus.isAcceleratingTPS = true;
+  configPage2.aeApplyMode = AE_MODE_ADDER;
+  currentStatus.AEamount = 105U;
+  uint16_t expectedOffset = (REQ_FUEL*(currentStatus.AEamount-100U))/100;
+
+  uint16_t result = PW(REQ_FUEL, VE, MAP, corrections, injOpen);
+  TEST_ASSERT_GREATER_THAN(PW_ALLOWED_ERROR, expectedOffset);
+  TEST_ASSERT_UINT16_WITHIN(PW_ALLOWED_ERROR, NO_MULTIPLY_EXPECTED + expectedOffset, result);
+}
+
+
 void testPW(void)
 {
   SET_UNITY_FILENAME() {
@@ -143,6 +202,8 @@ void testPW(void)
     RUN_TEST_P(test_PW_AFR_Multiply);
     RUN_TEST_P(test_PW_Large_Correction);
     RUN_TEST_P(test_PW_Very_Large_Correction);
+    RUN_TEST_P(test_PW_Zero_Correction);
     RUN_TEST_P(test_PW_4Cyl_PW0);
+    RUN_TEST_P(test_PW_ae_adder);
   }
 }
