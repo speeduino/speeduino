9105cc05d938f2e82069e8b61c61816793ee2648
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2025-12-04 15:02:38 -0600

    Dynamic test names & fan out of tests

--- test/test_decoders/test_decoder_init.cpp
+++ test/test_decoders/test_decoder_init.cpp
@@ -5,72 +5,68 @@
 #include "decoder_builder.h"
 #include "../test_utils.h"
 
-static void test_init_all(void)
+static void test_init(void)
 {
     auto defaultDecoder = decoder_builder_t().build();
+    // Should have at least a primary trigger
+    TEST_ASSERT_NOT_EQUAL_MESSAGE(getDecoder().primary.callback, defaultDecoder.primary.callback, "primary");
+
+    // Secondary is optional
+    if (getDecoder().secondary.isValid())
+    {
+        TEST_ASSERT_NOT_EQUAL_MESSAGE(getDecoder().secondary.callback, defaultDecoder.secondary.callback, "secondary");
+    }
+    else
+    {
+        TEST_ASSERT_EQUAL_MESSAGE(getDecoder().secondary.callback, defaultDecoder.secondary.callback, "secondary");
+    }
+    
+    // Tertiary is optional
+    if (getDecoder().tertiary.isValid())
+    {
+        TEST_ASSERT_NOT_EQUAL_MESSAGE(getDecoder().tertiary.callback, defaultDecoder.tertiary.callback, "tertiary");
+    }
+    else
+    {
+        TEST_ASSERT_EQUAL_MESSAGE(getDecoder().tertiary.callback, defaultDecoder.tertiary.callback, "tertiary");
+    }
+    
+    // Mandatory
+    TEST_ASSERT_NOT_EQUAL_MESSAGE(getDecoder().getRPM, defaultDecoder.getRPM, "getRPM");
+    
+    // Mandatory
+    TEST_ASSERT_NOT_EQUAL_MESSAGE(getDecoder().getCrankAngle, defaultDecoder.getCrankAngle, "getCrankAngle");
+
+    // Per tooth ignition is optional
+    if (getDecoderFeatures().supportsPerToothIgnition)
+    {
+        TEST_ASSERT_NOT_EQUAL_MESSAGE(getDecoder().setEndTeeth, defaultDecoder.setEndTeeth, "setEndTeeth");
+    }
+    else
+    {
+        TEST_ASSERT_EQUAL_MESSAGE(getDecoder().setEndTeeth, defaultDecoder.setEndTeeth, "setEndTeeth");
+    }
+
+    // Mandatory
+    TEST_ASSERT_NOT_EQUAL_MESSAGE(getDecoder().reset, defaultDecoder.reset, "reset");
+
+    // Mandatory
+    TEST_ASSERT_NOT_EQUAL_MESSAGE(getDecoder().isEngineRunning, defaultDecoder.isEngineRunning, "isEngineRunning");
+}
+
+static void test_init_all(void)
+{
     for (uint8_t decoder = 0; decoder < DECODER_MAX; ++decoder) {
+        char szName[128];
+        snprintf(szName, sizeof(szName), "test_init_%d", decoder);
         setDecoder(decoder);
-
-        char szMsg[64];
-
-        // Should have at least a primary trigger
-        snprintf(szMsg, sizeof(szMsg), "Decoder %d, primary", decoder);
-        TEST_ASSERT_NOT_EQUAL_MESSAGE(getDecoder().primary.callback, defaultDecoder.primary.callback, szMsg);
-
-        // Secondary is optional
-        snprintf(szMsg, sizeof(szMsg), "Decoder %d, secondary", decoder);
-        if (getDecoder().secondary.isValid())
-        {
-            TEST_ASSERT_NOT_EQUAL_MESSAGE(getDecoder().secondary.callback, defaultDecoder.secondary.callback, szMsg);
-        }
-        else
-        {
-            TEST_ASSERT_EQUAL_MESSAGE(getDecoder().secondary.callback, defaultDecoder.secondary.callback, szMsg);
-        }
-        
-        // Tertiary is optional
-        snprintf(szMsg, sizeof(szMsg), "Decoder %d, tertiary", decoder);
-        if (getDecoder().tertiary.isValid())
-        {
-            TEST_ASSERT_NOT_EQUAL_MESSAGE(getDecoder().tertiary.callback, defaultDecoder.secondary.callback, szMsg);
-        }
-        else
-        {
-            TEST_ASSERT_EQUAL_MESSAGE(getDecoder().tertiary.callback, defaultDecoder.secondary.callback, szMsg);
-        }
-        
-        // Mandatory
-        snprintf(szMsg, sizeof(szMsg), "Decoder %d, getRPM", decoder);
-        TEST_ASSERT_NOT_EQUAL_MESSAGE(getDecoder().getRPM, defaultDecoder.getRPM, szMsg);
-        
-        // Mandatory
-        snprintf(szMsg, sizeof(szMsg), "Decoder %d, getCrankAngle", decoder);
-        TEST_ASSERT_NOT_EQUAL_MESSAGE(getDecoder().getCrankAngle, defaultDecoder.getCrankAngle, szMsg);
-
-        // Per tooth ignition is optional
-        snprintf(szMsg, sizeof(szMsg), "Decoder %d, setEndTeeth", decoder);
-        if (getDecoderFeatures().supportsPerToothIgnition)
-        {
-            TEST_ASSERT_NOT_EQUAL_MESSAGE(getDecoder().setEndTeeth, defaultDecoder.setEndTeeth, szMsg);
-        }
-        else
-        {
-            TEST_ASSERT_EQUAL_MESSAGE(getDecoder().setEndTeeth, defaultDecoder.setEndTeeth, szMsg);
-        }
-
-        // Mandatory
-        snprintf(szMsg, sizeof(szMsg), "Decoder %d, reset", decoder);
-        TEST_ASSERT_NOT_EQUAL_MESSAGE(getDecoder().reset, defaultDecoder.reset, szMsg);
-
-        // Mandatory
-        snprintf(szMsg, sizeof(szMsg), "Decoder %d, isEngineRunning", decoder);
-        TEST_ASSERT_NOT_EQUAL_MESSAGE(getDecoder().isEngineRunning, defaultDecoder.isEngineRunning, szMsg);
+        UnityDefaultTestRun(test_init, szName, __LINE__);
     }
 }
 
 void testDecoderInit(void)
 {
   SET_UNITY_FILENAME() {
-    RUN_TEST( test_init_all );
+    test_init_all();
   }
 }
