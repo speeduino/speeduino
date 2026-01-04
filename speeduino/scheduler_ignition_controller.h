a20821db704351053b6f3be8e0ef996eb532972d
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2026-01-04 08:42:45 -0600

    Move calculateIgnitionAngles() to scheduler.*

--- speeduino/scheduler_ignition_controller.h
+++ speeduino/scheduler_ignition_controller.h
@@ -1,7 +1,15 @@
 #pragma once
 #include "scheduler.h"
-#include "decoders.h"
 #include "config_pages.h"
 #include "statuses.h"
 
 void matchIgnitionSchedulersToSyncState(const config2 &page2, const config4 &page4, statuses &current);
+
+/**
+ * @brief Calculate the charge & discharge angles for all ignition channels
+ * 
+ * @param page2 The tune
+ * @param page4 The tune
+ * @param current Current system state
+ */
+void calculateIgnitionAngles(const config2 &page2, const config4 &page4, statuses &current);
