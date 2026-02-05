d9697f66c907465801671cf89466f1ddfcd6e4dd
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2026-02-05 16:29:22 -0600

    Move definition of SET_COMPARE to board_definition.h

--- speeduino/timers.h
+++ speeduino/timers.h
@@ -21,8 +21,6 @@ Hence we will preload the timer with 131 cycles to leave 125 until overflow (1ms
 
 #include "globals.h"
 
-#define SET_COMPARE(compare, value) compare = (COMPARE_TYPE)(value) // It is important that we cast this to the actual overflow limit of the timer. The compare variables type can be bigger than the timer overflow.
-
 #if(defined(CORE_TEENSY) || defined(CORE_STM32))
   #define TACHO_PULSE_LOW()         (digitalWrite(pinTachOut, LOW))
   #define TACHO_PULSE_HIGH()        (digitalWrite(pinTachOut, HIGH))
