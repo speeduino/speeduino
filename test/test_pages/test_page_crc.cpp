f6ab6aa2b10a161b748616fac17498d16013876a
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2026-02-19 16:37:00 -0600

    Unit test calculatePageCRC32

---
+++ test/test_pages/test_page_crc.cpp
@@ -0,0 +1,36 @@
+#include <unity.h>
+#include "pages.h"
+#include "page_crc.h"
+#include "../test_utils.h"
+
+static void setPageValues_Incremental(uint8_t pageNum, char seedValue)
+{
+    for (uint16_t offset=0; offset<getPageSize(pageNum); ++offset)
+    {
+        setPageValue(boostvvtPage2, offset, seedValue+(uint8_t)offset);
+    }
+}
+static void setPageValues_Same(uint8_t pageNum, char seedValue)
+{
+    for (uint16_t offset=0; offset<getPageSize(pageNum); ++offset)
+    {
+        setPageValue(boostvvtPage2, offset, seedValue);
+    }
+}
+
+static void test_calculatePageCRC32(void)
+{
+    // boostvvtPage2 (12) & wmiMapPage contain multiple types of entities
+    // which makes it a good test subjects
+    setPageValues_Incremental(boostvvtPage2, 'X');
+    TEST_ASSERT_EQUAL_UINT32(0x3C15D3B7, calculatePageCRC32(boostvvtPage2));
+
+    setPageValues_Incremental(wmiMapPage, 'X');
+    TEST_ASSERT_EQUAL_UINT32(0x2ED1D425, calculatePageCRC32(wmiMapPage));
+}
+
+void testPageCrc(void) {
+    SET_UNITY_FILENAME() {
+        RUN_TEST(test_calculatePageCRC32);
+    }
+}
