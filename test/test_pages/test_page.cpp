c2e1f4c8905b828fbe26b9ce010f73fbaec7151e
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2026-02-18 21:58:28 -0600

    Unit test setEntityValue()

--- test/test_pages/test_page.cpp
+++ test/test_pages/test_page.cpp
@@ -84,14 +84,15 @@ static void assert_3d_table(const page_iterator_t &entity, const TTable &table, 
     // Offset is too large
     TEST_ASSERT_EQUAL(0, getEntityValue(entity, entity.address.size+1));
 }
+
 template <typename TTable>
 static void test_getEntityValue_tableT(void)
 {
     auto entity = setup3dTable<TTable>('X', 'Y', 'Z');
     page_iterator_t entityIter = setupTableIterator(entity);
     assert_3d_table(entityIter, entity, 'X', 'Y', 'Z');
-
 }
+
 static void test_getEntityValue_table(void)
 {
     test_getEntityValue_tableT<table3d4RpmLoad>();
@@ -100,10 +101,85 @@ static void test_getEntityValue_table(void)
     test_getEntityValue_tableT<table3d16RpmLoad>();
 }
 
+static void set_entity_values(page_iterator_t &entity, uint16_t from, uint16_t to, char value)
+{
+    for (uint16_t offset=from; offset<to; ++offset)
+    {
+        setEntityValue(entity, offset, value);
+    }
+}
+
+static void test_setEntityValue_raw(void)
+{
+    constexpr char PRE_MARKER = 'X';
+    char entity[48];
+    memset(&entity, PRE_MARKER, sizeof(entity));
+
+    page_iterator_t entityIter( entity, 
+                                entity_page_location_t(10, 0),
+                                entity_page_address_t(0, sizeof(entity)));
+
+    constexpr char POST_MARKER = 'Y';
+    set_entity_values(entityIter, 0, entityIter.address.size, POST_MARKER);
+    TEST_ASSERT_EACH_EQUAL_CHAR(POST_MARKER, entity, sizeof(entity));
+}
+
+static void test_setEntityValue_none(void)
+{
+    constexpr char PRE_MARKER = 'X';
+    char entity[48];
+    memset(&entity, PRE_MARKER, sizeof(entity));
+
+    page_iterator_t entityIter( NoEntity, 
+                                entity_page_location_t(10, 0),
+                                entity_page_address_t(0, sizeof(entity)));
+
+    constexpr char POST_MARKER = 'Y';
+    set_entity_values(entityIter, 0, entityIter.address.size, POST_MARKER);
+    // setEntityValue should have no effect
+    TEST_ASSERT_EACH_EQUAL_CHAR(PRE_MARKER, entity, sizeof(entity));
+}
+
+template <typename TTable>
+static void assert_set_3d_table(page_iterator_t entity, const TTable &table, byte valuePre, byte xAxisPre, byte yAxisPre)
+{
+    const uint16_t valueSize = table.values.num_rows*table.values.row_size; 
+    const char valuePost = valuePre+1;
+    set_entity_values(entity, 0, valueSize, valuePost);
+    TEST_ASSERT_EACH_EQUAL_CHAR(valuePost, table.values.values, valueSize);
+
+    const char xAxisPost = xAxisPre+1;
+    set_entity_values(entity, valueSize, valueSize+table.axisX.length, xAxisPost);
+    TEST_ASSERT_EACH_EQUAL_CHAR(xAxisPost, table.axisX.axis, table.axisX.length);
+
+    const char yAxisPost = yAxisPre+1;
+    set_entity_values(entity, valueSize+table.axisX.length, valueSize+table.axisX.length+table.axisY.length, yAxisPost);
+    TEST_ASSERT_EACH_EQUAL_CHAR(yAxisPost, table.axisY.axis, table.axisY.length);
+}
+
+
+template <typename TTable>
+static void test_setEntityValue_tableT(void)
+{
+    auto entity = setup3dTable<TTable>('X', 'Y', 'Z');
+    assert_set_3d_table(setupTableIterator(entity), entity, 'X', 'Y', 'Z');
+}
+
+static void test_setEntityValue_table(void)
+{
+    test_setEntityValue_tableT<table3d4RpmLoad>();
+    test_setEntityValue_tableT<table3d6RpmLoad>();
+    test_setEntityValue_tableT<table3d8RpmLoad>();
+    test_setEntityValue_tableT<table3d16RpmLoad>();
+}
+
 void testPage(void) {
     SET_UNITY_FILENAME() {
         RUN_TEST(test_getEntityValue_raw);
         RUN_TEST(test_getEntityValue_none);
         RUN_TEST(test_getEntityValue_table);
+        RUN_TEST(test_setEntityValue_raw);
+        RUN_TEST(test_setEntityValue_none);
+        RUN_TEST(test_setEntityValue_table);
     }
 }
