f36326189a12d90b1a627e20bf546aa5c75b84ef
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2026-02-20 06:32:12 -0600

    Unit test saveAllCalibrationTables

--- test/test_storage/test_storage.cpp
+++ test/test_storage/test_storage.cpp
@@ -2,24 +2,28 @@
 #include "../test_utils.h"
 #include "storage.h"
 #include "pages.h"
+#include "sensors.h"
 
 struct infinite_eeprom_t
 {
     char readValue;
     uint16_t _length;
     uint16_t _blockSize;
+    bool wasRead;
+    bool wasWritten;
 };
 
 static infinite_eeprom_t infiniteBuffer;
 
 static byte infiniteRead(uint16_t)
 {
+    infiniteBuffer.wasRead = true;
     return infiniteBuffer.readValue;
 }
 
 void infniteWrite(uint16_t, byte)
 {
-    // Do nothing
+    infiniteBuffer.wasWritten = true;
 }
 
 uint16_t infniteLength(void)
@@ -37,6 +41,8 @@ static void setupInfniteStorageApi(uint16_t length, uint16_t blockSize, char rea
     infiniteBuffer.readValue = readValue;
     infiniteBuffer._length = length;
     infiniteBuffer._blockSize = blockSize;
+    infiniteBuffer.wasRead = false;
+    infiniteBuffer.wasWritten = false;
     storage_api_t storage = { .read = infiniteRead, .write = infniteWrite, .length = infniteLength, .getMaxWriteBlockSize = infiniteGetMaxWriteBlockSize };
     setStorageAPI(storage);
 }
@@ -84,9 +90,64 @@ static void test_loadAllPages(void)
     }
 }
 
+
+template <typename axis_t, typename value_t, uint8_t sizeT>
+static void assert_2dtable(const table2D<axis_t, value_t, sizeT> &table, axis_t expectedAxis, value_t expectedValue)
+{
+    for (uint16_t index=0; index<sizeT; ++index)
+    {
+        TEST_ASSERT_EQUAL(expectedAxis, table.axis[index]);
+        TEST_ASSERT_EQUAL(expectedValue, table.values[index]);
+    }
+}
+
+static void test_loadAllCalibrationTables(void)
+{
+    setupInfniteStorageApi(8192, 8192, BUFFER_MARKER);
+    loadAllCalibrationTables();
+    uint16_t U16_MARKER = word(BUFFER_MARKER, BUFFER_MARKER);
+    constexpr uint8_t U8_MARKER = BUFFER_MARKER;
+
+    assert_2dtable(o2CalibrationTable, U16_MARKER, U8_MARKER);
+    assert_2dtable(iatCalibrationTable, U16_MARKER, U16_MARKER);
+    assert_2dtable(cltCalibrationTable, U16_MARKER, U16_MARKER);
+}
+
+template <typename axis_t, typename value_t, uint8_t sizeT>
+static void load_2dtable(table2D<axis_t, value_t, sizeT> &table, axis_t axis, value_t value)
+{
+    for (uint16_t index=0; index<sizeT; ++index)
+    {
+        table.axis[index] = axis;
+        table.values[index] = value;
+    }
+}
+
+static void test_saveAllCalibrationTables(void)
+{
+    uint16_t U16_MARKER = word(WRITE_MARKER, WRITE_MARKER);
+    constexpr uint8_t U8_MARKER = WRITE_MARKER;
+
+    load_2dtable(o2CalibrationTable, U16_MARKER, U8_MARKER);
+    load_2dtable(iatCalibrationTable, U16_MARKER, U16_MARKER);
+    load_2dtable(cltCalibrationTable, U16_MARKER, U16_MARKER);
+
+    setupInfniteStorageApi(8192, 8192, BUFFER_MARKER);
+    saveAllCalibrationTables();
+    TEST_ASSERT_TRUE(infiniteBuffer.wasRead);
+    TEST_ASSERT_TRUE(infiniteBuffer.wasWritten);
+
+    setupInfniteStorageApi(8192, 8192, BUFFER_MARKER);
+    saveCalibrationTable((SensorCalibrationTable)-1);
+    TEST_ASSERT_FALSE(infiniteBuffer.wasRead);
+    TEST_ASSERT_FALSE(infiniteBuffer.wasWritten);
+}
+
 void test_storage(void) {
     SET_UNITY_FILENAME() {     
         RUN_TEST_P(test_saveAllPages);
         RUN_TEST_P(test_loadAllPages);
+        RUN_TEST_P(test_loadAllCalibrationTables);
+        RUN_TEST_P(test_saveAllCalibrationTables);
     }
 }
