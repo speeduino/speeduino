2ced8f98ba757981b9cf4137fd812f7b62982634
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2026-02-25 07:58:52 -0600

    Increase 2d table test coverage

--- speeduino/type_traits.h
+++ speeduino/type_traits.h
@@ -22,3 +22,39 @@ namespace type_traits {
   };
 
 }
+// std::numeric_limits. See https://en.cppreference.com/w/cpp/types/numeric_limits
+#undef max
+template <typename T>
+struct numeric_limits {
+  static constexpr T max(void);
+};
+
+template <>
+struct numeric_limits<uint8_t> {
+  static constexpr uint8_t max(void) { return UINT8_MAX; }
+};
+
+template <>
+struct numeric_limits<int8_t> {
+  static constexpr int8_t max(void) { return INT8_MAX; }
+};
+
+template <>
+struct numeric_limits<uint16_t> {
+  static constexpr uint16_t max(void) { return UINT16_MAX; }
+};
+
+template <>
+struct numeric_limits<int16_t> {
+  static constexpr int16_t max(void) { return INT16_MAX; }
+};
+
+template <>
+struct numeric_limits<uint32_t> {
+  static constexpr uint32_t max(void) { return UINT32_MAX; }
+};
+
+template <>
+struct numeric_limits<int32_t> {
+  static constexpr int32_t max(void) { return INT32_MAX; }
+};
