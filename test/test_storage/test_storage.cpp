7874f6e078c7ba9f46f6dedf25b03f6de50b52b6
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2026-03-05 08:56:12 -0600

    Move storage fake to separate file

--- test/test_storage/test_storage.cpp
+++ test/test_storage/test_storage.cpp
@@ -3,60 +3,18 @@
 #include "storage.h"
 #include "pages.h"
 #include "sensors.h"
-
-struct infinite_eeprom_t
-{
-    char readValue;
-    uint16_t _length;
-    uint16_t _blockSize;
-    bool wasRead;
-    bool wasWritten;
-};
-
-static infinite_eeprom_t infiniteBuffer;
-
-static byte infiniteRead(uint16_t)
-{
-    infiniteBuffer.wasRead = true;
-    return infiniteBuffer.readValue;
-}
-
-void infniteWrite(uint16_t, byte)
-{
-    infiniteBuffer.wasWritten = true;
-}
-
-uint16_t infniteLength(void)
-{
-    return infiniteBuffer._length;
-}
-
-uint16_t infiniteGetMaxWriteBlockSize(const statuses&)
-{
-    return infiniteBuffer._blockSize;
-}
-
-static void setupInfniteStorageApi(uint16_t length, uint16_t blockSize, char readValue)
-{
-    infiniteBuffer.readValue = readValue;
-    infiniteBuffer._length = length;
-    infiniteBuffer._blockSize = blockSize;
-    infiniteBuffer.wasRead = false;
-    infiniteBuffer.wasWritten = false;
-    storage_api_t storage = { .read = infiniteRead, .write = infniteWrite, .length = infniteLength, .getMaxWriteBlockSize = infiniteGetMaxWriteBlockSize };
-    setStorageAPI(storage);
-}
+#include "fake_storage.h"
 
 constexpr char WRITE_MARKER = 'X';
 constexpr char BUFFER_MARKER = 'A';
 
 static void test_saveAllPages(void)
 {
-    setupInfniteStorageApi(8192, 8192, BUFFER_MARKER);
+    setStorageAPI(getOneByteStorageApi(8192, 8192, BUFFER_MARKER));
     saveAllPages();
     TEST_ASSERT_FALSE(isEepromWritePending());
 
-    setupInfniteStorageApi(8192, 16, BUFFER_MARKER);
+    setStorageAPI(getOneByteStorageApi(8192, 16, BUFFER_MARKER));
     saveAllPages();
     TEST_ASSERT_TRUE(isEepromWritePending());
 }
@@ -82,7 +40,7 @@ static void assert_page(uint8_t pageNum, char expectedContent)
 
 static void test_loadAllPages(void)
 {
-    setupInfniteStorageApi(8192, 8192, BUFFER_MARKER);
+    setStorageAPI(getOneByteStorageApi(8192, 8192, BUFFER_MARKER));
     loadAllPages();
     for (uint8_t page = MIN_PAGE_NUM; page<MAX_PAGE_NUM; ++page) {
         assert_page(page, BUFFER_MARKER);
@@ -102,7 +60,7 @@ static void assert_2dtable(const table2D<axis_t, value_t, sizeT> &table, axis_t 
 
 static void test_loadAllCalibrationTables(void)
 {
-    setupInfniteStorageApi(8192, 8192, BUFFER_MARKER);
+    setStorageAPI(getOneByteStorageApi(8192, 8192, BUFFER_MARKER));
     loadAllCalibrationTables();
     uint16_t U16_MARKER = word(BUFFER_MARKER, BUFFER_MARKER);
     constexpr uint8_t U8_MARKER = BUFFER_MARKER;
@@ -131,15 +89,15 @@ static void test_saveAllCalibrationTables(void)
     load_2dtable(iatCalibrationTable, U16_MARKER, U8_MARKER);
     load_2dtable(cltCalibrationTable, U16_MARKER, U8_MARKER);
 
-    setupInfniteStorageApi(8192, 8192, BUFFER_MARKER);
+    setStorageAPI(getOneByteStorageApi(8192, 8192, BUFFER_MARKER));
     saveAllCalibrationTables();
-    TEST_ASSERT_TRUE(infiniteBuffer.wasRead);
-    TEST_ASSERT_TRUE(infiniteBuffer.wasWritten);
+    TEST_ASSERT_GREATER_THAN(0, oneByteEeprom.readCount);
+    TEST_ASSERT_GREATER_THAN(0, oneByteEeprom.writeCount);
 
-    setupInfniteStorageApi(8192, 8192, BUFFER_MARKER);
+    setStorageAPI(getOneByteStorageApi(8192, 8192, BUFFER_MARKER));
     saveCalibrationTable((SensorCalibrationTable)-1);
-    TEST_ASSERT_FALSE(infiniteBuffer.wasRead);
-    TEST_ASSERT_FALSE(infiniteBuffer.wasWritten);
+    TEST_ASSERT_EQUAL(0, oneByteEeprom.readCount);
+    TEST_ASSERT_EQUAL(0, oneByteEeprom.writeCount);
 }
 
 void test_storage(void) {
