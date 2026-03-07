e5e91b1e1265e915c8496d9703e0e97aeb9aa16a
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2026-03-07 11:48:26 -0600

    Improve unit test coverage of matchIgnitionSchedulersToSyncState

--- speeduino/scheduler_ignition_controller.h
+++ speeduino/scheduler_ignition_controller.h
@@ -3,8 +3,6 @@
 #include "config_pages.h"
 #include "statuses.h"
 
-void matchIgnitionSchedulersToSyncState(const config2 &page2, const config4 &page4, statuses &current);
-
 /**
  * @brief Calculate the charge & discharge angles for all ignition channels
  * 
