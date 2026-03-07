cd7922d1ac0ca51047ea16bc0ace7dfb6d766680
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2026-03-07 12:12:04 -0600

    Improve over dwell test coverage

--- speeduino/scheduler.cpp
+++ speeduino/scheduler.cpp
@@ -493,6 +493,8 @@ TESTABLE_INLINE_STATIC bool isOverDwellActive(const config4 &page4, const status
   return (page4.useDwellLim) && !isCrankLocked;
 }
 
+// LCOV_EXCL_START
+// The lower level function should be tested, so this can be excluded from coverage
 void applyOverDwellProtection(void)
 {
   if (isOverDwellActive(configPage4, currentStatus)) {
@@ -522,6 +524,7 @@ void applyOverDwellProtection(void)
 #endif
   }
 }
+// LCOV_EXCL_STOP
 
 static inline bool isAnyFuelScheduleRunning(void) {
   ATOMIC() {
