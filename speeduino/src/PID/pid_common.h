52da342dd574d9c421d67cfdafdac8c4488bde75
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2026-04-06 23:57:11 -0500

    Remove PidDirection

--- speeduino/src/PID/pid_common.h
+++ speeduino/src/PID/pid_common.h
@@ -2,12 +2,6 @@
 
 #include <stdint.h>
 
-enum class PidDirection : uint8_t
-{
-    Direct = 0,  ///< The output will increase when error is positive. 
-    Reverse = 1, ///< The output will decrease when error is positive. 
-};
-
 /** @brief Structure to hold PID tuning parameters */
 struct PidTuningParameters
 {
