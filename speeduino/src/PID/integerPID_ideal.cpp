c05cd3046696c60f28737eff55a8b63dc3ab3769
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2026-04-15 07:36:28 -0500

    Push descaling to last step of calculation

--- speeduino/src/PID/integerPID_ideal.cpp
+++ speeduino/src/PID/integerPID_ideal.cpp
@@ -48,13 +48,14 @@ bool integerPID_ideal::Compute(unsigned long now, uint16_t FeedForward)
       long unitless_setpoint = (((long)*mySetpoint - 0) * 10000L) / (sensitivity - 0);
       long unitless_input = (((long)*myInput - 0) * 10000L) / (sensitivity - 0);
       long error = unitless_setpoint - unitless_input;
+      // Bias is % in whole numbers. Multiply it by 10 to get it with 2 places.
+      uint32_t scaledFeedForward = FeedForward*10UL;
 
       ITerm += error;
 
       /*Compute PID Output*/
-      long output = (kp * error) + (ki * ITerm) + (kd * (error - lastError));
-      output = FeedForward + (output / 10); //output is % multiplied by 1000. To get % with 2 decimal places, divide it by 10. Likewise, bias is % in whole numbers. Multiply it by 100 to get it with 2 places.
-
+      long output = scaledFeedForward + (kp * error) + (ki * ITerm) + (kd * (error - lastError));
+      
       if(output > outMax)
       {
          output  = outMax;
@@ -66,7 +67,8 @@ bool integerPID_ideal::Compute(unsigned long now, uint16_t FeedForward)
          ITerm -= error;
       }
 
-	    *myOutput = output;
+	   //output is % multiplied by 1000. To get % with 2 decimal places, divide it by 10. 
+      *myOutput = output/10;
 
       /*Remember some variables for next time*/
       lastTime = now;
@@ -118,7 +120,7 @@ void integerPID_ideal::SetOutputLimits(long Min, long Max)
 {
    if(Min < Max)
    {
-     constexpr uint16_t LIMIT_FACTOR = 100; //How much outMin and OutMax must be multiplied by to get them in the same scale as the output
+     constexpr uint16_t LIMIT_FACTOR = 1000; //How much outMin and OutMax must be multiplied by to get them in the same scale as the output
 
      outMin = Min * LIMIT_FACTOR;
      outMax = Max * LIMIT_FACTOR;
