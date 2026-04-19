a62498bfd3a5163bafbf595f4ec202ecbbec25ea
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2026-04-19 15:59:27 -0500

    initialiseResetControl() - inject all parameters

--- speeduino/resetControl.cpp
+++ speeduino/resetControl.cpp
@@ -1,4 +1,4 @@
-#include "globals.h"
+#include "resetControl.h"
 
 //This needs to be here because using the config page directly can prevent burning the setting
 static uint8_t _resetControl = RESET_CONTROL_DISABLED;
@@ -8,32 +8,37 @@ uint8_t getResetControl(void)
     return _resetControl;
 }
 
-void __attribute__((optimize("Os"))) setResetControlPinState(uint8_t resetControl)
+void __attribute__((optimize("Os"))) initialiseResetControl(statuses &current, uint8_t resetControlMode, uint8_t resetPin)
 {
-  _resetControl = resetControl;
-  currentStatus.resetPreventActive = false;
+  _resetControl = resetControlMode;
+  current.resetPreventActive = false;
 
   /* Setup reset control initial state */
-  switch (resetControl)
+  switch (resetControlMode)
   {
     case RESET_CONTROL_PREVENT_WHEN_RUNNING:
       /* Set the reset control pin LOW and change it to HIGH later when we get sync. */
-      digitalWrite(pinResetControl, LOW);
-      currentStatus.resetPreventActive = false;
+      digitalWrite(resetPin, LOW);
+      current.resetPreventActive = false;
       break;
     case RESET_CONTROL_PREVENT_ALWAYS:
       /* Set the reset control pin HIGH and never touch it again. */
-      digitalWrite(pinResetControl, HIGH);
-      currentStatus.resetPreventActive = true;
+      digitalWrite(resetPin, HIGH);
+      current.resetPreventActive = true;
       break;
     case RESET_CONTROL_SERIAL_COMMAND:
       /* Set the reset control pin HIGH. There currently isn't any practical difference
          between this and PREVENT_ALWAYS but it doesn't hurt anything to have them separate. */
-      digitalWrite(pinResetControl, HIGH);
-      currentStatus.resetPreventActive = false;
+      digitalWrite(resetPin, HIGH);
+      current.resetPreventActive = false;
       break;
     default:
       // Do nothing - keep MISRA happy
       break;
   }
+
+  /* Reset control is a special case. If reset control is enabled, it needs its initial state set BEFORE its pinMode.
+     If that doesn't happen and reset control is in "Serial Command" mode, the Arduino will end up in a reset loop
+     because the control pin will go low as soon as the pinMode is set to OUTPUT. */
+  pinMode(resetPin, OUTPUT);
 }
--- speeduino/resetControl.h
+++ speeduino/resetControl.h
@@ -1,7 +1,12 @@
 #pragma once
 
-#include <stdint.h>
+#include "statuses.h"
 
-void setResetControlPinState(uint8_t resetControl);
+constexpr uint8_t RESET_CONTROL_DISABLED             = 0U;
+constexpr uint8_t RESET_CONTROL_PREVENT_WHEN_RUNNING = 1U;
+constexpr uint8_t RESET_CONTROL_PREVENT_ALWAYS       = 2U;
+constexpr uint8_t RESET_CONTROL_SERIAL_COMMAND       = 3U;
+
+void initialiseResetControl(statuses &current, uint8_t resetControlMode, uint8_t resetPin);
 
 uint8_t getResetControl(void);
