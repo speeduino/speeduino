afea830d385ad1204ee3e06fc70d11ca1fd91bed
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2026-04-20 09:58:23 -0500

    Simplify initialiseResetControl() & matchResetControlToEngineState()

--- speeduino/resetControl.cpp
+++ speeduino/resetControl.cpp
@@ -19,31 +19,10 @@ void __attribute__((optimize("Os"))) initialiseResetControl(ResetControlMode res
 {
   _resetControl = resetControlMode;
   _resetPin = resetPin;
-  _resetPreventActive = false;
-
-  /* Setup reset control initial state */
-  switch (resetControlMode)
-  {
-    case ResetControlMode::PreventWhenRunning:
-      /* Set the reset control pin LOW and change it to HIGH later when we get sync. */
-      digitalWrite(resetPin, LOW);
-      _resetPreventActive = false;
-      break;
-    case ResetControlMode::PreventAlways:
-      /* Set the reset control pin HIGH and never touch it again. */
-      digitalWrite(resetPin, HIGH);
-      _resetPreventActive = true;
-      break;
-    case ResetControlMode::SerialCommand:
-      /* Set the reset control pin HIGH. There currently isn't any practical difference
-         between this and PREVENT_ALWAYS but it doesn't hurt anything to have them separate. */
-      digitalWrite(resetPin, HIGH);
-      _resetPreventActive = false;
-      break;
-    default:
-      // Do nothing - keep MISRA happy
-      break;
-  }
+  // We are assuming the engine is not running at the point of initialisation, so reset prevent 
+  // should only be active if the mode is "Prevent Always"
+  _resetPreventActive = resetControlMode == ResetControlMode::PreventAlways;
+  digitalWrite(resetPin, resetControlMode == ResetControlMode::PreventWhenRunning ? LOW : HIGH);
 
   /* Reset control is a special case. If reset control is enabled, it needs its initial state set BEFORE its pinMode.
      If that doesn't happen and reset control is in "Serial Command" mode, the Arduino will end up in a reset loop
@@ -56,22 +35,7 @@ void matchResetControlToEngineState(const statuses &current)
   if (getResetControlMode() == ResetControlMode::PreventWhenRunning)
   {
     // TODO: consolidate this check with status.isEngineRunning, which is based on the same conditions? 
-    if ((current.decoder.getStatus().syncStatus!=SyncStatus::None) && (current.RPM > 0))
-    {
-      if ( (!_resetPreventActive)) 
-      {
-        //Reset prevention is supposed to be on while the engine is running but isn't. Fix that.
-        digitalWrite(_resetPin, HIGH);
-        _resetPreventActive = true;
-      }
-    } 
-    else
-    {
-      if (_resetPreventActive)
-      {
-        digitalWrite(_resetPin, LOW);
-        _resetPreventActive = false;
-      }
-    }
+    _resetPreventActive = (current.decoder.getStatus().syncStatus!=SyncStatus::None) && (current.RPM > 0);
+    digitalWrite(_resetPin, _resetPreventActive ? HIGH : LOW);
   }
 }
