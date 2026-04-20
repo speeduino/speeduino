077fd5859b0dfaa15df58a8552b25ac56060fe0f
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2026-04-20 15:13:37 -0500

    Limit programmableIOControl public surface area and inject all dependencies.

--- speeduino/programmableIOControl.cpp
+++ speeduino/programmableIOControl.cpp
@@ -5,89 +5,101 @@
 */
 /** @file
  * Custom Programmable I/O.
- * The config related to Programmable I/O is found on configPage13 (of type @ref config13).
+ * The config related to Programmable I/O is found on page13 (of type @ref config13).
  */
-#include <avr/pgmspace.h>
-#include "globals.h"
 #include "programmableIOControl.h"
-#include "decoders.h"
-#include "comms.h"
 #include "logger.h"
-#include "scheduler.h"
 #include "units.h"
 
-uint8_t ioDelay[sizeof(configPage13.outputPin)];
-uint8_t ioOutDelay[sizeof(configPage13.outputPin)];
-uint8_t pinIsValid = 0;
-uint8_t currentRuleStatus = 0;
+static uint8_t ioDelay[_countof(config13::outputPin)];
+static uint8_t ioOutDelay[_countof(config13::outputPin)];
+static uint8_t pinIsValid = 0;
+static uint8_t currentRuleStatus = 0;
+
+constexpr uint8_t COMPARATOR_EQUAL = 0;
+constexpr uint8_t COMPARATOR_NOT_EQUAL = 1;
+constexpr uint8_t COMPARATOR_GREATER = 2;
+constexpr uint8_t COMPARATOR_GREATER_EQUAL = 3;
+constexpr uint8_t COMPARATOR_LESS = 4;
+constexpr uint8_t COMPARATOR_LESS_EQUAL = 5;
+constexpr uint8_t COMPARATOR_AND = 6;
+constexpr uint8_t COMPARATOR_XOR = 7;
+
+constexpr uint8_t BITWISE_DISABLED = 0;
+constexpr uint8_t BITWISE_AND = 1;
+constexpr uint8_t BITWISE_OR = 2;
+constexpr uint8_t BITWISE_XOR = 3;
+
+constexpr uint8_t REUSE_RULES = 240;
 
 //*********************************************************************************************************************************************************************************
-void initialiseProgrammableIO(void)
+void __attribute__((optimize("Os"))) initialiseProgrammableIO(statuses& current, const config13& page13)
 {
   uint8_t outputPin;
-  for (uint8_t y = 0; y < sizeof(configPage13.outputPin); y++)
+  for (uint8_t y = 0; y < _countof(ioDelay); y++)
   {
     ioDelay[y] = 0;
     ioOutDelay[y] = 0;
-    outputPin = configPage13.outputPin[y];
+    outputPin = page13.outputPin[y];
     if (outputPin > 0)
     {
       if ( outputPin >= 128 ) //Cascate rule usage
       {
-        BIT_WRITE(currentStatus.outputsStatus, y, BIT_CHECK(configPage13.outputInverted, y));
+        BIT_WRITE(current.outputsStatus, y, BIT_CHECK(page13.outputInverted, y));
         BIT_SET(pinIsValid, y);
       }
       else if ( !pinIsUsed(outputPin) )
       {
         pinMode(outputPin, OUTPUT);
-        digitalWrite(outputPin, BIT_CHECK(configPage13.outputInverted, y));
-        BIT_WRITE(currentStatus.outputsStatus, y, BIT_CHECK(configPage13.outputInverted, y));
+        digitalWrite(outputPin, BIT_CHECK(page13.outputInverted, y));
+        BIT_WRITE(current.outputsStatus, y, BIT_CHECK(page13.outputInverted, y));
         BIT_SET(pinIsValid, y);
       }
       else { BIT_CLEAR(pinIsValid, y); }
     }
   }
 }
+
 /** Check all (8) programmable I/O:s and carry out action on output pin as needed.
  * Compare 2 (16 bit) vars in a way configured by @ref cmpOperation (see also @ref config13.operation).
  * Use ProgrammableIOGetData() to get 2 vars to compare.
  * Skip all programmable I/O:s where output pin is set 0 (meaning: not programmed).
  */
-void checkProgrammableIO(void)
+void checkProgrammableIO(statuses& current, const config13& page13)
 {
   int16_t data, data2;
   uint8_t dataRequested;
   bool firstCheck, secondCheck;
 
-  for (uint8_t y = 0; y < sizeof(configPage13.outputPin); y++)
+  for (uint8_t y = 0; y < _countof(ioDelay); y++)
   {
     firstCheck = false;
     secondCheck = false;
     if ( BIT_CHECK(pinIsValid, y) ) //if outputPin == 0 it is disabled
     {
-      dataRequested = configPage13.firstDataIn[y];
+      dataRequested = page13.firstDataIn[y];
       if ( dataRequested > 239U ) //Somehow using 239 uses 9 bytes of RAM, why??
       {
         dataRequested -= REUSE_RULES;
-        if ( dataRequested <= sizeof(configPage13.outputPin) ) { data = BIT_CHECK(currentRuleStatus, dataRequested); }
+        if ( dataRequested <= sizeof(page13.outputPin) ) { data = BIT_CHECK(currentRuleStatus, dataRequested); }
         else { data = 0; }
       }
       else { data = ProgrammableIOGetData(dataRequested); }
-      data2 = configPage13.firstTarget[y];
+      data2 = page13.firstTarget[y];
 
-      if ( (configPage13.operation[y].firstCompType == COMPARATOR_EQUAL) && (data == data2) ) { firstCheck = true; }
-      else if ( (configPage13.operation[y].firstCompType == COMPARATOR_NOT_EQUAL) && (data != data2) ) { firstCheck = true; }
-      else if ( (configPage13.operation[y].firstCompType == COMPARATOR_GREATER) && (data > data2) ) { firstCheck = true; }
-      else if ( (configPage13.operation[y].firstCompType == COMPARATOR_GREATER_EQUAL) && (data >= data2) ) { firstCheck = true; }
-      else if ( (configPage13.operation[y].firstCompType == COMPARATOR_LESS) && (data < data2) ) { firstCheck = true; }
-      else if ( (configPage13.operation[y].firstCompType == COMPARATOR_LESS_EQUAL) && (data <= data2) ) { firstCheck = true; }
-      else if ( (configPage13.operation[y].firstCompType == COMPARATOR_AND) && ((data & data2) != 0) ) { firstCheck = true; }
-      else if ( (configPage13.operation[y].firstCompType == COMPARATOR_XOR) && ((data ^ data2) != 0) ) { firstCheck = true; }
+      if ( (page13.operation[y].firstCompType == COMPARATOR_EQUAL) && (data == data2) ) { firstCheck = true; }
+      else if ( (page13.operation[y].firstCompType == COMPARATOR_NOT_EQUAL) && (data != data2) ) { firstCheck = true; }
+      else if ( (page13.operation[y].firstCompType == COMPARATOR_GREATER) && (data > data2) ) { firstCheck = true; }
+      else if ( (page13.operation[y].firstCompType == COMPARATOR_GREATER_EQUAL) && (data >= data2) ) { firstCheck = true; }
+      else if ( (page13.operation[y].firstCompType == COMPARATOR_LESS) && (data < data2) ) { firstCheck = true; }
+      else if ( (page13.operation[y].firstCompType == COMPARATOR_LESS_EQUAL) && (data <= data2) ) { firstCheck = true; }
+      else if ( (page13.operation[y].firstCompType == COMPARATOR_AND) && ((data & data2) != 0) ) { firstCheck = true; }
+      else if ( (page13.operation[y].firstCompType == COMPARATOR_XOR) && ((data ^ data2) != 0) ) { firstCheck = true; }
 
-      if (configPage13.operation[y].bitwise != BITWISE_DISABLED)
+      if (page13.operation[y].bitwise != BITWISE_DISABLED)
       {
-        dataRequested = configPage13.secondDataIn[y];
-        if ( dataRequested <= (REUSE_RULES + sizeof(configPage13.outputPin)) ) //Failsafe check
+        dataRequested = page13.secondDataIn[y];
+        if ( dataRequested <= (REUSE_RULES + sizeof(page13.outputPin)) ) //Failsafe check
         {
           if ( dataRequested > 239U ) //Somehow using 239 uses 9 bytes of RAM, why??
           {
@@ -95,59 +107,59 @@ void checkProgrammableIO(void)
             data = BIT_CHECK(currentRuleStatus, dataRequested);
           }
           else { data = ProgrammableIOGetData(dataRequested); }
-          data2 = configPage13.secondTarget[y];
+          data2 = page13.secondTarget[y];
           
-          if ( (configPage13.operation[y].secondCompType == COMPARATOR_EQUAL) && (data == data2) ) { secondCheck = true; }
-          else if ( (configPage13.operation[y].secondCompType == COMPARATOR_NOT_EQUAL) && (data != data2) ) { secondCheck = true; }
-          else if ( (configPage13.operation[y].secondCompType == COMPARATOR_GREATER) && (data > data2) ) { secondCheck = true; }
-          else if ( (configPage13.operation[y].secondCompType == COMPARATOR_GREATER_EQUAL) && (data >= data2) ) { secondCheck = true; }
-          else if ( (configPage13.operation[y].secondCompType == COMPARATOR_LESS) && (data < data2) ) { secondCheck = true; }
-          else if ( (configPage13.operation[y].secondCompType == COMPARATOR_LESS_EQUAL) && (data <= data2) ) { secondCheck = true; }
-          else if ( (configPage13.operation[y].secondCompType == COMPARATOR_AND) && ((data & data2) != 0) ) { secondCheck = true; }
-          else if ( (configPage13.operation[y].secondCompType == COMPARATOR_XOR) && ((data ^ data2) != 0) ) { secondCheck = true; }
+          if ( (page13.operation[y].secondCompType == COMPARATOR_EQUAL) && (data == data2) ) { secondCheck = true; }
+          else if ( (page13.operation[y].secondCompType == COMPARATOR_NOT_EQUAL) && (data != data2) ) { secondCheck = true; }
+          else if ( (page13.operation[y].secondCompType == COMPARATOR_GREATER) && (data > data2) ) { secondCheck = true; }
+          else if ( (page13.operation[y].secondCompType == COMPARATOR_GREATER_EQUAL) && (data >= data2) ) { secondCheck = true; }
+          else if ( (page13.operation[y].secondCompType == COMPARATOR_LESS) && (data < data2) ) { secondCheck = true; }
+          else if ( (page13.operation[y].secondCompType == COMPARATOR_LESS_EQUAL) && (data <= data2) ) { secondCheck = true; }
+          else if ( (page13.operation[y].secondCompType == COMPARATOR_AND) && ((data & data2) != 0) ) { secondCheck = true; }
+          else if ( (page13.operation[y].secondCompType == COMPARATOR_XOR) && ((data ^ data2) != 0) ) { secondCheck = true; }
 
-          if (configPage13.operation[y].bitwise == BITWISE_AND) { firstCheck &= secondCheck; }
-          if (configPage13.operation[y].bitwise == BITWISE_OR) { firstCheck |= secondCheck; }
-          if (configPage13.operation[y].bitwise == BITWISE_XOR) { firstCheck ^= secondCheck; }
+          if (page13.operation[y].bitwise == BITWISE_AND) { firstCheck &= secondCheck; }
+          if (page13.operation[y].bitwise == BITWISE_OR) { firstCheck |= secondCheck; }
+          if (page13.operation[y].bitwise == BITWISE_XOR) { firstCheck ^= secondCheck; }
         }
       }
 
       //If the limiting time is active(>0) and using maximum time
-      if (BIT_CHECK(configPage13.kindOfLimiting, y))
+      if (BIT_CHECK(page13.kindOfLimiting, y))
       {
         if(firstCheck)
         {
-          if ((configPage13.outputTimeLimit[y] != 0) && (ioOutDelay[y] >= configPage13.outputTimeLimit[y])) { firstCheck = false; } //Time has counted, disable the output
+          if ((page13.outputTimeLimit[y] != 0) && (ioOutDelay[y] >= page13.outputTimeLimit[y])) { firstCheck = false; } //Time has counted, disable the output
         }
         else
         {
           //Released before Maximum time, set delay to maximum to flip the output next
-          if(BIT_CHECK(currentStatus.outputsStatus, y)) { ioOutDelay[y] = configPage13.outputTimeLimit[y]; }
+          if(BIT_CHECK(current.outputsStatus, y)) { ioOutDelay[y] = page13.outputTimeLimit[y]; }
           else { ioOutDelay[y] = 0; } //Reset the counter for next time
         }
       }
 
-      if ( (firstCheck == true) && (configPage13.outputDelay[y] < UINT8_MAX) )
+      if ( (firstCheck == true) && (page13.outputDelay[y] < UINT8_MAX) )
       {
-        if (ioDelay[y] >= configPage13.outputDelay[y])
+        if (ioDelay[y] >= page13.outputDelay[y])
         {
-          bool bitStatus = BIT_CHECK(configPage13.outputInverted, y) ^ firstCheck;
-          if (BIT_CHECK(currentStatus.outputsStatus, y) && (ioOutDelay[y] < configPage13.outputTimeLimit[y])) { ioOutDelay[y]++; }
-          if (configPage13.outputPin[y] < 128) { digitalWrite(configPage13.outputPin[y], bitStatus); }
+          bool bitStatus = BIT_CHECK(page13.outputInverted, y) ^ firstCheck;
+          if (BIT_CHECK(current.outputsStatus, y) && (ioOutDelay[y] < page13.outputTimeLimit[y])) { ioOutDelay[y]++; }
+          if (page13.outputPin[y] < 128) { digitalWrite(page13.outputPin[y], bitStatus); }
           else { BIT_WRITE(currentRuleStatus, y, bitStatus); }
-          BIT_WRITE(currentStatus.outputsStatus, y, bitStatus);
+          BIT_WRITE(current.outputsStatus, y, bitStatus);
         }
         else { ioDelay[y]++; }
       }
       else
       {
-        if (ioOutDelay[y] >= configPage13.outputTimeLimit[y])
+        if (ioOutDelay[y] >= page13.outputTimeLimit[y])
         {
-          bool bitStatus = BIT_CHECK(configPage13.outputInverted, y) ^ firstCheck;
-          if (configPage13.outputPin[y] < 128) { digitalWrite(configPage13.outputPin[y], bitStatus); }
+          bool bitStatus = BIT_CHECK(page13.outputInverted, y) ^ firstCheck;
+          if (page13.outputPin[y] < 128) { digitalWrite(page13.outputPin[y], bitStatus); }
           else { BIT_WRITE(currentRuleStatus, y, bitStatus); }
-          BIT_WRITE(currentStatus.outputsStatus, y, bitStatus);
-          if(!BIT_CHECK(configPage13.kindOfLimiting, y)) { ioOutDelay[y] = 0; }
+          BIT_WRITE(current.outputsStatus, y, bitStatus);
+          if(!BIT_CHECK(page13.kindOfLimiting, y)) { ioOutDelay[y] = 0; }
         }
         else { ioOutDelay[y]++; }
 
@@ -156,7 +168,7 @@ void checkProgrammableIO(void)
     }
   }
 }
-/** Get single I/O data var (from currentStatus) for comparison.
+/** Get single I/O data var (from current) for comparison.
  * @param index - Field index/number (?)
  * @return 16 bit (int) result
  */
--- speeduino/programmableIOControl.h
+++ speeduino/programmableIOControl.h
@@ -1,35 +1,9 @@
-/*
-These are some utility functions and variables used through the main code
-*/
-#ifndef UTILS_H
-#define UTILS_H
+#pragma once
 
-#include <Arduino.h>
+#include <stdint.h>
+#include "statuses.h"
+#include "config_pages.h"
 
-#define COMPARATOR_EQUAL 0
-#define COMPARATOR_NOT_EQUAL 1
-#define COMPARATOR_GREATER 2
-#define COMPARATOR_GREATER_EQUAL 3
-#define COMPARATOR_LESS 4
-#define COMPARATOR_LESS_EQUAL 5
-#define COMPARATOR_AND 6
-#define COMPARATOR_XOR 7
-
-#define BITWISE_DISABLED 0
-#define BITWISE_AND 1
-#define BITWISE_OR 2
-#define BITWISE_XOR 3
-
-#define REUSE_RULES 240
-
-extern uint8_t ioOutDelay[sizeof(configPage13.outputPin)];
-extern uint8_t ioDelay[sizeof(configPage13.outputPin)];
-extern uint8_t pinIsValid;
-extern uint8_t currentRuleStatus;
-//uint8_t outputPin[sizeof(configPage13.outputPin)];
-
-void initialiseProgrammableIO(void);
-void checkProgrammableIO(void);
+void initialiseProgrammableIO(statuses& current, const config13& page13);
+void checkProgrammableIO(statuses& current, const config13& page13);
 int16_t ProgrammableIOGetData(uint16_t index);
-
-#endif // UTILS_H
