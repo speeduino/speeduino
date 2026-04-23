0e0c799a98e71df9aac9ac1ea86a9891b1d6516e
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2026-04-23 13:01:49 -0500

    shift_blink

--- speeduino/programmableIOControl.cpp
+++ speeduino/programmableIOControl.cpp
@@ -12,22 +12,6 @@ TESTABLE_STATIC uint8_t ioOutDelay[_countof(config13::outputPin)];
 TESTABLE_STATIC uint8_t pinIsValid = 0;
 TESTABLE_STATIC uint8_t currentRuleStatus = 0;
 
-constexpr uint8_t COMPARATOR_EQUAL = 0;
-constexpr uint8_t COMPARATOR_NOT_EQUAL = 1;
-constexpr uint8_t COMPARATOR_GREATER = 2;
-constexpr uint8_t COMPARATOR_GREATER_EQUAL = 3;
-constexpr uint8_t COMPARATOR_LESS = 4;
-constexpr uint8_t COMPARATOR_LESS_EQUAL = 5;
-constexpr uint8_t COMPARATOR_AND = 6;
-constexpr uint8_t COMPARATOR_XOR = 7;
-
-constexpr uint8_t BITWISE_DISABLED = 0;
-constexpr uint8_t BITWISE_AND = 1;
-constexpr uint8_t BITWISE_OR = 2;
-constexpr uint8_t BITWISE_XOR = 3;
-
-constexpr uint8_t REUSE_RULES = 240;
-
 //*********************************************************************************************************************************************************************************
 void __attribute__((optimize("Os"))) initialiseProgrammableIO(statuses& current, const config13& page13)
 {
