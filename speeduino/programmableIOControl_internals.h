eca8992c0c59bc4b625bde9c5197f5792e2c33d8
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2026-04-23 23:51:20 -0500

    Internal struct memeber optimization

--- speeduino/programmableIOControl_internals.h
+++ speeduino/programmableIOControl_internals.h
@@ -4,16 +4,21 @@
  */
 
 #include <stdint.h>
-
 namespace programmableIOControl_details
 {
 
-struct programmableio_channel_t
+// The struct representing the state of each programmable I/O channel, used for processing the 
+// rules and keeping track of delays and active status.
+struct channel_t
 {
-  bool pinIsValid = false;
-  uint8_t ioDelay = 0;
-  uint8_t ioOutDelay = 0;
-  bool currentRuleStatus = false;
+  bool isPinValid : 1;
+  bool isRuleActive : 1;
+  uint8_t activationDelayCount = 0;
+  uint8_t outputDelayCount = 0;
+
+  channel_t()
+  : isPinValid(false), isRuleActive(false), activationDelayCount(0), outputDelayCount(0)
+  {}
 };
 
 // Defined here for unit testing, but only used in programmableIOControl.cpp
@@ -33,35 +38,52 @@ constexpr uint8_t BITWISE_XOR = 3;
 
 constexpr uint8_t REUSE_RULES = 240;
 
-struct compOperation {
+struct compOperation_t {
   uint8_t opType;
   uint8_t dataIndex;
   int16_t target;
+
+  bool isVirtualData() const {
+    return dataIndex >= REUSE_RULES;
+  }
+  uint8_t getDataIndex() const {
+    if (!isVirtualData()) {
+      return dataIndex;
+    }
+    return dataIndex - REUSE_RULES;
+  }
+};
+
+enum class LimitingType : uint8_t {
+  Min = 0,
+  Max = 1,
 };
 
 // It's much easier to work with a struct that represents the rule in a more direct way, so we convert the config13 struct into this for processing
 // Ideally the tune would also be able to use this struct directly, but that would require some refactoring of how the config pages are stored and accessed, 
 // so for now we keep using config13 for the tune and convert to programmableOutputRule for processing
-struct programmableOutputRule {
-  bool outputInverted; ///< Invert (on/off) value before writing to output pin (for all programmable I/O:s).
-  bool kindOfLimiting; ///< Select which kind of output limiting are active (0 - minimum | 1 - maximum)
+struct rule_t {
+  bool isOutputInverted; ///< Invert (on/off) value before writing to output pin (for all programmable I/O:s).
+  LimitingType limitType; ///< Select which kind of output limiting are active (0 - minimum | 1 - maximum)
   uint8_t outputPin;   ///< Disable(0) or enable (set to valid pin number) Programmable Pin (output/target pin to set)
-  uint8_t outputDelay; ///< Output write delay for each programmable I/O (Unit: 0.1S)
+  uint8_t activationDelay; ///< Output write delay for each programmable I/O (Unit: 0.1S)
   uint8_t outputTimeLimit; ///< Output delay for each programmable I/O, kindOfLimiting bit dependent(Unit: 0.1S)
-  uint8_t opCompare;
-  compOperation firstOp; ///< First operand for comparison
-  compOperation secondOp; ///< Second operand for comparison, used if bitwise operation is enabled
+  uint8_t combineOpType;
+  compOperation_t firstOp; ///< First operand for comparison
+  compOperation_t secondOp; ///< Second operand for comparison, used if bitwise operation is enabled
+  uint8_t _index; // Index of the rule
 
-  programmableOutputRule() = default;
-  programmableOutputRule(const config13& page13, uint8_t index) 
-   : outputInverted(BIT_CHECK(page13.outputInverted, index)),
-     kindOfLimiting(BIT_CHECK(page13.kindOfLimiting, index)),
+  rule_t() = default;
+  rule_t(const config13& page13, uint8_t index) 
+   : isOutputInverted(BIT_CHECK(page13.outputInverted, index)),
+     limitType(BIT_CHECK(page13.kindOfLimiting, index) ? LimitingType::Max : LimitingType::Min),
      outputPin(page13.outputPin[index]),
-     outputDelay(page13.outputDelay[index]),
+     activationDelay(page13.outputDelay[index]),
      outputTimeLimit(page13.outputTimeLimit[index]),
-     opCompare(page13.operation[index].bitwise),
+     combineOpType(page13.operation[index].bitwise),
      firstOp({page13.operation[index].firstCompType, page13.firstDataIn[index], page13.firstTarget[index]}),
-     secondOp({page13.operation[index].secondCompType, page13.secondDataIn[index], page13.secondTarget[index]})
+     secondOp({page13.operation[index].secondCompType, page13.secondDataIn[index], page13.secondTarget[index]}),
+     _index(index)
   {
   }
 
