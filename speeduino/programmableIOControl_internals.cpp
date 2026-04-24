3910b1121e53d8bac40bb3cfdd353215a30a1ff9
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2026-04-24 14:26:40 -0500

    Add state_t to store the programmable IO state

---
+++ speeduino/programmableIOControl_internals.cpp
@@ -0,0 +1,14 @@
+#include "programmableIOControl_internals.h"
+#include "globals.h"
+
+namespace programmableIOControl_details {
+
+void channel_t::initialize(const rule_t& rule) 
+{
+    isPinValid = rule.outputPin>0 && (rule.isCascadeRule() || !pinIsUsed(rule.outputPin));
+    isRuleActive = false;
+    activationDelayCount = 0;
+    outputDelayCount = 0;
+}
+
+}
