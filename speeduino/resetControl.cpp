e729cd3af8f2550a0fe954cc94db6a90e81374ed
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2026-05-14 09:04:12 -0500

    Only set reset control pin mode of reset control is *not* disabled.

--- speeduino/resetControl.cpp
+++ speeduino/resetControl.cpp
@@ -18,16 +18,19 @@ bool isResetPreventActive(void)
 void __attribute__((optimize("Os"))) initialiseResetControl(ResetControlMode resetControlMode, uint8_t resetPin)
 {
   _resetControl = resetControlMode;
-  _resetPin = resetPin;
   // We are assuming the engine is not running at the point of initialisation, so reset prevent 
   // should only be active if the mode is "Prevent Always"
   _resetPreventActive = resetControlMode == ResetControlMode::PreventAlways;
-  digitalWrite(resetPin, resetControlMode == ResetControlMode::PreventWhenRunning ? LOW : HIGH);
+  if (_resetControl!=ResetControlMode::Disabled)
+  {
+    _resetPin = resetPin;
+    digitalWrite(resetPin, resetControlMode == ResetControlMode::PreventWhenRunning ? LOW : HIGH);
 
-  /* Reset control is a special case. If reset control is enabled, it needs its initial state set BEFORE its pinMode.
-     If that doesn't happen and reset control is in "Serial Command" mode, the Arduino will end up in a reset loop
-     because the control pin will go low as soon as the pinMode is set to OUTPUT. */
-  pinMode(resetPin, OUTPUT);
+    /* Reset control is a special case. If reset control is enabled, it needs its initial state set BEFORE its pinMode.
+      If that doesn't happen and reset control is in "Serial Command" mode, the Arduino will end up in a reset loop
+      because the control pin will go low as soon as the pinMode is set to OUTPUT. */
+    pinMode(resetPin, OUTPUT);
+  }
 }
 
 void matchResetControlToEngineState(const statuses &current)
