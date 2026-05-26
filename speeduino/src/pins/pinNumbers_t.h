2b1b3b02ff5170a7f3a20fcb5cf64ecc19d38b32
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2026-05-26 07:35:57 -0500

    Consistent pin initialisation

--- speeduino/src/pins/pinNumbers_t.h
+++ speeduino/src/pins/pinNumbers_t.h
@@ -8,7 +8,7 @@
 template <uint8_t N>
 struct pin_array_t
 {
-  uint8_t pins[N] = {0};
+  uint8_t pins[N] = {NOT_A_PIN};
 
   /**
    * @brief Populate by copying from a source array stored in flash
@@ -19,14 +19,14 @@ struct pin_array_t
   {
     uint8_t elementsToCopy = min(N, length);
     (void)memcpy_P(pins, pSrc, elementsToCopy*sizeof(pins[0]));
-    // Fill remainder with zero
-    memset(pins+elementsToCopy, 0, (N-elementsToCopy)*sizeof(pins[0]));
+    // Fill remainder with NOT_A_PIN
+    (void)memset(pins+elementsToCopy, NOT_A_PIN, (N-elementsToCopy)*sizeof(pins[0]));
   }
 
   /** @brief Does the array contain \p pin? */
   bool isPinUsed(uint8_t pin) const
   {
-    uint8_t index = 0;
+    uint8_t index = NOT_A_PIN;
     while (index<N && pin!=pins[index])
     {
       ++index;
@@ -54,27 +54,27 @@ struct coil_pins_t : pin_array_t<IGN_CHANNELS>
 
 struct decoder_pins_t
 {
-  uint8_t primary = 0; ///< Crank angle sensor pin
-  uint8_t secondary = 0; ///< Cam angle sensor pin
-  uint8_t tertiary = 0; ///< 2nd cam angle sensor pin (often VVT)
+  uint8_t primary = NOT_A_PIN; ///< Crank angle sensor pin
+  uint8_t secondary = NOT_A_PIN; ///< Cam angle sensor pin
+  uint8_t tertiary = NOT_A_PIN; ///< 2nd cam angle sensor pin (often VVT)
 };
 
 /** @brief Input sensor pins */
 struct sensor_pins_t
 {
-  uint8_t TPS = 0;///< TPS input pin
-  uint8_t MAP = 0; ///< MAP sensor pin
-  uint8_t EMAP = 0; ///< EMAP sensor pin
-  uint8_t IAT = 0; ///< IAT sensor pin
-  uint8_t CLT = 0; ///< CLS sensor pin
-  uint8_t O2 = 0; ///< O2 Sensor pin
-  uint8_t O2_2 = 0; ///< Second O2 pin
-  uint8_t Bat = 0; ///< Battery voltage pin
-  uint8_t CTPS = 0; ///< Input for triggering closed throttle state
-  uint8_t flex = 0; ///< Pin with the flex sensor attached
-  uint8_t fuelPressure = 0;
-  uint8_t oilPressure = 0;
-  uint8_t baro = 0; //Pin that an external barometric pressure sensor is attached to (If used)
+  uint8_t TPS = NOT_A_PIN;///< TPS input pin
+  uint8_t MAP = NOT_A_PIN; ///< MAP sensor pin
+  uint8_t EMAP = NOT_A_PIN; ///< EMAP sensor pin
+  uint8_t IAT = NOT_A_PIN; ///< IAT sensor pin
+  uint8_t CLT = NOT_A_PIN; ///< CLS sensor pin
+  uint8_t O2 = NOT_A_PIN; ///< O2 Sensor pin
+  uint8_t O2_2 = NOT_A_PIN; ///< Second O2 pin
+  uint8_t Bat = NOT_A_PIN; ///< Battery voltage pin
+  uint8_t CTPS = NOT_A_PIN; ///< Input for triggering closed throttle state
+  uint8_t flex = NOT_A_PIN; ///< Pin with the flex sensor attached
+  uint8_t fuelPressure = NOT_A_PIN;
+  uint8_t oilPressure = NOT_A_PIN;
+  uint8_t baro = NOT_A_PIN; //Pin that an external barometric pressure sensor is attached to (If used)
 
   bool isPinUsed(uint8_t pin) const;
 };
@@ -82,36 +82,36 @@ struct sensor_pins_t
 /** @brief Idle pins */
 struct idle_pins_t
 {
-  uint8_t idle1 = 0; ///< Single wire idle control
-  uint8_t idle2 = 0; ///< 2 wire idle control
-  uint8_t idleUp = 0; ///< Input for triggering Idle Up
-  uint8_t idleUpOutput = 0; ///< Output that follows (normal or inverted) the idle up pin
-  uint8_t stepperDir = 0; //Direction pin for the stepper motor driver
-  uint8_t stepperStep = 0; //Step pin for the stepper motor driver
-  uint8_t stepperEnable = 0; //Turning the DRV8825 driver on/off
+  uint8_t idle1 = NOT_A_PIN; ///< Single wire idle control
+  uint8_t idle2 = NOT_A_PIN; ///< 2 wire idle control
+  uint8_t idleUp = NOT_A_PIN; ///< Input for triggering Idle Up
+  uint8_t idleUpOutput = NOT_A_PIN; ///< Output that follows (normal or inverted) the idle up pin
+  uint8_t stepperDir = NOT_A_PIN; //Direction pin for the stepper motor driver
+  uint8_t stepperStep = NOT_A_PIN; //Step pin for the stepper motor driver
+  uint8_t stepperEnable = NOT_A_PIN; //Turning the DRV8825 driver on/off
 };
 
 /** @brief Water Methanol Injection (WMI) pins */
 struct wmi_pins_t
 {
-  uint8_t empty = 0; ///< Water tank empty sensor
-  uint8_t indicator = 0; ///< No water indicator bulb
-  uint8_t enabled = 0; ///< ON-OFF output to relay/pump/solenoid
+  uint8_t empty = NOT_A_PIN; ///< Water tank empty sensor
+  uint8_t indicator = NOT_A_PIN; ///< No water indicator bulb
+  uint8_t enabled = NOT_A_PIN; ///< ON-OFF output to relay/pump/solenoid
 };
 
 /** @brief Air conditioning pins */
 struct aircon_pins_t
 {
-  uint8_t compressor = 0;    ///< Air conditioning compressor output
-  uint8_t fan = 0;    ///< Stand-alone air conditioning fan output
-  uint8_t request = 0; ///< Air conditioning request input
+  uint8_t compressor = NOT_A_PIN;    ///< Air conditioning compressor output
+  uint8_t fan = NOT_A_PIN;    ///< Stand-alone air conditioning fan output
+  uint8_t request = NOT_A_PIN; ///< Air conditioning request input
 };
 
 /** @brief Pin configuration for an MC33810 board */
 struct mc33810_pins_t
 {
-  uint8_t CS_1 = 0; ///< Chip select pin for the first MC33810 IC (Channels 1-4)
-  uint8_t CS_2 = 0; ///< Chip select pin for the second MC33810 IC (Channels 5-8)
+  uint8_t CS_1 = NOT_A_PIN; ///< Chip select pin for the first MC33810 IC (Channels 1-4)
+  uint8_t CS_2 = NOT_A_PIN; ///< Chip select pin for the second MC33810 IC (Channels 5-8)
   uint8_t injBits[8];
   uint8_t ignBits[8];
 };
@@ -127,20 +127,20 @@ struct pinNumbers_t
   wmi_pins_t wmi;
   aircon_pins_t airCon;
 
-  uint8_t pinTachOut = 0; ///< Tacho output
-  uint8_t pinFuelPump = 0; ///< Fuel pump on/off
-  uint8_t pinFuel2Input = 0; ///< Input for switching to the 2nd fuel table
-  uint8_t pinSpark2Input = 0; ///< Input for switching to the 2nd ignition table
-  uint8_t pinBoost = 0; ///< Boost control output
-  uint8_t pinVVT_1 = 0;		///< vvt output 1
-  uint8_t pinVVT_2 = 0;		///< vvt output 2
-  uint8_t pinFan = 0;       ///< Cooling fan output
-  uint8_t pinLaunch = 0; ///< Launch control
-  uint8_t pinIgnBypass = 0; ///< The pin used for an ignition bypass (Optional)
-  uint8_t pinVSS = 0; ///< Speed sensor
-  uint8_t pinResetControl = 0; ///< Output pin used control resetting the Arduino
+  uint8_t pinTachOut = NOT_A_PIN; ///< Tacho output
+  uint8_t pinFuelPump = NOT_A_PIN; ///< Fuel pump on/off
+  uint8_t pinFuel2Input = NOT_A_PIN; ///< Input for switching to the 2nd fuel table
+  uint8_t pinSpark2Input = NOT_A_PIN; ///< Input for switching to the 2nd ignition table
+  uint8_t pinBoost = NOT_A_PIN; ///< Boost control output
+  uint8_t pinVVT_1 = NOT_A_PIN;		///< vvt output 1
+  uint8_t pinVVT_2 = NOT_A_PIN;		///< vvt output 2
+  uint8_t pinFan = NOT_A_PIN;       ///< Cooling fan output
+  uint8_t pinLaunch = NOT_A_PIN; ///< Launch control
+  uint8_t pinIgnBypass = NOT_A_PIN; ///< The pin used for an ignition bypass (Optional)
+  uint8_t pinVSS = NOT_A_PIN; ///< Speed sensor
+  uint8_t pinResetControl = NOT_A_PIN; ///< Output pin used control resetting the Arduino
 #ifdef SD_LOGGING
-  uint8_t pinSDEnable = 0; //Input for manually enabling SD logging
+  uint8_t pinSDEnable = NOT_A_PIN; //Input for manually enabling SD logging
 #endif
 #if defined(MC33810_SUPPORT)
   mc33810_pins_t mc33810;
