49b09753eaca014428e523226b2290f915ff50ae
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2026-06-08 17:02:16 +0100

    Rename ProgrammableIOGetData - correct capitalization

--- speeduino/programmableIOControl.h
+++ speeduino/programmableIOControl.h
@@ -5,5 +5,5 @@
 
 void initialiseProgrammableIO(const config13& page13);
 void checkProgrammableIO(const config13& page13);
-int16_t ProgrammableIOGetData(uint16_t index);
+int16_t programmableIOGetData(uint16_t index);
 uint8_t getProgrammableIOOutputStatus(void);
