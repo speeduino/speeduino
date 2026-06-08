944dcd055beb6426ae09e2a58fa015dbaf003576
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2026-06-08 16:54:01 +0100

    Move evaluateRule() to rule_t

--- test/test_programmableio/test_rule_t.cpp
+++ test/test_programmableio/test_rule_t.cpp
@@ -52,6 +52,54 @@ static void test_combine_disabled(void)
     TEST_ASSERT_FALSE(rule.evaluateCombineOp(true, true));
 }
 
+static void test_evaluate_first_op_only_true(void)
+{
+    rule_t rule{};
+    rule.combineOpType = COMBINE_DISABLED;
+    rule.firstOp = { COMPARATOR_EQUAL, 0, 1 };
+
+    TEST_ASSERT_TRUE(rule.evaluate(state_t{}, mockGetData));
+}
+
+static void test_evaluate_first_op_only_false(void)
+{
+    rule_t rule{};
+    rule.combineOpType = COMBINE_DISABLED;
+    rule.firstOp = { COMPARATOR_LESS, 0, 1 };
+
+    TEST_ASSERT_FALSE(rule.evaluate(state_t{}, mockGetData));
+}
+
+static void test_evaluate_combine_and(void)
+{
+    rule_t rule{};
+    rule.combineOpType = COMBINE_AND;
+    rule.firstOp = { COMPARATOR_EQUAL, 0, 1 };
+    rule.secondOp = { COMPARATOR_GREATER, 1, 5 };
+
+    TEST_ASSERT_TRUE(rule.evaluate(state_t{}, mockGetData));
+}
+
+static void test_evaluate_combine_or(void)
+{
+    rule_t rule{};
+    rule.combineOpType = COMBINE_OR;
+    rule.firstOp = { COMPARATOR_LESS, 0, 1 };
+    rule.secondOp = { COMPARATOR_EQUAL, 1, 11 };
+
+    TEST_ASSERT_TRUE(rule.evaluate(state_t{}, mockGetData));
+}
+
+static void test_evaluate_combine_xor(void)
+{
+    rule_t rule{};
+    rule.combineOpType = COMBINE_XOR;
+    rule.firstOp = { COMPARATOR_EQUAL, 0, 1 };
+    rule.secondOp = { COMPARATOR_EQUAL, 1, 11 };
+
+    TEST_ASSERT_FALSE(rule.evaluate(state_t{}, mockGetData));
+}
+
 void testRule(void) 
 {
     SET_UNITY_FILENAME() {
@@ -59,5 +107,10 @@ void testRule(void)
         RUN_TEST_P(test_combine_or);
         RUN_TEST_P(test_combine_xor);
         RUN_TEST_P(test_combine_disabled);
+        RUN_TEST_P(test_evaluate_first_op_only_true);
+        RUN_TEST_P(test_evaluate_first_op_only_false);
+        RUN_TEST_P(test_evaluate_combine_and);
+        RUN_TEST_P(test_evaluate_combine_or);
+        RUN_TEST_P(test_evaluate_combine_xor);
     }
 }
