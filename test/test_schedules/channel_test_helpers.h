ff026b13a0e4d720d60e91c6c37c53942d49a6fd
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2026-06-16 22:33:34 +0100

    Attempt to workaround SimAVR 1.7 bug

--- test/test_schedules/channel_test_helpers.h
+++ test/test_schedules/channel_test_helpers.h
@@ -44,14 +44,14 @@
 #define RUNIF_INJCHANNEL8(ignored, codeBlock) (codeBlock);
 #endif
 
-#define INJCHANNEL_TEST_HELPER1(codeBlock) RUNIF_INJCHANNEL1(codeBlock, TEST_IGNORE_MESSAGE("Not enough channels"))
-#define INJCHANNEL_TEST_HELPER2(codeBlock) RUNIF_INJCHANNEL2(codeBlock, TEST_IGNORE_MESSAGE("Not enough channels"))
-#define INJCHANNEL_TEST_HELPER3(codeBlock) RUNIF_INJCHANNEL3(codeBlock, TEST_IGNORE_MESSAGE("Not enough channels"))
-#define INJCHANNEL_TEST_HELPER4(codeBlock) RUNIF_INJCHANNEL4(codeBlock, TEST_IGNORE_MESSAGE("Not enough channels"))
-#define INJCHANNEL_TEST_HELPER5(codeBlock) RUNIF_INJCHANNEL5(codeBlock, TEST_IGNORE_MESSAGE("Not enough channels"))
-#define INJCHANNEL_TEST_HELPER6(codeBlock) RUNIF_INJCHANNEL6(codeBlock, TEST_IGNORE_MESSAGE("Not enough channels"))
-#define INJCHANNEL_TEST_HELPER7(codeBlock) RUNIF_INJCHANNEL7(codeBlock, TEST_IGNORE_MESSAGE("Not enough channels"))
-#define INJCHANNEL_TEST_HELPER8(codeBlock) RUNIF_INJCHANNEL8(codeBlock, TEST_IGNORE_MESSAGE("Not enough channels"))
+#define INJCHANNEL_TEST_HELPER1(codeBlock) RUNIF_INJCHANNEL1(codeBlock, TEST_IGNORE_MESSAGE(String(F("Not enough channels")).c_str()))
+#define INJCHANNEL_TEST_HELPER2(codeBlock) RUNIF_INJCHANNEL2(codeBlock, TEST_IGNORE_MESSAGE(String(F("Not enough channels")).c_str()))
+#define INJCHANNEL_TEST_HELPER3(codeBlock) RUNIF_INJCHANNEL3(codeBlock, TEST_IGNORE_MESSAGE(String(F("Not enough channels")).c_str()))
+#define INJCHANNEL_TEST_HELPER4(codeBlock) RUNIF_INJCHANNEL4(codeBlock, TEST_IGNORE_MESSAGE(String(F("Not enough channels")).c_str()))
+#define INJCHANNEL_TEST_HELPER5(codeBlock) RUNIF_INJCHANNEL5(codeBlock, TEST_IGNORE_MESSAGE(String(F("Not enough channels")).c_str()))
+#define INJCHANNEL_TEST_HELPER6(codeBlock) RUNIF_INJCHANNEL6(codeBlock, TEST_IGNORE_MESSAGE(String(F("Not enough channels")).c_str()))
+#define INJCHANNEL_TEST_HELPER7(codeBlock) RUNIF_INJCHANNEL7(codeBlock, TEST_IGNORE_MESSAGE(String(F("Not enough channels")).c_str()))
+#define INJCHANNEL_TEST_HELPER8(codeBlock) RUNIF_INJCHANNEL8(codeBlock, TEST_IGNORE_MESSAGE(String(F("Not enough channels")).c_str()))
 
 #if IGN_CHANNELS >= 1
 #define RUNIF_IGNCHANNEL1(codeBlock, ignored) (codeBlock);
@@ -94,11 +94,11 @@
 #define RUNIF_IGNCHANNEL8(ignored, codeBlock) (codeBlock);
 #endif
 
-#define IGNCHANNEL_TEST_HELPER1(codeBlock) RUNIF_IGNCHANNEL1(codeBlock, TEST_IGNORE_MESSAGE("Not enough channels"))
-#define IGNCHANNEL_TEST_HELPER2(codeBlock) RUNIF_IGNCHANNEL2(codeBlock, TEST_IGNORE_MESSAGE("Not enough channels"))
-#define IGNCHANNEL_TEST_HELPER3(codeBlock) RUNIF_IGNCHANNEL3(codeBlock, TEST_IGNORE_MESSAGE("Not enough channels"))
-#define IGNCHANNEL_TEST_HELPER4(codeBlock) RUNIF_IGNCHANNEL4(codeBlock, TEST_IGNORE_MESSAGE("Not enough channels"))
-#define IGNCHANNEL_TEST_HELPER5(codeBlock) RUNIF_IGNCHANNEL5(codeBlock, TEST_IGNORE_MESSAGE("Not enough channels"))
-#define IGNCHANNEL_TEST_HELPER6(codeBlock) RUNIF_IGNCHANNEL6(codeBlock, TEST_IGNORE_MESSAGE("Not enough channels"))
-#define IGNCHANNEL_TEST_HELPER7(codeBlock) RUNIF_IGNCHANNEL7(codeBlock, TEST_IGNORE_MESSAGE("Not enough channels"))
-#define IGNCHANNEL_TEST_HELPER8(codeBlock) RUNIF_IGNCHANNEL8(codeBlock, TEST_IGNORE_MESSAGE("Not enough channels"))
+#define IGNCHANNEL_TEST_HELPER1(codeBlock) RUNIF_IGNCHANNEL1(codeBlock, TEST_IGNORE_MESSAGE(String(F("Not enough channels")).c_str()))
+#define IGNCHANNEL_TEST_HELPER2(codeBlock) RUNIF_IGNCHANNEL2(codeBlock, TEST_IGNORE_MESSAGE(String(F("Not enough channels")).c_str()))
+#define IGNCHANNEL_TEST_HELPER3(codeBlock) RUNIF_IGNCHANNEL3(codeBlock, TEST_IGNORE_MESSAGE(String(F("Not enough channels")).c_str()))
+#define IGNCHANNEL_TEST_HELPER4(codeBlock) RUNIF_IGNCHANNEL4(codeBlock, TEST_IGNORE_MESSAGE(String(F("Not enough channels")).c_str()))
+#define IGNCHANNEL_TEST_HELPER5(codeBlock) RUNIF_IGNCHANNEL5(codeBlock, TEST_IGNORE_MESSAGE(String(F("Not enough channels")).c_str()))
+#define IGNCHANNEL_TEST_HELPER6(codeBlock) RUNIF_IGNCHANNEL6(codeBlock, TEST_IGNORE_MESSAGE(String(F("Not enough channels")).c_str()))
+#define IGNCHANNEL_TEST_HELPER7(codeBlock) RUNIF_IGNCHANNEL7(codeBlock, TEST_IGNORE_MESSAGE(String(F("Not enough channels")).c_str()))
+#define IGNCHANNEL_TEST_HELPER8(codeBlock) RUNIF_IGNCHANNEL8(codeBlock, TEST_IGNORE_MESSAGE(String(F("Not enough channels")).c_str()))
