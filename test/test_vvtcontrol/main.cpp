3cdd7350646b23e222df59745b06356b52258b9b
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2026-08-01 09:54:34 -0500

    Unit test VvtOutputChannel

--- test/test_vvtcontrol/main.cpp
+++ test/test_vvtcontrol/main.cpp
@@ -3,13 +3,15 @@
 
 void runAllTests(void)
 {
-    void testVvtControl(void);
-    void testVvtInterrupt(void);
-    void testInit(void);
+    extern void testVvtControl(void);
+    extern void testVvtInterrupt(void);
+    extern void testInit(void);
+    extern void testVvtOutputChannel(void);
 
     testVvtControl();
     testVvtInterrupt();
     testInit();
+    testVvtOutputChannel();
 }
 
 TEST_HARNESS(runAllTests)
