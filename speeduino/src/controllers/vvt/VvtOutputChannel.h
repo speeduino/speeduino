9e92beab1b6443cffb4c4a3cc2e71ae0339eb066
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2026-08-02 11:19:29 -0500

    Remove VvtOutputChannel::compareTicks - it's always the same as targetDuty

--- speeduino/src/controllers/vvt/VvtOutputChannel.h
+++ speeduino/src/controllers/vvt/VvtOutputChannel.h
@@ -5,7 +5,6 @@
 struct VvtOutputChannel {
     uint16_t maxDuty = 0;        ///< Maximum duty based on motor frequency
     uint16_t targetDuty = 0;     ///< Requested duty cycle (0-100% or 0-255)
-    uint16_t compareTicks = 0;   ///< Active compare threshold for ISR match
     trackedOutputPin_t<boardOutputPin_t> pin; ///< The pin
 
     VvtOutputChannel() = default;
