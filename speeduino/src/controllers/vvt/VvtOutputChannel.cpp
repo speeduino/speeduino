0ac677fd8a536b373c9253c113665ee7e4076be1
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2026-08-02 11:08:43 -0500

    Remove VvtOutputChannel::periodTicks - it just shadows isFullDuty()

--- speeduino/src/controllers/vvt/VvtOutputChannel.cpp
+++ speeduino/src/controllers/vvt/VvtOutputChannel.cpp
@@ -15,18 +15,15 @@ void VvtOutputChannel::setTargetDutyFromDuty(uint8_t duty) noexcept
         //Make sure solenoid is off (0% duty)
         targetDuty = 0;
         pin.setPinLow();
-        periodTicks = false;
     }
     else if(duty >= 200 )
     {
         //Make sure solenoid is on (100% duty)
         targetDuty = maxDuty;
         pin.setPinHigh();
-        periodTicks = true;
     }
     else
     {
         targetDuty = halfPercentage(duty, maxDuty);
-        periodTicks = false;
     }
 }
--- speeduino/src/controllers/vvt/VvtOutputChannel.h
+++ speeduino/src/controllers/vvt/VvtOutputChannel.h
@@ -6,7 +6,6 @@ struct VvtOutputChannel {
     uint16_t maxDuty = 0;        ///< Maximum duty based on motor frequency
     uint16_t targetDuty = 0;     ///< Requested duty cycle (0-100% or 0-255)
     uint16_t compareTicks = 0;   ///< Active compare threshold for ISR match
-    bool     periodTicks = false;    // Total clock ticks per PWM period (frequency ceiling)
     trackedOutputPin_t<boardOutputPin_t> pin; ///< The pin
 
     VvtOutputChannel() = default;
