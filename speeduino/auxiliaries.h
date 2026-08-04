fb17ad55cf25974127ec534a6a081cb2ba76338b
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2026-08-04 17:58:36 -0500

    Rename initialiseAuxPWM() to initialiseVvtWmi()

--- speeduino/auxiliaries.h
+++ speeduino/auxiliaries.h
@@ -5,11 +5,16 @@
 #include "statuses.h"
 #include "src/pins/pinNumbers_t.h"
 
-void initialiseAuxPWM(statuses &current, const pinNumbers_t &pins, const config4 &page4, const config6 &page6, config10 &page10);
+/** @brief Initialise the VVT & WMI controllers */
+void initialiseVvtWmi(statuses &current, const pinNumbers_t &pins, const config4 &page4, const config6 &page6, config10 &page10);
 
+/** @brief VVT control. Should be called from the main loop */
 void vvtControl(statuses &current, const config4 &page4, const config6 &page6, config10 &page10);
+
+/** @brief WMI control. Should be called from the main loop */
 void wmiControl(statuses &current, const config10 &page10);
 
+/** @brief VVT timer callback for PWM control*/
 void vvtInterrupt(void);
 
 #endif
