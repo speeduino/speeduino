525a988b7c203798b916c803315b6162bc4c34c2
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2026-08-06 16:45:39 -0500

    Improve boost interrupt coverage

--- test/test_boostcontrol/test_boostinterrupt.cpp
+++ test/test_boostcontrol/test_boostinterrupt.cpp
@@ -5,37 +5,74 @@
 #include "src/pins/boardOutputPin.h"
 
 extern volatile bool boost_pwm_state;
+extern long boost_pwm_target_value;
+extern uint16_t boost_pwm_max_count;
 extern boardOutputPin_t boost_pin;
 
-static void test_on_to_off(void)
+static void test_duty_full(void)
 {
     pinNumbers.pinBoost = TEST_BOOST_PIN;
     initialiseBoost(TEST_BOOST_PIN);
 
-    boost_pwm_state = true;
-    boostInterrupt();
+    boost_pin.setPinHigh();
+    boost_pwm_state = false;
+    boost_pwm_target_value = boost_pwm_max_count;
 
-    TEST_ASSERT_TRUE(boost_pin._pin.isPinLow());
-    TEST_ASSERT_FALSE(boost_pwm_state);
+    // for (uint8_t loop=0; loop<7; ++loop)
+    {
+      boostInterrupt();
+
+      TEST_ASSERT_TRUE(boost_pin._pin.isPinHigh());
+      TEST_ASSERT_TRUE(boost_pwm_state);
+    }
 }
 
-static void test_off_to_on(void)
+
+static void test_partial_duty(void)
 {
     pinNumbers.pinBoost = TEST_BOOST_PIN;
     initialiseBoost(TEST_BOOST_PIN);
 
+    boost_pin.setPinLow();
     boost_pwm_state = false;
-    boostInterrupt();
+    boost_pwm_target_value = percentage(66, boost_pwm_max_count);
 
-    TEST_ASSERT_TRUE(boost_pin._pin.isPinHigh());
-    TEST_ASSERT_TRUE(boost_pwm_state);
+    for (uint8_t loop=0; loop<7; ++loop)
+    {
+      boostInterrupt();
+      TEST_ASSERT_TRUE(boost_pin._pin.isPinHigh());
+      TEST_ASSERT_TRUE(boost_pwm_state);
+
+      boostInterrupt();
+      TEST_ASSERT_TRUE(boost_pin._pin.isPinLow());
+      TEST_ASSERT_FALSE(boost_pwm_state);
+    }
+}
+
+static void test_duty_none(void)
+{
+    pinNumbers.pinBoost = TEST_BOOST_PIN;
+    initialiseBoost(TEST_BOOST_PIN);
+
+    boost_pin.setPinHigh();
+    boost_pwm_state = true;
+    boost_pwm_target_value = 0;
+
+    // for (uint8_t loop=0; loop<7; ++loop)
+    {
+      boostInterrupt();
+
+      TEST_ASSERT_TRUE(boost_pin._pin.isPinLow());
+      TEST_ASSERT_FALSE(boost_pwm_state);
+    }
 }
 
 void testBoostInterrupt(void)
 {
   SET_UNITY_FILENAME()
   {
-    RUN_TEST_P(test_on_to_off);
-    RUN_TEST_P(test_off_to_on);
+    RUN_TEST_P(test_duty_none);
+    RUN_TEST_P(test_partial_duty);
+    RUN_TEST_P(test_duty_full);
   }
 }
