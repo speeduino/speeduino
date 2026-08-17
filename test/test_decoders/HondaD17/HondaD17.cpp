0c96e4aae2b9d3f79029ed0452531593a6ad4db9
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2026-08-17 09:00:22 -0500

    HondaD17 is non-sequential

--- test/test_decoders/HondaD17/HondaD17.cpp
+++ test/test_decoders/HondaD17/HondaD17.cpp
@@ -7,11 +7,12 @@ static void test_getCrankAngle(void)
 {
   extern volatile unsigned long toothLastToothTime;
   extern volatile int toothCurrentCount;
-      
+  extern bool revolutionOne;      
   auto decoder = triggerSetup_HondaD17();
 
   // Use deterministic time->angle conversion
   CRANK_ANGLE_MAX_IGN = CRANK_ANGLE_MAX_INJ = 720;
+  revolutionOne = false;
   setAngleConverterRevolutionTime(2000);
 
   // Setup common deterministic state
