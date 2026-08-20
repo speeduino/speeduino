d6b036e79b26c7a73eb0f5199b526517529b322a
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2026-08-20 08:52:04 -0500

    DOxygen

--- speeduino/crank_angle_calculator.h
+++ speeduino/crank_angle_calculator.h
@@ -1,17 +1,35 @@
 /**
  * @file 
  * @brief Crank calculations. To be reused by individual decoders
+ * 
+ * Crank angle calculations (usually) have 4 components:
+ * 1. The angle of the last detected tooth. 
+ * This can be computed from a fixed tooth angle or looked up from an array.
+ * This only gives accuracy to the nearest tooth.
+ * 2. A temporal adjustment to account for rotation since the last tooth was detected.
+ * This can be a simple delta or based on the interval between teeth
+ * 3. An adjustment for sequential operation to account for the 1st (0-359°) or 2nd (360-719°) revolution
+ * 4. A user defined offset from TDC (this accounts for sensor position on the wheel)
  */
 #pragma once
 #include "src/utils/nominmax.h"
 #include <tuple>
 #include "config_pages.h"
 
+/**
+ * @addtogroup CrankCalcMixins Crank calculation mixins
+ * @brief Mix-in classes, each capturing one component of a crank angle computation.
+ * 
+ * Do not use directly, instead use one of the concrete classes.
+ * @{
+ */
+
 /** @brief A crank angle calculator that computes a temporal adjustment based on when the tooth was last detected */
 struct last_tooth_rev_calculator_t
 {
   uint32_t _toothLastToothTime = 0;  ///< Time in µS that the current tooth was last detected
 
+protected:
   last_tooth_rev_calculator_t() = default;
   explicit last_tooth_rev_calculator_t(uint32_t toothLastToothTime);
   
@@ -19,8 +37,7 @@ struct last_tooth_rev_calculator_t
 };
 
 /** @brief A crank angle calculator that computes a temporal adjustment based on  the gap between the 2 most recent 
- * teeth rather than the last full revolution
- */
+ * teeth rather than the last full revolution */
 struct tooth_interval_calculator_t
 {
   uint32_t _toothLastToothTime = 0;         ///< Time in µS that the current tooth was last detected
@@ -28,6 +45,7 @@ struct tooth_interval_calculator_t
   uint16_t _toothAngle = 0;                 ///< Tooth angle (1-359°)
   bool _toothAngleCorrect;                  ///< Is @ref _toothAngle valid
 
+protected:
   tooth_interval_calculator_t() = default;
   explicit tooth_interval_calculator_t(uint32_t toothLastToothTime, uint32_t toothLastMinusOneToothTime, uint16_t toothAngle, bool toothAngleCorrect);
   
@@ -36,12 +54,12 @@ struct tooth_interval_calculator_t
 
 /** @brief A crank angle calculator that looks up the initial crank angle from an array.
  * 
- * Useful when the teeth are unevenly spaced or of varying widths
- */
+ * Useful when the teeth are unevenly spaced or of varying widths */
 struct lookup_initial_calculator_t
 {
   uint16_t _toothCurrentCount = 0;   ///< 1-based tooth number
 
+protected:
   lookup_initial_calculator_t() = default;
   explicit lookup_initial_calculator_t(uint16_t toothCurrentCount);
   
@@ -55,17 +73,16 @@ struct lookup_initial_calculator_t
 
 /** @brief A crank angle calculator that computes the initial crank angle from a tooth angle
  * 
- * Useful when the teeth are evenly spaced and the same width
- */
+ * Useful when the teeth are evenly spaced and the same width */
 struct compute_initial_calculator_t
 {
   uint16_t _toothCurrentCount = 0;  ///< 1-based tooth number
   uint16_t _toothAngle = 0;         ///< Tooth angle (1-359°)
 
+protected:
   compute_initial_calculator_t() = default;
   explicit compute_initial_calculator_t(uint16_t toothCurrentCount, uint16_t toothAngle);
   
-  /** @brief Calculate the crank angle */
   int16_t calculate(void) const;
 };
 
@@ -74,17 +91,21 @@ struct sequential_correction_calculator_t
 {
   bool _revZeroOrOne = false; ///< When calculating over 720°, tells the calculator if we are in the 1st revolution (false) or the 2nd (true)
 
+protected:
   sequential_correction_calculator_t() = default;
   explicit sequential_correction_calculator_t(bool revZeroOrOne);
   
-  /**
-   * @brief Calculate the crank angle
-   * 
-   * @param page4 The tune
-   */
   int16_t calculate(const config4 &page4) const;
 };
 
+/// @}
+
+/**
+ * @addtogroup CrankCalcs Crank calculation classes
+ * @brief Each of these is a composite of various mix-in classes, so pay close attention to the
+ * base classes - they define the calculation.
+ * @{
+ */
 struct simple_crank_angle_calculator_t : last_tooth_rev_calculator_t, sequential_correction_calculator_t
  {
     int16_t calculate(int16_t initialCrankAngle, uint32_t currMicros, const config4 &page4) const;
@@ -107,10 +128,8 @@ struct lookup_crank_angle_calculator_t : public last_tooth_rev_calculator_t, seq
 
 struct trigger_angle_crank_angle_calculator_t : public last_tooth_rev_calculator_t, sequential_correction_calculator_t, compute_initial_calculator_t
 {
-  using data_type = std::tuple<uint32_t, bool, uint16_t, uint16_t>;
-
   trigger_angle_crank_angle_calculator_t() = default;
-  explicit trigger_angle_crank_angle_calculator_t(const data_type &data);
+  explicit trigger_angle_crank_angle_calculator_t(const std::tuple<uint32_t, bool, uint16_t, uint16_t> &data);
 
   /**
    * @brief Calculate the crank angle
@@ -149,3 +168,5 @@ struct compute_crank_angle_calculator_tooth_interval_t : public tooth_interval_c
    */
   int16_t calculate(uint32_t currMicros, const config4 &page4) const;
 };
+
+/// @}
