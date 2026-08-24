4f1ac0dfb4bb65c77b846a94aa4e39b18e70cce2
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2026-08-24 09:53:05 -0500

    Remove processing_channel_t::outputPin - unused

--- speeduino/src/controllers/progammableIO/programmableIOControl_details.h
+++ speeduino/src/controllers/progammableIO/programmableIOControl_details.h
@@ -108,7 +108,6 @@ struct channel_state_t
 struct processing_channel_t
 {
   channel_state_t& _channel_state;
-  uint8_t outputPin = 0;   ///< Disable(0) or enable (set to valid pin number) Programmable Pin (output/target pin to set)
   LimitingType limitType; ///< Select which kind of output limiting are active (0 - minimum | 1 - maximum)
   uint8_t outputTimeLimit = 0; ///< Output delay for each programmable I/O, kindOfLimiting bit dependent(Unit: 0.1S)
   uint8_t activationDelay = 0; ///< Output write delay for each programmable I/O (Unit: 0.1S)
